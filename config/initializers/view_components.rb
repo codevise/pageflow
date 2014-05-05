a47051331795ab70b5b953279706e0a0af819703
Tim Fischbach <tfischbach@codevise.de>
2014-05-05 20:40:15 +0200

    fix view components initializer

--- config/initializers/view_components.rb
+++ config/initializers/view_components.rb
@@ -1,3 +1,3 @@
 # View components cannot be discovered via the auto loader since they
 # are never referenced by class name.
-Dir[config.root.join('app/views/components/**/*.rb')].each { |f| require(f) }
+Dir[Pageflow::Engine.root.join('app/views/components/**/*.rb')].each { |f| require(f) }
