c49b39ac3899a0c230e6e7237291dcee58310e01
Stefan Schoettelndreyer <sschoettelndreyer@codevise.de>
2014-06-02 17:24:30 +0200

    wire up account and theming

--- app/models/pageflow/theming.rb
+++ app/models/pageflow/theming.rb
@@ -1,5 +1,7 @@
 module Pageflow
   class Theming < ActiveRecord::Base
     belongs_to :theme
+    has_one :account, :foreign_key => :default_theming_id, :inverse_of => :default_theming
+    accepts_nested_attributes_for :account
   end
 end
