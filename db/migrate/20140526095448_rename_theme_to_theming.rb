1d61297575bac4c4f8fdd68ad324681e8d445b05
Stefan Schoettelndreyer <sschoettelndreyer@codevise.de>
2014-06-16 19:02:41 +0200

    improve migrations to move existing data

--- db/migrate/20140526095448_rename_theme_to_theming.rb
+++ db/migrate/20140526095448_rename_theme_to_theming.rb
@@ -1,11 +1,27 @@
 class RenameThemeToTheming < ActiveRecord::Migration
-  def change
+  def up
     rename_table :pageflow_themes, :pageflow_themings
 
-    remove_reference :pageflow_entries, :theme, :index => true
     add_reference :pageflow_entries, :theming, :index => true
-
-    remove_reference :pageflow_accounts, :default_theme
     add_reference :pageflow_accounts, :default_theming
+
+    execute("UPDATE pageflow_entries SET theming_id = theme_id;")
+    execute("UPDATE pageflow_accounts SET default_theming_id = default_theme_id;")
+
+    remove_reference :pageflow_entries, :theme, :index => true
+    remove_reference :pageflow_accounts, :default_theme
+  end
+
+  def down
+    rename_table :pageflow_themings, :pageflow_themes
+
+    add_reference :pageflow_entries, :theme, :index => true
+    add_reference :pageflow_accounts, :default_theme
+
+    execute("UPDATE pageflow_entries SET theme_id = theming_id;")
+    execute("UPDATE pageflow_accounts SET default_theme_id = default_theming_id;")
+
+    remove_reference :pageflow_entries, :theming, :index => true
+    remove_reference :pageflow_accounts, :default_theming
   end
 end
