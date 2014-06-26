91e86d29a3577c9f4f00402f6b4d1ad08a8227b6
Vangelis Tsoumenis <kioopi@gmail.com>
2014-06-26 12:50:52 +0200

    Use teaspoon for javascript testing
    Conflicts:
    	Gemfile.lock

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Dummy::Application.routes.draw do
+  mount Teaspoon::Engine, at: '/teaspoon' if defined? Teaspoon
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   Pageflow.routes(self)
