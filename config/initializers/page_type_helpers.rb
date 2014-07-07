9f989b80190eccfab1edfc5d451fda8fcce81c40
Tim Fischbach <tfischbach@codevise.de>
2014-07-07 17:06:12 +0200

    register page type view helpers with RevisionsController

--- config/initializers/page_type_helpers.rb
+++ config/initializers/page_type_helpers.rb
@@ -2,6 +2,7 @@ Pageflow.after_configure do
   Pageflow.config.page_types.each do |page_type|
     page_type.view_helpers.each do |helper|
       Pageflow::EntriesController.helper(helper)
+      Pageflow::RevisionsController.helper(helper)
     end
   end
 end
