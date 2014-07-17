687f2d0bc5c314326c7d9bee226fa37eee33c66b
Christoph Merkelbach <cmerkelbach@codevise.de>
2014-07-17 16:33:05 +0200

    adds audio loop page type, commit 5664f6b3d401dd6dfcd137454a6277c861206916

---
+++ app/assets/javascripts/pageflow/editor/views/configuration_editors/audio_loop.js
@@ -0,0 +1,23 @@
+pageflow.ConfigurationEditorView.register('audio_loop', {
+  configure: function() {
+    this.tab('general', function() {
+      this.group('general');
+
+      this.input('additional_title', pageflow.TextInputView);
+      this.input('additional_description', pageflow.TextAreaInputView, {size: 'short'});
+    });
+
+    this.tab('files', function() {
+      this.input('audio_file_id', pageflow.FileInputView, {collection: pageflow.audioFiles});
+      this.input('background_image_id', pageflow.FileInputView, {collection: pageflow.imageFiles});
+      this.input('thumbnail_image_id', pageflow.FileInputView, {
+        collection: pageflow.imageFiles,
+        imagePositioning: false
+      });
+    });
+
+    this.tab('options', function() {
+      this.group('options');
+    });
+  }
+});
