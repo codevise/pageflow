70e0a1b3f6f9ee4bcde199ae0a9f786da4a69be0
Tim Fischbach <tfischbach@codevise.de>
2014-08-19 11:19:49 +0200

    Add home button attributes to entries and themings.

---
+++ db/migrate/20140819081454_add_home_url_attributes_to_themings_and_revisions.rb
@@ -0,0 +1,9 @@
+class AddHomeUrlAttributesToThemingsAndRevisions < ActiveRecord::Migration
+  def change
+    add_column :pageflow_themings, :home_url, :string, default: '', null: false
+    add_column :pageflow_themings, :home_button_enabled_by_default, :boolean, default: false, null: false
+
+    add_column :pageflow_revisions, :home_url, :string, default: '', null: false
+    add_column :pageflow_revisions, :home_button_enabled, :boolean, default: false, null: false
+  end
+end
