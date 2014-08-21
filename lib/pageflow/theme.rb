a87817b0e789000582f776976fbc4b6ccf8a3e5e
Tim Fischbach <tfischbach@codevise.de>
2014-08-21 09:47:24 +0200

    allow specifying theme options via Themes#register

--- lib/pageflow/theme.rb
+++ lib/pageflow/theme.rb
@@ -1,14 +1,19 @@
 module Pageflow
   class Theme
-    attr_reader :name, :directory_name
+    attr_reader :name, :directory_name, :options
 
-    def initialize(name)
+    def initialize(name, options = {})
       @name = name.to_s
       @directory_name = name.to_s
+      @options = options
     end
 
     def stylesheet_path
       "pageflow/themes/#{name}.css"
+    end
+
+    def has_home_button?
+      @options[:home_button]
     end
   end
 end
