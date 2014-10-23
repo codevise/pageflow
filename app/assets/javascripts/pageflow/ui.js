5f13a6d8fbfc89064597e75c0b8137b495aa1d95
Tim Fischbach <tfischbach@codevise.de>
2014-10-23 15:51:51 +0200

    Extract pageflow.ui from editor.
    Backbone views and templates for reuse in Backbone apps other than the
    pageflow editor.

---
+++ app/assets/javascripts/pageflow/ui.js
@@ -0,0 +1,16 @@
+//= require_self
+
+//= require wysihtml5
+//= require parser_rules/simple
+
+//= require i18n
+//= require i18n/translations
+
+//= require ./ui/renderer
+//= require ./ui/object
+
+//= require_tree ./ui/views/mixins
+//= require_tree ./ui/views
+//= require_tree ./ui/templates
+
+window.pageflow = window.pageflow || {};
