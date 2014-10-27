4d38188e2710ffe263bcae1a9f58dd8cc071cae5
Tim Fischbach <tfischbach@codevise.de>
2014-10-27 10:12:56 +0100

    Turn admin panels into admin resource tabs

---
+++ app/views/components/pageflow/admin/users_tab.rb
@@ -0,0 +1,22 @@
+module Pageflow
+  module Admin
+    class UsersTab < ViewComponent
+      def build(theming)
+        account = theming.account
+        if account.users.any?
+          table_for account.users, :i18n => User do
+            column :full_name do |user|
+              link_to user.full_name, admin_user_path(user)
+            end
+          end
+        else
+          div :class => "blank_slate_container" do
+            span :class => "blank_slate" do
+              I18n.t('admin.accounts.no_members')
+            end
+          end
+        end
+      end
+    end
+  end
+end
