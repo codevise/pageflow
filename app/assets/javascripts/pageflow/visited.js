32f47b53882e12e2cbabdc883d808e680792a00e
Stefan Schoettelndreyer <sschoettelndreyer@codevise.de>
2014-11-19 17:02:45 +0100

    make pageflow.visited independent from pageflow.pageNavigationList

--- app/assets/javascripts/pageflow/visited.js
+++ app/assets/javascripts/pageflow/visited.js
@@ -1,39 +1,53 @@
-pageflow.Visited = function(links) {
+pageflow.visited = (function() {
 
-  var name = '_pageflow_' + pageflow.entry.id + '_visited';
-  var cookies = pageflow.cookies;
+  var name, cookies = pageflow.cookies;
 
-  if (!cookies.hasItem(name)) {
-    cookies.setItem(name, _getAllIds(), Infinity);
+  $(function() {
+    name = '_pageflow_' + pageflow.entryId + '_visited';
+
+    if (pageflow.visited.enabled) {
+      init();
+    }
+  });
+
+  function init() {
+    if (!cookies.hasItem(name)) {
+      cookies.setItem(name, _getAllIds(), Infinity);
+    }
+
+    pageflow.ready.then(function() {
+      pageflow.slides.on('pageactivate', function (e) {
+        var id = e.target.getAttribute('id');
+        var ids = _getCookieIds();
+
+        if (ids.indexOf(id) < 0) {
+          ids.push(id);
+        }
+
+        cookies.setItem(name, ids, Infinity);
+      });
+    });
   }
 
   function _getCookieIds() {
     if (cookies.hasItem(name) && !!cookies.getItem(name)) {
-      return cookies.getItem(name).split(',');
+      return cookies.getItem(name).split(',').map(function(id) {
+        return parseInt(id, 10);
+      });
     }
     return [];
   }
 
   function _getAllIds() {
-    return links.map(function() {
-      return this.getAttribute('href').substr(1);
-    }).toArray();
+    return pageflow.pages.map(function(page) {
+      return page.perma_id;
+    });
   }
 
   return {
     getUnvisitedPages: function() {
       var visitedIds = _getCookieIds();
       return visitedIds.length ? _.difference(_getAllIds(), visitedIds) : visitedIds;
-    },
-
-    setPageVisited: function(id) {
-      var ids = _getCookieIds();
-
-      if (ids.indexOf(id) < 0) {
-        ids.push(id);
-      }
-
-      cookies.setItem(name, ids, Infinity);
     }
   };
-};
+}());
