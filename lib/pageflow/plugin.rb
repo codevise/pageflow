fb72c14f33dae793a83891f6d8eb7d4fdce9b531
Tim Fischbach <tfischbach@codevise.de>
2015-01-29 14:47:08 +0100

    Add Plugin API to Configuration
    Allow Pageflow extensions to group multiple configuration changes in a
    single method. This allows extensions authors to change the way their
    plugin integrates with Pageflow without changing the Pageflow
    initializer of the host application.

---
+++ lib/pageflow/plugin.rb
@@ -0,0 +1,13 @@
+module Pageflow
+  # Base class for Pageflow extensions which perform more complex
+  # configuration changes.
+  #
+  # @since 0.7
+  class Plugin
+    # Override to configure Pageflow
+    #
+    # @param [Configuration] config
+    def configure(config)
+    end
+  end
+end
