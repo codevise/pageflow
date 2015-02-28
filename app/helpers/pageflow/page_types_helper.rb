acdd7a12f88b60687efb3f974c45be5772fdc430
Tim Fischbach <tfischbach@codevise.de>
2015-02-28 19:02:18 +0100

    Render Editor Page Type Seeds for Enabled Page Types only
    Ensure that only page types that are enabled in the entry's
    configuration are available in the editor.

--- app/helpers/pageflow/page_types_helper.rb
+++ app/helpers/pageflow/page_types_helper.rb
@@ -2,8 +2,10 @@ module Pageflow
   module PageTypesHelper
     include RenderJsonHelper
 
-    def page_type_json_seeds
-      render_json_partial('pageflow/page_types/page_type', :collection => Pageflow.config.page_types, :as => :page_type)
+    def page_type_json_seeds(config)
+      render_json_partial('pageflow/page_types/page_type',
+                          collection: config.page_types,
+                          as: :page_type)
     end
 
     def page_type_templates
