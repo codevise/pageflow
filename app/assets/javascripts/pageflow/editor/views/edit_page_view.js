1177c2e4980b3840f25d1bd5d4229d990e9ce175
Tim Fischbach <tfischbach@codevise.de>
2015-03-02 14:24:32 +0100

    Use new Page Type API to Display Page Type Help

--- app/assets/javascripts/pageflow/editor/views/edit_page_view.js
+++ app/assets/javascripts/pageflow/editor/views/edit_page_view.js
@@ -32,7 +32,8 @@ pageflow.EditPageView = Backbone.Marionette.Layout.extend({
       pictogramClass: 'type_pictogram',
 
       helpLinkClicked: function(value) {
-        pageflow.app.trigger('toggle-help', pageflow.Page.typesByName[value].help_entry_translation_key);
+        var pageType = pageflow.editor.pageTypes.findByName(value);
+        pageflow.app.trigger('toggle-help', pageType.seed.help_entry_translation_key);
       }
     }));
 
