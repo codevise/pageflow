9ae0f0c16f148a28beec7728c36cb365b761ac75
Christoph Merkelbach <cmerkelbach@codevise.de>
2015-03-13 18:14:02 +0100

    refactoring and locale adding according to pageflow wiki, browserchoice taken out (cause its offline)

--- app/assets/javascripts/pageflow/admin/ie_hint.js
+++ app/assets/javascripts/pageflow/admin/ie_hint.js
@@ -1,11 +1,7 @@
 jQuery(function($) {
-  if(navigator.appName.indexOf("Internet Explorer")!=-1){     //yeah, he's using IE
-    var badBrowser=(
-      navigator.appVersion.indexOf("MSIE 1")==-1  //v10, 11, 12, etc. is fine too
-    );
+  var isOldInternetExplorer=(navigator.appVersion.indexOf("MSIE 1") === -1 && navigator.appName.indexOf("Internet Explorer")!==-1);
 
-    if(badBrowser){
-      $('body').append($('<div style="background-color:black; color: white; padding: 20px; left: 50%; top: 50%; margin-left: -150px; margin-top: -200px;width: 300px; font-size: 16px; position: absolute; ">'+ I18n.t('pageflow.entries.ie9_hint.deprecated_browser') + '</div>'));
-    }
+  if(isOldInternetExplorer){
+    $('body').append($('<div style="background-color:black; color: white; padding: 20px; left: 50%; top: 50%; margin-left: -150px; margin-top: -200px;width: 300px; font-size: 16px; position: absolute; ">'+ I18n.t('pageflow.entries.ie9_hint.deprecated_browser') + '</div>'));
   }
 });
