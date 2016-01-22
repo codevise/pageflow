ba1cb1d46239230c51dec73c93ee660241b1d4b8
Tim Fischbach <tfischbach@codevise.de>
2016-01-22 14:01:56 +0100

    Define widget types for player control variants
    Activating a widget type causes a css class to be set on the wrapper
    div. We use this to toggle the player control styles.

--- config/initializers/widget_types.rb
+++ config/initializers/widget_types.rb
@@ -1,4 +1,7 @@
 Pageflow.configure do |config|
   config.widget_types.register(Pageflow::BuiltInWidgetType.navigation, default: true)
   config.widget_types.register(Pageflow::BuiltInWidgetType.mobile_navigation, default: true)
+
+  config.widget_types.register(Pageflow::BuiltInWidgetType.classic_player_controls, default: true)
+  config.widget_types.register(Pageflow::BuiltInWidgetType.slim_player_controls)
 end
