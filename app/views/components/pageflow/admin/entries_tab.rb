d7e676f1126c7ede843ad40ccc681688215391e0
Roman Volkov <rvolkov@codevise.de>
2016-02-01 18:04:17 +0100

    Implement sorting for entries and users tab

--- app/views/components/pageflow/admin/entries_tab.rb
+++ app/views/components/pageflow/admin/entries_tab.rb
@@ -3,11 +3,20 @@ module Pageflow
     class EntriesTab < ViewComponent
       def build(theming)
         account = theming.account
-        embedded_index_table account.entries, blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
-          table_for_collection :class => 'entries', :i18n => Pageflow::Entry do
-            column :title do |entry|
+        embedded_index_table account.entries.order(
+            params[:order] && Entry.column_names.include?(params[:order].gsub('_asc', '').gsub('_desc', '')) ?
+                params[:order].gsub('_asc', ' asc').gsub('_desc', ' desc') : 'title desc'),
+                             blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
+          table_for_collection :sortable => true, :class => 'entries', :i18n => Pageflow::Entry do
+            column :title, :sortable => :title do |entry|
               link_to(entry.title, admin_entry_path(entry))
             end
+            column I18n.t('pageflow.admin.entries.members'), :class => 'members' do |entry|
+              entry_user_badge_list(entry)
+            end
+            column :created_at
+            column :updated_at
+            column :first_published_at
           end
         end
       end
--- app/views/components/pageflow/admin/users_tab.rb
+++ app/views/components/pageflow/admin/users_tab.rb
@@ -3,10 +3,13 @@ module Pageflow
     class UsersTab < ViewComponent
       def build(theming)
         account = theming.account
-        embedded_index_table account.users, blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
-          table_for_collection :class => 'users', :i18n => User do
-            column :full_name do |user|
-              link_to user.full_name, admin_user_path(user)
+        embedded_index_table account.users.order(
+            params[:order] && User.column_names.include?(params[:order].gsub('_asc', '').gsub('_desc', '')) ?
+                params[:order].gsub('_asc', ' asc').gsub('_desc', ' desc') : 'last_name asc'),
+                             blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
+          table_for_collection :sortable => true, :class => 'users', :i18n => User do
+            column :formal_name, :sortable => :last_name do |user|
+              link_to user.formal_name, admin_user_path(user)
             end
           end
         end
