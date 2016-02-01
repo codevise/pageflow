f0e2494a871f452e84add7ed661215f95c11ea62
Roman Volkov <rvolkov@codevise.de>
2016-02-01 03:05:39 +0100

    Use embedded index table for entries, members, users

--- app/views/components/pageflow/admin/entries_tab.rb
+++ app/views/components/pageflow/admin/entries_tab.rb
@@ -3,16 +3,10 @@ module Pageflow
     class EntriesTab < ViewComponent
       def build(theming)
         account = theming.account
-        if account.entries.any?
-          table_for account.entries, :i18n => Pageflow::Entry do
+        embedded_index_table account.entries, blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
+          table_for_collection :class => 'entries', :i18n => Pageflow::Entry do
             column :title do |entry|
               link_to(entry.title, admin_entry_path(entry))
-            end
-          end
-          else
-            div :class => "blank_slate_container" do
-            span :class => "blank_slate" do
-              I18n.t('pageflow.admin.accounts.no_entries')
             end
           end
         end
