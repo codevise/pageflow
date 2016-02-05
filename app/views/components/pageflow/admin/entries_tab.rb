97ace5e2191741cc3ab92a172071e32f90b44dbc
Roman Volkov <rvolkov@codevise.de>
2016-02-05 11:56:32 +0100

    Use embedded index table for entries, members and users
    Implement column sorting as well.

--- app/views/components/pageflow/admin/entries_tab.rb
+++ app/views/components/pageflow/admin/entries_tab.rb
@@ -3,17 +3,20 @@ module Pageflow
     class EntriesTab < ViewComponent
       def build(theming)
         account = theming.account
-        if account.entries.any?
-          table_for account.entries, :i18n => Pageflow::Entry do
-            column :title do |entry|
+        embedded_index_table account.entries.order(
+            params[:order] && Entry.column_names.include?(params[:order].gsub('_asc', '').gsub('_desc', '')) ?
+                params[:order].gsub('_asc', ' asc').gsub('_desc', ' desc') : 'title desc'),
+                             blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
+          table_for_collection sortable: true, class: 'entries', i18n: Pageflow::Entry do
+            column :title, sortable: :title do |entry|
               link_to(entry.title, admin_entry_path(entry))
             end
-          end
-          else
-            div :class => "blank_slate_container" do
-            span :class => "blank_slate" do
-              I18n.t('pageflow.admin.accounts.no_entries')
+            column I18n.t('pageflow.admin.entries.members'), class: 'members' do |entry|
+              entry_user_badge_list(entry)
             end
+            column :created_at
+            column :updated_at
+            column :first_published_at
           end
         end
       end
--- app/views/components/pageflow/admin/members_tab.rb
+++ app/views/components/pageflow/admin/members_tab.rb
@@ -2,31 +2,27 @@ module Pageflow
   module Admin
     class MembersTab < ViewComponent
       def build(entry)
-        if entry.memberships.any?
-          table_for entry.memberships, :class => 'memberships' do
+        embedded_index_table entry.memberships, blank_slate_text: I18n.t('pageflow.admin.entries.no_members') do
+          table_for_collection class: 'memberships', i18n: Pageflow::Membership do
             column t('activerecord.attributes.user.full_name'), class: 'name' do |membership|
               if authorized? :manage, User
-                link_to membership.user.full_name, admin_user_path(membership.user), :class => 'view_creator'
+                link_to membership.user.full_name, admin_user_path(membership.user), class: 'view_creator'
               else
                 membership.user.full_name
               end
             end
             column do |membership|
               if authorized?(:destroy, membership)
-                link_to(I18n.t('pageflow.admin.entries.remove'), admin_entry_membership_path(membership.entry, membership), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation'), :rel => 'delete_membership'})
+                link_to(I18n.t('pageflow.admin.entries.remove'), admin_entry_membership_path(membership.entry, membership),
+                        method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation'), rel: 'delete_membership'})
               end
-            end
-          end
-        else
-          div :class => "blank_slate_container" do
-            span :class => "blank_slate" do
-              I18n.t('pageflow.admin.entries.no_members')
             end
           end
         end
         if authorized? :manage, Pageflow::Entry
           span do
-            link_to I18n.t('pageflow.admin.users.add'), new_admin_entry_membership_path(entry), :class => 'button', :data => {:rel => 'add_member'}
+            link_to I18n.t('pageflow.admin.users.add'), new_admin_entry_membership_path(entry),
+                    class: 'button', data: {rel: 'add_member'}
           end
         end
       end
