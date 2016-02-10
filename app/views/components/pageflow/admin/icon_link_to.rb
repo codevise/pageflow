b7ec0c35b357dc18ce3de705a9ae43fd796c021c
Roman Volkov <rvolkov@codevise.de>
2016-02-10 12:41:32 +0100

    Improve entries table
    * Display publication state
    * Add published since column
    * Use icons instead of buttons in table
    * Use same icons on buttons on show page

---
+++ app/views/components/pageflow/admin/icon_link_to.rb
@@ -0,0 +1,21 @@
+module Pageflow
+  module Admin
+    class IconLinkTo < ViewComponent
+      builder_method :icon_link_to
+
+      def build(path, options = {})
+        text_node(link_to(tooltip_span(options), path, class: options[:class]))
+      end
+
+      def tag_name
+        'span'
+      end
+
+      private
+
+      def tooltip_span(options)
+        content_tag(:span, options[:tooltip], class: 'tooltip_bubble')
+      end
+    end
+  end
+end
