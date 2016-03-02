3f0eee1e492e15601d2f70435a6b372a06196579
Tim Fischbach <tfischbach@codevise.de>
2016-03-02 11:32:14 +0100

    Rewrite InvitedUser
    Devise 3.1 no longer stores the unencrypted reset token. We need to
    pass the token manually to the mailer.

--- app/mailers/pageflow/user_mailer.rb
+++ app/mailers/pageflow/user_mailer.rb
@@ -4,9 +4,11 @@ module Pageflow
 
     def invitation(options)
       @user = User.find(options['user_id'])
+      @password_token = options['password_token']
+
       I18n.with_locale(@user.locale) do
         headers('X-Language' => I18n.locale)
-        mail(:to => @user.email, :subject => t('.subject'), :from => Pageflow.config.mailer_sender)
+        mail(to: @user.email, subject: t('.subject'), from: Pageflow.config.mailer_sender)
       end
     end
   end
