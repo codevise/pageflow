8ff4086f1b39f5cf91aa669d81cc824d3839fd99
Tim Fischbach <tfischbach@codevise.de>
2016-03-02 10:33:50 +0100

    Fix attributes table patch
    Attribute tables can now handle multiple resources.

--- lib/active_admin/patches/views/attributes_table.rb
+++ lib/active_admin/patches/views/attributes_table.rb
@@ -1,8 +1,13 @@
 class ActiveAdmin::Views::AttributesTable
-  def status_tag_row(name, texts, states)
-    value = @record.send(name)
+  STATUS_TAG_ROW_CLASS = lambda do |record|
+    value = record.send(name)
+    [name.to_s.gsub(/\?$/, ''), value ? 'yes' : 'no'] * ' '
+  end
 
-    row name, :class => [name.to_s.gsub(/\?$/, ''), value ? 'yes' : 'no'] * ' ' do
+  def status_tag_row(name, texts, states)
+    row(name, class: STATUS_TAG_ROW_CLASS) do |record|
+      value = record.send(name)
+
       text = value ? texts.first : texts.last
       state = value ? states.first : states.last
 
@@ -15,6 +20,8 @@ class ActiveAdmin::Views::AttributesTable
   end
 
   def boolean_status_tag_row(name, yes_state = :warning)
-    status_tag_row(name, ['Ja', '-'], [yes_state, nil])
+    status_tag_row(name,
+                   [I18n.t('active_admin.status_tag.yes'), '-'],
+                   [yes_state, nil])
   end
 end
