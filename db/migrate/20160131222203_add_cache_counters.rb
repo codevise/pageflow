b0c2b2c1294dbc89808b9cb35c12e35aebf7f505
Tim Fischbach <tfischbach@codevise.de>
2016-03-10 11:41:57 +0100

    Filtering and additional columns for account admin page

---
+++ db/migrate/20160131222203_add_cache_counters.rb
@@ -0,0 +1,23 @@
+class AddCacheCounters < ActiveRecord::Migration
+  def self.up
+    add_column :pageflow_accounts, :users_count, :integer, default: 0, null: false
+    add_column :pageflow_accounts, :entries_count, :integer, default: 0, null: false
+
+    execute(<<-SQL)
+      UPDATE pageflow_accounts SET users_count = (
+        SELECT COUNT(*)
+        FROM users
+        WHERE users.account_id = pageflow_accounts.id
+      ), entries_count = (
+        SELECT COUNT(*)
+        FROM pageflow_entries
+        WHERE pageflow_entries.account_id = pageflow_accounts.id
+      );
+    SQL
+  end
+
+  def self.down
+    remove_column :pageflow_accounts, :users_count
+    remove_column :pageflow_accounts, :entries_count
+  end
+end
