eccaef7d72c682b406ef5af70cd94fd539660ad8
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-03-14 17:43:19 +0100

    Add preview revision functionality

--- spec/features/editor/viewing_entry_revisions_spec.rb
+++ spec/features/editor/viewing_entry_revisions_spec.rb
@@ -2,14 +2,13 @@ require 'spec_helper'
 
 feature 'viewing entry revisions' do
   scenario 'view depublished revision' do
-    entry = create(:entry)
-    revision = create(:revision, :depublished, :entry => entry)
-    storyline = create(:storyline, :revision => revision)
-    chapter = create(:chapter, :storyline => storyline)
-    create(:page, :chapter => chapter, :template => 'background_image')
-
     user = Dom::Admin::Page.sign_in_as(:editor)
-    create(:membership, :entry => revision.entry, :user => user)
+
+    entry = create(:entry, with_previewer: user)
+    revision = create(:revision, :depublished, entry: entry)
+    storyline = create(:storyline, revision: revision)
+    chapter = create(:chapter, storyline: storyline)
+    create(:page, chapter: chapter, template: 'background_image')
 
     visit(admin_entry_path(entry))
     Dom::Admin::EntryRevision.first.show_link.click
