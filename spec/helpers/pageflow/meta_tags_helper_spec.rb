12f01e0e1b149bdd65272d49c948235e042c8e45
Joost Baaij <joost@spacebabies.nl>
2016-03-31 14:14:38 +0200

    busywork to keep Hound happy

--- spec/helpers/pageflow/meta_tags_helper_spec.rb
+++ spec/helpers/pageflow/meta_tags_helper_spec.rb
@@ -2,22 +2,34 @@ require 'spec_helper'
 
 module Pageflow
   RSpec.describe MetaTagsHelper, type: :helper do
-    let(:entry) { create(:entry, :published)}
-    let(:published_entry) { PublishedEntry.new(entry)}
+    let(:entry) { create(:entry, :published) }
+    let(:published_entry) { PublishedEntry.new(entry) }
 
     it 'renders default keywords from configuration' do
       html = helper.meta_tags_for_entry(published_entry)
-      expect(html).to have_css(%Q{meta[content="pageflow, multimedia, reportage"][name="keywords"]}, visible: false, count: 1)
+      expect(html).to have_css(
+        %{meta[content="pageflow, multimedia, reportage"][name="keywords"]},
+        visible: false,
+        count: 1
+      )
     end
 
     it 'renders default author from configuration' do
       html = helper.meta_tags_for_entry(published_entry)
-      expect(html).to have_css(%Q{meta[content="Pageflow"][name="author"]}, visible: false, count: 1)
+      expect(html).to have_css(
+        %{meta[content="Pageflow"][name="author"]},
+        visible: false,
+        count: 1
+      )
     end
 
     it 'renders default publisher from configuration' do
       html = helper.meta_tags_for_entry(published_entry)
-      expect(html).to have_css(%Q{meta[content="Pageflow"][name="publisher"]}, visible: false, count: 1)
+      expect(html).to have_css(
+        %{meta[content="Pageflow"][name="publisher"]},
+        visible: false,
+        count: 1
+      )
     end
   end
 end
