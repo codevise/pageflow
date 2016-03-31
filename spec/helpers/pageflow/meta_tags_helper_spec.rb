263205acc44f24cae3de3e0aa5158ac075209574
Joost Baaij <joost@spacebabies.nl>
2016-04-01 01:32:25 +0200

    add test case for override config

--- spec/helpers/pageflow/meta_tags_helper_spec.rb
+++ spec/helpers/pageflow/meta_tags_helper_spec.rb
@@ -5,8 +5,9 @@ module Pageflow
     let(:entry) { create(:entry, :published) }
     let(:published_entry) { PublishedEntry.new(entry) }
 
+    let(:html) { helper.meta_tags_for_entry(published_entry) }
+
     it 'renders default keywords from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
       expect(html).to have_css(
         %{meta[content="pageflow, multimedia, reportage"][name="keywords"]},
         visible: false,
@@ -14,8 +15,17 @@ module Pageflow
       )
     end
 
+    it 'allows keywords being set from configuration' do
+      Pageflow.configure { |config| config.default_keywords_meta_tag = 'story, environment' }
+
+      expect(html).to have_css(
+        %{meta[content="story, environment"][name="keywords"]},
+        visible: false,
+        count: 1
+      )
+    end
+
     it 'renders default author from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
       expect(html).to have_css(
         %{meta[content="Pageflow"][name="author"]},
         visible: false,
@@ -23,10 +33,29 @@ module Pageflow
       )
     end
 
+    it 'allows author being set from configuration' do
+      Pageflow.configure { |config| config.default_author_meta_tag = 'Acme, Inc.' }
+
+      expect(html).to have_css(
+        %{meta[content="Acme, Inc."][name="author"]},
+        visible: false,
+        count: 1
+      )
+    end
+
     it 'renders default publisher from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
       expect(html).to have_css(
         %{meta[content="Pageflow"][name="publisher"]},
+        visible: false,
+        count: 1
+      )
+    end
+
+    it 'allows publisher being set from configuration' do
+      Pageflow.configure { |config| config.default_publisher_meta_tag = 'Acme, Inc.' }
+
+      expect(html).to have_css(
+        %{meta[content="Acme, Inc."][name="publisher"]},
         visible: false,
         count: 1
       )
