8d95d4dbde6472025bc17df30b79ea88c86cc643
Joost Baaij <joost@spacebabies.nl>
2016-04-04 16:07:43 +0200

    add test cases for revision override meta

--- spec/helpers/pageflow/meta_tags_helper_spec.rb
+++ spec/helpers/pageflow/meta_tags_helper_spec.rb
@@ -5,70 +5,98 @@ module Pageflow
     let(:entry) { create(:entry, :published) }
     let(:published_entry) { PublishedEntry.new(entry) }
 
-    it 'renders default keywords from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
-
-      expect(html).to have_css(
-        %{meta[content="pageflow, multimedia, reportage"][name="keywords"]},
-        visible: false,
-        count: 1
-      )
+    def html
+      helper.meta_tags_for_entry(published_entry)
     end
 
-    it 'allows default keywords being set from configuration' do
-      pageflow_configure { |config| config.default_keywords_meta_tag = 'story, environment' }
+    describe 'keywords' do
+      it 'has default value from configuration' do
+        expect(html).to have_css(
+          %{meta[content="pageflow, multimedia, reportage"][name="keywords"]},
+          visible: false,
+          count: 1
+        )
+      end
 
-      html = helper.meta_tags_for_entry(published_entry)
+      it 'can have default set in configuration' do
+        pageflow_configure { |config| config.default_keywords_meta_tag = 'story, environment' }
 
-      expect(html).to have_css(
-        %{meta[content="story, environment"][name="keywords"]},
-        visible: false,
-        count: 1
-      )
+        expect(html).to have_css(
+          %{meta[content="story, environment"][name="keywords"]},
+          visible: false,
+          count: 1
+        )
+      end
+
+      it 'can be overriden per revision' do
+        entry.published_revision.update! keywords: 'longform, journalism'
+
+        expect(html).to have_css(
+          %{meta[content="longform, journalism"][name="keywords"]},
+          visible: false,
+          count: 1
+        )
+      end
     end
 
-    it 'renders default author from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
+    describe 'author' do
+      it 'has default value from configuration' do
+        expect(html).to have_css(
+          %{meta[content="Pageflow"][name="author"]},
+          visible: false,
+          count: 1
+        )
+      end
 
-      expect(html).to have_css(
-        %{meta[content="Pageflow"][name="author"]},
-        visible: false,
-        count: 1
-      )
+      it 'can have default set in configuration' do
+        pageflow_configure { |config| config.default_author_meta_tag = 'Prof. Dr. Sahra Isak' }
+
+        expect(html).to have_css(
+          %{meta[content="Prof. Dr. Sahra Isak"][name="author"]},
+          visible: false,
+          count: 1
+        )
+      end
+
+      it 'can be overriden per revision' do
+        entry.published_revision.update! author: 'Henri zu Walter'
+
+        expect(html).to have_css(
+          %{meta[content="Henri zu Walter"][name="author"]},
+          visible: false,
+          count: 1
+        )
+      end
     end
 
-    it 'allows author being set from configuration' do
-      pageflow_configure { |config| config.default_author_meta_tag = 'Acme, Inc.' }
+    describe 'publisher' do
+      it 'has default value from configuration' do
+        expect(html).to have_css(
+          %{meta[content="Pageflow"][name="publisher"]},
+          visible: false,
+          count: 1
+        )
+      end
 
-      html = helper.meta_tags_for_entry(published_entry)
+      it 'can have default set in configuration' do
+        pageflow_configure { |config| config.default_publisher_meta_tag = 'Kempe-Heuck' }
 
-      expect(html).to have_css(
-        %{meta[content="Acme, Inc."][name="author"]},
-        visible: false,
-        count: 1
-      )
-    end
+        expect(html).to have_css(
+          %{meta[content="Kempe-Heuck"][name="publisher"]},
+          visible: false,
+          count: 1
+        )
+      end
 
-    it 'renders default publisher from configuration' do
-      html = helper.meta_tags_for_entry(published_entry)
+      it 'can be overriden per revision' do
+        entry.published_revision.update! publisher: 'Adams, Kähler und Tafelmeier'
 
-      expect(html).to have_css(
-        %{meta[content="Pageflow"][name="publisher"]},
-        visible: false,
-        count: 1
-      )
-    end
-
-    it 'allows publisher being set from configuration' do
-      pageflow_configure { |config| config.default_publisher_meta_tag = 'Acme, Inc.' }
-
-      html = helper.meta_tags_for_entry(published_entry)
-
-      expect(html).to have_css(
-        %{meta[content="Acme, Inc."][name="publisher"]},
-        visible: false,
-        count: 1
-      )
+        expect(html).to have_css(
+          %{meta[content="Adams, Kähler und Tafelmeier"][name="publisher"]},
+          visible: false,
+          count: 1
+        )
+      end
     end
   end
 end
