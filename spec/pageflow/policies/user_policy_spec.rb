885d45edaf8efe4eab414541a6a5033fabfae0d2
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-04-26 18:56:09 +0200

    Fix indexing Memberships
    Also test UserPolicy

--- spec/pageflow/policies/user_policy_spec.rb
+++ spec/pageflow/policies/user_policy_spec.rb
@@ -9,6 +9,94 @@ module Pageflow
                       of_account: -> (topic) { topic.membership_accounts.first },
                       to: :read,
                       topic: -> { create(:user, :member, on: create(:account)) }
+
+      it_behaves_like 'a membership-based permission that',
+                      allows: 'manager',
+                      but_forbids: 'publisher',
+                      of_account: -> (topic) { topic.membership_accounts.first },
+                      to: :redirect_to_user,
+                      topic: -> { create(:user, :member, on: create(:account)) }
+
+      describe 'index_users?' do
+        it 'allows user with manager permissions on account to index users' do
+          user = create(:user, :manager, on: create(:account))
+
+          policy = UserPolicy.new(user, create(:user))
+
+          expect(policy).to permit_action(:index_users)
+        end
+
+        it 'does not allow user with publisher permissions on account to index users' do
+          user = create(:user, :publisher, on: create(:account))
+
+          policy = UserPolicy.new(user, create(:user))
+
+          expect(policy).not_to permit_action(:index_users)
+        end
+
+        it 'allows user with manager permissions on entry to index users' do
+          user = create(:user, :manager, on: create(:entry))
+
+          policy = UserPolicy.new(user, create(:user))
+
+          expect(policy).not_to permit_action(:index_users)
+        end
+      end
+
+      describe '.resolve' do
+        it 'includes all users if admin' do
+          admin = create(:user, :admin)
+
+          expect(Policies::UserPolicy::Scope.new(admin, ::User).resolve).to include(create(:user))
+        end
+
+        it 'includes member on managed account' do
+          account_manager = create(:user)
+          managed_user = create(:user)
+          create(:account, with_member: managed_user, with_manager: account_manager)
+
+          expect(Policies::UserPolicy::Scope
+                  .new(account_manager, ::User).resolve).to include(managed_user)
+        end
+
+        it 'does not include member on publisher account' do
+          account_publisher = create(:user)
+          managed_user = create(:user)
+          create(:account, with_member: managed_user, with_publisher: account_publisher)
+
+          expect(Policies::UserPolicy::Scope
+                  .new(account_publisher, ::User).resolve).not_to include(managed_user)
+        end
+
+        it 'does not include member on managed entry' do
+          account_manager = create(:user)
+          managed_user = create(:user)
+          create(:entry, with_member: managed_user, with_manager: account_manager)
+
+          expect(Policies::UserPolicy::Scope
+                  .new(account_manager, ::User).resolve).not_to include(managed_user)
+        end
+
+        it 'does not include member on other account' do
+          account_manager = create(:user)
+          managed_user = create(:user)
+          create(:account, with_manager: account_manager)
+          create(:account, with_member: managed_user)
+
+          expect(Policies::UserPolicy::Scope
+                  .new(account_manager, ::User).resolve).not_to include(managed_user)
+        end
+
+        it 'does not include user with nil id' do
+          account_manager = create(:user)
+          managed_user = User.new
+          create(:account, with_manager: account_manager)
+          create(:account, with_member: managed_user)
+
+          expect(Policies::UserPolicy::Scope
+                  .new(account_manager, ::User).resolve).not_to include(managed_user)
+        end
+      end
     end
   end
 end
