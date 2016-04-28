c3c7eca597b8a6ab2221fa1bf25cb533cf385fda
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-04-28 12:48:45 +0200

    Clean up User admin formatting

--- admins/pageflow/user.rb
+++ admins/pageflow/user.rb
@@ -23,15 +23,17 @@ module Pageflow
     filter :email
 
     action_item(:invite, only: :index) do
-      link_to I18n.t('pageflow.admin.users.invite_user'), new_admin_user_path, :data => {:rel => 'invite_user'}
+      link_to I18n.t('pageflow.admin.users.invite_user'),
+              new_admin_user_path,
+              data: {rel: 'invite_user'}
     end
 
     show do |user|
       div do
         attributes_table_for user do
-          row :last_name, :class => 'last_name'
-          row :first_name, :class => 'first_name'
-          row :email, :class => 'email'
+          row :last_name, class: 'last_name'
+          row :first_name, class: 'first_name'
+          row :email, class: 'email'
 
           row :created_at
           row :last_sign_in_at
@@ -40,42 +42,47 @@ module Pageflow
             I18n.t('language', locale: user.locale)
           end
           if user.role.to_sym == :admin
-            row :role, :class => 'role' do
+            row :role, class: 'role' do
               span 'data-user-role' => user.role do
-                I18n.t(user.role, :scope => 'pageflow.admin.users.roles')
+                I18n.t(user.role, scope: 'pageflow.admin.users.roles')
               end
             end
           end
         end
 
         para do
-          link_to I18n.t('pageflow.admin.users.resend_invitation'), resend_invitation_admin_user_path(user), :method => :post, :class => 'button', :data => {:rel => 'resend_invitation'}
+          link_to I18n.t('pageflow.admin.users.resend_invitation'),
+                  resend_invitation_admin_user_path(user),
+                  method: :post, class: 'button', data: {rel: 'resend_invitation'}
         end
 
-        entry_memberships = user.memberships.on_entries.accessible_by(Ability.new(current_user), :index)
+        entry_memberships = user.memberships.on_entries
+                            .accessible_by(Ability.new(current_user), :index)
         if entry_memberships.any?
           panel I18n.t('activerecord.models.entry.other') do
-            table_for entry_memberships, :class => 'memberships', :i18n => Membership do
+            table_for entry_memberships, class: 'memberships', i18n: Membership do
               column :entry do |membership|
                 link_to(membership.entry.title, admin_entry_path(membership.entry))
               end
               column :role, sortable: 'pageflow_memberships.role' do |membership|
                 span class: "membership_role #{membership.role}" do
-                  I18n.t(membership.role, :scope => 'activerecord.values.pageflow/membership.role')
+                  I18n.t(membership.role, scope: 'activerecord.values.pageflow/membership.role')
                 end
               end
               column I18n.t('activerecord.models.account.one'), :account do |membership|
-                link_to(membership.entity.account.name, admin_account_path(membership.entity.account))
+                link_to(membership.entity.account.name,
+                        admin_account_path(membership.entity.account))
               end
               column :created_at, sortable: 'pageflow_memberships.created_at'
             end
           end
         end
 
-        account_memberships = user.memberships.on_accounts.accessible_by(Ability.new(current_user), :index)
+        account_memberships = user.memberships.on_accounts
+                              .accessible_by(Ability.new(current_user), :index)
         if account_memberships.any?
           panel I18n.t('activerecord.models.account.other') do
-            table_for account_memberships, :class => 'memberships', :i18n => Membership do
+            table_for account_memberships, class: 'memberships', i18n: Membership do
               if authorized?(:list_memberships_on, Account)
               end
               column I18n.t('activerecord.models.account.one'), :account do |membership|
@@ -87,7 +94,7 @@ module Pageflow
               end
               column :role, sortable: 'pageflow_memberships.role' do |membership|
                 span class: "membership_role #{membership.role}" do
-                  I18n.t(membership.role, :scope => 'activerecord.values.pageflow/membership.role')
+                  I18n.t(membership.role, scope: 'activerecord.values.pageflow/membership.role')
                 end
               end
               column :created_at, sortable: 'pageflow_memberships.created_at'
@@ -135,58 +142,69 @@ module Pageflow
     end
 
     action_item(:edit, only: :show) do
-      link_to I18n.t('pageflow.admin.users.edit'), edit_admin_user_path(user), :data => {:rel => 'edit_user'}
+      link_to I18n.t('pageflow.admin.users.edit'),
+              edit_admin_user_path(user),
+              data: {rel: 'edit_user'}
     end
 
     action_item(:toggle_suspended, only: :show) do
       if user != current_user
         if user.suspended?
-          link_to I18n.t('pageflow.admin.users.unsuspend'), unsuspend_admin_user_path(user), :method => :post, :data => {:rel => 'unsuspend_user'}
+          link_to I18n.t('pageflow.admin.users.unsuspend'),
+                  unsuspend_admin_user_path(user),
+                  method: :post, data: {rel: 'unsuspend_user'}
         else
-          link_to I18n.t('pageflow.admin.users.suspend'), suspend_admin_user_path(user), :method => :post, :data => {:rel => 'suspend_user'}
+          link_to I18n.t('pageflow.admin.users.suspend'),
+                  suspend_admin_user_path(user),
+                  method: :post, data: {rel: 'suspend_user'}
         end
       end
     end
 
     action_item(:delete, only: :show) do
       if user != current_user
-        link_to I18n.t('pageflow.admin.users.delete'), admin_user_path(user), :method => :delete, :data => {:rel => 'delete_user', :confirm => I18n.t('pageflow.admin.users.confirm_delete')}
+        link_to I18n.t('pageflow.admin.users.delete'),
+                admin_user_path(user),
+                method: :delete,
+                data: {rel: 'delete_user',
+                       confirm: I18n.t('pageflow.admin.users.confirm_delete')}
       end
     end
 
-    form(:partial => 'form')
+    form(partial: 'form')
 
-    collection_action 'me', :title => I18n.t('pageflow.admin.users.account'), :method => [:get, :patch] do
+    collection_action 'me', title: I18n.t('pageflow.admin.users.account'), method: [:get, :patch] do
       if request.patch?
         if current_user.update_with_password(user_profile_params)
-          sign_in current_user, :bypass => true
-          redirect_to admin_root_path, :notice => I18n.t('pageflow.admin.users.me.updated')
+          sign_in current_user, bypass: true
+          redirect_to admin_root_path, notice: I18n.t('pageflow.admin.users.me.updated')
         end
       end
     end
 
-    collection_action 'delete_me', :title => I18n.t('pageflow.admin.users.account'), :method => [:get, :delete] do
+    collection_action 'delete_me',
+                      title: I18n.t('pageflow.admin.users.account'), method: [:get, :delete] do
       if request.delete?
         if authorized?(:delete_own_user, current_user) &&
            current_user.destroy_with_password(params.require(:user)[:current_password])
-          redirect_to admin_root_path, :notice => I18n.t('pageflow.admin.users.me.updated')
+          redirect_to admin_root_path, notice: I18n.t('pageflow.admin.users.me.updated')
         end
       end
     end
 
-    member_action :resend_invitation, :method => :post do
+    member_action :resend_invitation, method: :post do
       InvitedUser.find(params[:id]).send_invitation!
-      redirect_to :back, :notice => I18n.t('pageflow.admin.users.resent_invitation')
+      redirect_to :back, notice: I18n.t('pageflow.admin.users.resent_invitation')
     end
 
-    member_action :suspend, :method => :post do
+    member_action :suspend, method: :post do
       User.find(params[:id]).suspend!
-      redirect_to :back, :notice => I18n.t('pageflow.admin.users.suspended')
+      redirect_to :back, notice: I18n.t('pageflow.admin.users.suspended')
     end
 
-    member_action :unsuspend, :method => :post do
+    member_action :unsuspend, method: :post do
       User.find(params[:id]).unsuspend!
-      redirect_to :back, :notice => I18n.t('pageflow.admin.users.unsuspended')
+      redirect_to :back, notice: I18n.t('pageflow.admin.users.unsuspended')
     end
 
     controller do
@@ -209,18 +227,30 @@ module Pageflow
       end
 
       def user_profile_params
-        params.require(:user).permit(:first_name, :last_name, :current_password, :password, :password_confirmation, :locale)
+        params.require(:user).permit(:first_name,
+                                     :last_name,
+                                     :current_password,
+                                     :password,
+                                     :password_confirmation,
+                                     :locale)
       end
 
       def permitted_params
-        result = params.permit(:user => [:first_name, :last_name, :email, :password, :password_confirmation, :account_id, :role, :locale])
+        result = params.permit(user: [:first_name,
+                                      :last_name,
+                                      :email,
+                                      :password,
+                                      :password_confirmation,
+                                      :account_id,
+                                      :role,
+                                      :locale])
         restrict_attributes(params[:id], result[:user]) if result[:user]
         result
       end
 
       private
 
-      def restrict_attributes(id, attributes)
+      def restrict_attributes(_id, attributes)
         if !authorized?(:read, Account)
           attributes.delete(:account_id)
         end
