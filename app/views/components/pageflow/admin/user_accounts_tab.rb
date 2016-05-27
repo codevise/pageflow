4a7419e19788ec78a4abae94c717e09265e8e93d
Tim Fischbach <tfischbach@codevise.de>
2016-05-27 10:22:54 +0200

    Warn that deleting account membership deletes entry memberships

--- app/views/components/pageflow/admin/user_accounts_tab.rb
+++ app/views/components/pageflow/admin/user_accounts_tab.rb
@@ -4,7 +4,7 @@ module Pageflow
       def build(user)
         embedded_index_table(user.memberships.on_accounts.includes(:account)
                               .accessible_by(Ability.new(current_user), :index),
-                             blank_slate_text: I18n.t('pageflow.admin.users.no_accounts')) do
+                             blank_slate_text: t('pageflow.admin.users.no_accounts')) do
           table_for_collection class: 'memberships', sortable: true, i18n: Pageflow::Membership do
             column :account, sortable: 'pageflow_accounts.name' do |membership|
               if authorized?(:read, membership.entity)
@@ -14,17 +14,16 @@ module Pageflow
               end
             end
             column :role, sortable: 'pageflow_memberships.role' do |membership|
-              span I18n.t(membership.role, scope: 'activerecord.values.pageflow/membership.role'),
+              span t(membership.role, scope: 'activerecord.values.pageflow/membership.role'),
                    class: "membership_role #{membership.role} tooltip_clue" do
-                div I18n.t(membership.role,
-                           scope: 'pageflow.admin.users.roles.accounts.tooltip'),
+                div t(membership.role, scope: 'pageflow.admin.users.roles.accounts.tooltip'),
                     class: 'tooltip_bubble'
               end
             end
             column :created_at, sortable: 'pageflow_memberships.created_at'
             column do |membership|
               if authorized?(:update, membership)
-                link_to(I18n.t('pageflow.admin.users.edit_role'),
+                link_to(t('pageflow.admin.users.edit_role'),
                         edit_admin_user_membership_path(user, membership, entity_type: :account),
                         data: {
                           rel: "edit_account_role_#{membership.role}"
@@ -33,11 +32,11 @@ module Pageflow
             end
             column do |membership|
               if authorized?(:destroy, membership)
-                link_to(I18n.t('pageflow.admin.users.delete'),
+                link_to(t('pageflow.admin.users.delete'),
                         admin_user_membership_path(user, membership),
                         method: :delete,
                         data: {
-                          confirm: I18n.t('active_admin.delete_confirmation'),
+                          confirm: t('pageflow.admin.users.delete_account_membership_confirmation'),
                           rel: "delete_account_membership_#{membership.role}"
                         })
               end
--- app/views/components/pageflow/admin/user_entries_tab.rb
+++ app/views/components/pageflow/admin/user_entries_tab.rb
@@ -4,16 +4,15 @@ module Pageflow
       def build(user)
         embedded_index_table(user.memberships.on_entries.includes(:entry, entry: :account)
                               .accessible_by(Ability.new(current_user), :index),
-                             blank_slate_text: I18n.t('pageflow.admin.users.no_entries')) do
+                             blank_slate_text: t('pageflow.admin.users.no_entries')) do
           table_for_collection class: 'memberships', sortable: true, i18n: Pageflow::Membership do
             column :entry, sortable: 'pageflow_entries.title' do |membership|
               link_to(membership.entity.title, admin_entry_path(membership.entity))
             end
             column :role, sortable: 'pageflow_memberships.role' do |membership|
-              span I18n.t(membership.role, scope: 'activerecord.values.pageflow/membership.role'),
+              span t(membership.role, scope: 'activerecord.values.pageflow/membership.role'),
                    class: "membership_role #{membership.role} tooltip_clue" do
-                div I18n.t(membership.role,
-                           scope: 'pageflow.admin.users.roles.entries.tooltip'),
+                div t(membership.role, scope: 'pageflow.admin.users.roles.entries.tooltip'),
                     class: 'tooltip_bubble'
               end
             end
@@ -28,7 +27,7 @@ module Pageflow
             column :created_at, sortable: 'pageflow_memberships.created_at'
             column do |membership|
               if authorized?(:update, membership)
-                link_to(I18n.t('pageflow.admin.users.edit_role'),
+                link_to(t('pageflow.admin.users.edit_role'),
                         edit_admin_user_membership_path(user, membership, entity_type: :entry),
                         data: {
                           rel: "edit_entry_role_#{membership.role}"
@@ -37,11 +36,11 @@ module Pageflow
             end
             column do |membership|
               if authorized?(:destroy, membership)
-                link_to(I18n.t('pageflow.admin.users.delete'),
+                link_to(t('pageflow.admin.users.delete'),
                         admin_user_membership_path(user, membership),
                         method: :delete,
                         data: {
-                          confirm: I18n.t('active_admin.delete_confirmation'),
+                          confirm: t('pageflow.admin.users.delete_entry_membership_confirmation'),
                           rel: "delete_entry_membership_#{membership.role}"
                         })
               end
