bc1538f9e1ce845b701420634e493e91f7047179
Tim Fischbach <tfischbach@codevise.de>
2016-08-22 10:54:00 +0200

    Remove test config for integration specs
    There are no specs with an integration tag.

--- spec/support/config/timecop.rb
+++ spec/support/config/timecop.rb
@@ -5,14 +5,9 @@ RSpec.configure do |config|
     Timecop.freeze(Time.local(2013))
   end
 
-  config.before(:each, :capybara_feature => true) do
+  config.before(:each, capybara_feature: true) do
     # Do not freeze time in capybara tests to enable waiting for elements
     Timecop.travel(Time.local(2013))
-  end
-
-  config.before(:each, :integration => true) do
-    # Do not freeze time in integration tests
-    Timecop.return
   end
 
   config.after(:each) do
