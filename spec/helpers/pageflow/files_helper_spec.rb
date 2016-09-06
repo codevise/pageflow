c5ed65bafe6282d85f0dd7cd7a5920387ea6f425
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-09-06 11:54:38 +0200

    Improve files helper specs
    Test whether custom partials are really used. Reduce volume of tests for
    specific fields on file types and nested file types.

--- spec/helpers/pageflow/files_helper_spec.rb
+++ spec/helpers/pageflow/files_helper_spec.rb
@@ -45,7 +45,19 @@ module Pageflow
     end
 
     describe '#files_json_seeds' do
-      it 'has keys of all file types' do
+      let(:page_type_class) do
+        Class.new(PageType) do
+          name 'test'
+
+          def initialize(options)
+            @file_types = options.fetch(:file_types)
+          end
+
+          attr_reader :file_types
+        end
+      end
+
+      it 'has keys for all built-in file types' do
         revision = create(:revision, :published)
         entry = create(:entry, published_revision: revision)
         published_entry = PublishedEntry.new(entry)
@@ -57,31 +69,7 @@ module Pageflow
         expect(files_seed).to have_key('audio_files')
       end
 
-      it 'seeds required data for an image file' do
-        revision = create(:revision, :published)
-        entry = create(:entry, published_revision: revision)
-        published_entry = PublishedEntry.new(entry)
-        image_file = create(:image_file)
-        create(:file_usage, revision: revision, file: image_file)
-
-        files_seed = JSON.parse(helper.files_json_seeds(published_entry))
-        image_file_seed = files_seed['image_files'].first
-
-        expect(image_file_seed).to have_key('id')
-        expect(image_file_seed).to have_key('state')
-        expect(image_file_seed).to have_key('rights')
-        expect(image_file_seed).to have_key('usage_id')
-        expect(image_file_seed).to have_key('retryable')
-        expect(image_file_seed).to have_key('file_name')
-        expect(image_file_seed).to have_key('width')
-        expect(image_file_seed).to have_key('height')
-        expect(image_file_seed).to have_key('panorama_url')
-        expect(image_file_seed).to have_key('dimensions')
-        expect(image_file_seed).to have_key('thumbnail_url')
-        expect(image_file_seed).to have_key('link_thumbnail_url')
-      end
-
-      it 'seeds required data for a video file' do
+      it 'seeds required data, in this example for a video file' do
         revision = create(:revision, :published)
         entry = create(:entry, published_revision: revision)
         published_entry = PublishedEntry.new(entry)
@@ -92,56 +80,26 @@ module Pageflow
         video_file_seed = files_seed['video_files'].first
 
         expect(video_file_seed).to have_key('id')
-        expect(video_file_seed).to have_key('dimensions')
-        expect(video_file_seed).to have_key('duration')
-        expect(video_file_seed).to have_key('encoding_progress')
         expect(video_file_seed).to have_key('file_name')
-        expect(video_file_seed).to have_key('format')
-        expect(video_file_seed).to have_key('height')
         expect(video_file_seed).to have_key('link_thumbnail_url')
         expect(video_file_seed).to have_key('original_url')
         expect(video_file_seed).to have_key('retryable')
         expect(video_file_seed).to have_key('rights')
-        expect(video_file_seed).to have_key('sources')
         expect(video_file_seed).to have_key('state')
         expect(video_file_seed).to have_key('thumbnail_url')
         expect(video_file_seed).to have_key('url')
         expect(video_file_seed).to have_key('usage_id')
-        expect(video_file_seed).to have_key('width')
-        expect(video_file_seed).to have_key('text_track_file')
+        expect(video_file_seed).to have_key('nested_files')
+        expect(video_file_seed).to have_key('height')
       end
 
-      it 'seeds required data for an audio file' do
+      it 'seeds required data, in this example for a nested text track file' do
         revision = create(:revision, :published)
         entry = create(:entry, published_revision: revision)
         published_entry = PublishedEntry.new(entry)
-        audio_file = create(:audio_file)
+        audio_file = create(:audio_file, used_in: entry.draft)
         create(:file_usage, revision: revision, file: audio_file)
-
-        files_seed = JSON.parse(helper.files_json_seeds(published_entry))
-        audio_file_seed = files_seed['audio_files'].first
-
-        expect(audio_file_seed).to have_key('id')
-        expect(audio_file_seed).to have_key('duration')
-        expect(audio_file_seed).to have_key('encoding_progress')
-        expect(audio_file_seed).to have_key('file_name')
-        expect(audio_file_seed).to have_key('format')
-        expect(audio_file_seed).to have_key('original_url')
-        expect(audio_file_seed).to have_key('retryable')
-        expect(audio_file_seed).to have_key('rights')
-        expect(audio_file_seed).to have_key('state')
-        expect(audio_file_seed).to have_key('url')
-        expect(audio_file_seed).to have_key('usage_id')
-        expect(audio_file_seed).to have_key('nested_files')
-      end
-
-      it 'seeds required data for a nested text track file' do
-        revision = create(:revision, :published)
-        entry = create(:entry, published_revision: revision)
-        published_entry = PublishedEntry.new(entry)
-        audio_file = create(:audio_file)
-        create(:file_usage, revision: revision, file: audio_file)
-        create(:text_track_file, parent_file: audio_file, entry: audio_file.entry)
+        create(:text_track_file, parent_file: audio_file, entry: entry)
 
         files_seed = JSON.parse(helper.files_json_seeds(published_entry))
         text_track_file_seed = files_seed['audio_files']
@@ -155,9 +113,32 @@ module Pageflow
         expect(text_track_file_seed).to have_key('file_name')
         expect(text_track_file_seed).to have_key('url')
         expect(text_track_file_seed).to have_key('original_url')
-        expect(text_track_file_seed).to have_key('label')
-        expect(text_track_file_seed).to have_key('kind')
-        expect(text_track_file_seed).to have_key('srclang')
+      end
+
+      it 'renders specified template' do
+        stub_template 'pageflow/editor/stub_files/_stub_file.json.jbuilder' =>
+                      'json.test_property "value"'
+        stub_file_type = FileType.new(model: 'Pageflow::VideoFile',
+                                      editor_partial: 'pageflow/editor/stub_files/stub_file',
+                                      collection_name: 'stub_files')
+        stub_page_type = page_type_class.new(file_types: [stub_file_type])
+        Pageflow.config.page_types.register(stub_page_type)
+
+        revision = create(:revision, :published)
+        entry = create(:entry, published_revision: revision)
+        published_entry = PublishedEntry.new(entry)
+        video_file = create(:video_file)
+        create(:file_usage, revision: revision, file: video_file)
+
+        files_seed = JSON.parse(
+          helper.render_json_partial(partial: 'pageflow/editor/files/file',
+                                     collection: published_entry.files(stub_file_type.model),
+                                     locals: {file_type: stub_file_type},
+                                     as: :file)
+        )
+        stub_file_seed = files_seed.first
+
+        expect(stub_file_seed).to have_key('test_property')
       end
     end
   end
