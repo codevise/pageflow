e2f36f4433a1df8ced235b2bf0db1381fac312f9
Joost Baaij <joost@spacebabies.nl>
2016-11-07 11:44:46 +0100

    move disabled into the initializer
    This way, sites have the option to force this value after the Pageflow
    javascript-code is done initialising.

--- app/assets/javascripts/pageflow/slideshow/atmo.js
+++ app/assets/javascripts/pageflow/slideshow/atmo.js
@@ -5,7 +5,7 @@
     initialize: function(slideshow, events, multiPlayer) {
       this.slideshow = slideshow;
       this.multiPlayer = multiPlayer;
-      this.disabled = false;
+      this.disabled = pageflow.browser.has('mobile platform');
 
       this.listenTo(events, 'page:change page:update', function() {
         this.update();
@@ -61,7 +61,7 @@
     update: function() {
       var configuration = this.slideshow.currentPageConfiguration();
 
-      if (!this.disabled && !pageflow.browser.has('mobile platform')) {
+      if (!this.disabled) {
         this.multiPlayer.fadeTo(configuration[attributeName]);
       }
     },
