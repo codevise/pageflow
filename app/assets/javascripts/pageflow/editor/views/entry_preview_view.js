61ba1e9af0c134dc19d0df43874e2aa9825cdc15
Tim Fischbach <tfischbach@codevise.de>
2016-12-21 19:27:31 +0100

    Trigger event when widgets are updated

--- app/assets/javascripts/pageflow/editor/views/entry_preview_view.js
+++ app/assets/javascripts/pageflow/editor/views/entry_preview_view.js
@@ -119,10 +119,16 @@ pageflow.EntryPreviewView = Backbone.Marionette.ItemView.extend({
   updatePresentWidgetsCssClasses: function(newWidgets) {
     var previousClasses = this.widgetNames(this.widgets);
     var newClasses = this.widgetNames(newWidgets);
+    var removedClasses = _.difference(previousClasses, newClasses);
+    var addedClasses = _.difference(newClasses, previousClasses);
 
     this.$el.addClass('widgets_present');
-    this.$el.removeClass(_.difference(previousClasses, newClasses).join(' '));
-    this.$el.addClass(newClasses.join(' '));
+    this.$el.removeClass(removedClasses.join(' '));
+    this.$el.addClass(addedClasses.join(' '));
+
+    if (removedClasses.length || addedClasses.length) {
+      pageflow.events.trigger('widgets:update');
+    }
   },
 
   widgetNames: function(widgets) {
