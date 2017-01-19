2e56b51c9aa8286f121f0a0cec20de501d0d0576
Tim Fischbach <tfischbach@codevise.de>
2017-01-19 18:26:08 +0100

    Add method to temporarily enable a widget

--- app/assets/javascripts/pageflow/widgets.js
+++ app/assets/javascripts/pageflow/widgets.js
@@ -1,9 +1,43 @@
-pageflow.widgets = {
-  isPresent: function(name) {
-    return !!$('div.widget_' + name + '_present').length;
-  },
+pageflow.widgets = (function() {
+  return {
+    isPresent: function(name) {
+      return !!$('div.' + className(name)).length;
+    },
 
-  areLoaded: function() {
-    return !!$('div.widgets_present').length;
+    areLoaded: function() {
+      return !!$('div.widgets_present').length;
+    },
+
+    use: function(options, callback) {
+      var original = options.insteadOf;
+
+      var originalClassName = className(original);
+      var replacementClassNames =
+        className(options.name) + ' ' +
+        className(original, 'replaced');
+
+      if (this.isPresent(original)) {
+        replace(originalClassName, replacementClassNames);
+
+        callback(function() {
+          replace(replacementClassNames, originalClassName);
+        });
+      }
+      else {
+        callback(function() {});
+      }
+    }
+  };
+
+  function replace(original, replacement) {
+    $('div.widgets_present')
+      .removeClass(original)
+      .addClass(replacement);
+
+    pageflow.events.trigger('widgets:update');
   }
-};
+
+  function className(name, state) {
+    return 'widget_' + name + '_' + (state || 'present');
+  }
+}());
