fa902e1b007a02b9964efce401e48dcf95a3c46e
Tim Fischbach <tfischbach@codevise.de>
2017-02-10 19:56:00 +0100

    Add help entry for text tracks
    Describe how text tracks can be added and how the automatic selection
    of subtitles works.

--- config/initializers/help_entries.rb
+++ config/initializers/help_entries.rb
@@ -2,6 +2,7 @@ Pageflow.configure do |config|
   config.help_entries.register('pageflow.help_entries.overview', priority: 100)
   config.help_entries.register('pageflow.help_entries.meta_data', priority: 50)
   config.help_entries.register('pageflow.help_entries.files', priority: 40)
+  config.help_entries.register('pageflow.help_entries.text_tracks', priority: 35)
   config.help_entries.register('pageflow.help_entries.outline', priority: 30)
   config.help_entries.register('pageflow.help_entries.page_options', priority: 20)
   config.help_entries.register('pageflow.help_entries.page_types', priority: 10)
