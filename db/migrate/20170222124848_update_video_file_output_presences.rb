5617421f4746ac98b76af2a80c78b85a07d605b6
Tim Fischbach <tfischbach@codevise.de>
2017-02-22 14:24:18 +0100

    Update output_presences for legacy videos in migration
    Record that the default mp4 variants are present.

---
+++ db/migrate/20170222124848_update_video_file_output_presences.rb
@@ -0,0 +1,12 @@
+class UpdateVideoFileOutputPresences < ActiveRecord::Migration
+  def up
+    execute(<<-SQL)
+      UPDATE pageflow_video_files
+        SET output_presences = '{"high":true,"medium":true,"low":true}'
+        WHERE output_presences IS NULL;
+    SQL
+  end
+
+  def down
+  end
+end
