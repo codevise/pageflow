c750128a6261f693d30ba0f16a7a85637d8d2b29
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-03-16 18:08:42 +0100

    Use more specific config where needed

--- app/helpers/pageflow/themes_helper.rb
+++ app/helpers/pageflow/themes_helper.rb
@@ -1,7 +1,9 @@
 module Pageflow
   module ThemesHelper
-    def themes_options_json_seed
-      Pageflow.config.themes.each_with_object({}) do |theme, options|
+    include RenderJsonHelper
+
+    def themes_options_json_seed(config = Pageflow.config)
+      config.themes.each_with_object({}) do |theme, options|
         options[theme.name] = theme.options
       end.to_json.html_safe
     end
