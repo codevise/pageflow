81cb602e09b4dd2100bae51b117ec8106c6396a1
Tim Fischbach <tfischbach@codevise.de>
2017-03-17 13:22:56 +0100

    Introduce memoized selectors
    * Add reselect dependency
    
    * Allow wrapping selector function so that they can be used as
      factories for memoized selectors.

--- node_package/src/utils/index.js
+++ node_package/src/utils/index.js
@@ -1,9 +1,11 @@
 import camelize from './camelize';
 import combine from './combine';
+import {combine as combineSelectors} from './memoizedSelector';
 import has from './has';
 
 export {
   camelize,
   combine,
+  combineSelectors,
   has
 };
