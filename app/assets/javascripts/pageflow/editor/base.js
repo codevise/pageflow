7da32cf45cccf2d4b4a79a978e4e07855d3d04cf
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-04-10 16:46:24 +0200

    Set up themes for editor

--- app/assets/javascripts/pageflow/editor/base.js
+++ app/assets/javascripts/pageflow/editor/base.js
@@ -53,6 +53,7 @@
 //= require ./initializers/setup_page_types
 //= require ./initializers/setup_hotkeys
 //= require ./initializers/setup_file_uploader
+//= require ./initializers/setup_themes
 //= require ./initializers/edit_lock
 //= require ./initializers/files_polling
 //= require ./initializers/stylesheet_reloading
