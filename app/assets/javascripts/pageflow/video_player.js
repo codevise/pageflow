72714f51cd8063fd01c766d0c98e9bef2c099132
Tim Fischbach <tfischbach@codevise.de>
2017-04-19 17:44:06 +0200

    Remove VideoJS patches

--- app/assets/javascripts/pageflow/video_player.js
+++ app/assets/javascripts/pageflow/video_player.js
@@ -7,8 +7,6 @@
 //= require ./video_player/prebuffering
 //= require ./video_player/buffer_underrun_waiting
 //= require ./video_player/src_from_options_method
-// require ./video_player/play_button_patch
-// require ./video_player/player_buffered_patch
 //= require ./video_player/filter_sources
 //= require ./video_player/lazy
 
