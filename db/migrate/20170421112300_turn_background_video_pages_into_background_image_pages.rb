310a70205e8c3f529e3803280b9fdfe214ec0ed4
Tim Fischbach <tfischbach@codevise.de>
2017-04-21 18:16:14 +0200

    Turn background_video pages into background_image pages
    Background videos are now supported in a lot of places that only
    supported background images before. The `background_video` page type
    this equivalent to a `background_image` page with `background_type`
    set to `video`.

---
+++ db/migrate/20170421112300_turn_background_video_pages_into_background_image_pages.rb
@@ -0,0 +1,22 @@
+class TurnBackgroundVideoPagesIntoBackgroundImagePages < ActiveRecord::Migration
+  # Pageflow::Page might have gotten out of sync with schema at this
+  # point. Use local model instead.
+  class MigratedPage < ActiveRecord::Base
+    self.table_name = 'pageflow_pages'
+
+    serialize :configuration, JSON
+
+    def configuration
+      super || {}
+    end
+  end
+
+  def change
+    MigratedPage.where(template: 'background_video').find_each do |page|
+      page.template = 'background_image'
+      page.configuration['background_type'] = 'video'
+
+      page.save!
+    end
+  end
+end
