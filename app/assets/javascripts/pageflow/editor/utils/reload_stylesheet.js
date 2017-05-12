8d9afe1db514d40ea510b9443defe0c6dea2b201
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-05-12 11:54:00 +0200

    Refactor stylesheet utils

--- app/assets/javascripts/pageflow/editor/utils/reload_stylesheet.js
+++
@@ -1,9 +0,0 @@
-pageflow.reloadStylesheet = function(name) {
-  var link = $('head link[data-name=' + name + ']');
-
-  if (!link.data('originalHref')) {
-    link.data('originalHref', link.attr('href'));
-  }
-
-  link.attr('href', link.data('originalHref') + '&reload=' + new Date().getTime());
-};
