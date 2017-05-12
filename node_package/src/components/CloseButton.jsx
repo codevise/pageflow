1e14b2fb3b16bc33cb0ec2fdc6860db1f7018606
Tim Fischbach <tfischbach@codevise.de>
2017-05-12 14:21:41 +0200

    Use entry locale in CloseButton translations
    Backport of #760
    
    Use the `t` function which is given via `connect`, rather than
    accessing the global `I18n` object which uses the   default locale.

--- node_package/src/components/CloseButton.jsx
+++ node_package/src/components/CloseButton.jsx
@@ -1,13 +1,21 @@
-export default function(props) {
+import {t} from 'i18n/selectors';
+import {combineSelectors} from 'utils';
+import {connect} from 'react-redux';
+
+function CloseButton(props) {
   return (
     <div className="close_button text_hidden_only"
          tabIndex="4"
-         title={I18n.t('pageflow.public.close')}
+         title={props.t('pageflow.public.close')}
          onClick={props.onClick}>
 
       <div className="label">
-        {I18n.t('pageflow.public.close')}
+        {props.t('pageflow.public.close')}
       </div>
     </div>
   );
 }
+
+export default connect(combineSelectors({
+  t
+}))(CloseButton);
