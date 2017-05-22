8364c5db1ef9dc4ef8647f20b9584665de64bf55
Tim Fischbach <tfischbach@codevise.de>
2017-05-22 18:23:10 +0200

    Fix exception when sorting user accounts table by role
    When eager loading memberships entities, we need to ensure (using
    `references`) that ActiveRecord always does a join instead of separate
    query. Otherwise the conidition inside the `account`/`entry`
    associations references a missing column.
    
    Before the table only worked when sorting by entity name since the
    order requires a join.
    
    Also make sure to actually use the preloaded association to prevent
    N+1 queries.

--- app/views/components/pageflow/admin/user_entries_tab.rb
+++ app/views/components/pageflow/admin/user_entries_tab.rb
@@ -2,22 +2,23 @@ module Pageflow
   module Admin
     class UserEntriesTab < ViewComponent
       def build(user)
-        embedded_index_table(user.memberships.on_entries.includes(:entry, entry: :account)
-                              .accessible_by(current_ability, :index),
+        embedded_index_table(user.memberships.on_entries
+                               .includes(:entry, entry: :account).references(:pageflow_entries)
+                               .accessible_by(current_ability, :index),
                              blank_slate_text: t('pageflow.admin.users.no_entries')) do
           table_for_collection class: 'memberships', sortable: true, i18n: Pageflow::Membership do
             column :entry, sortable: 'pageflow_entries.title' do |membership|
-              link_to(membership.entity.title, admin_entry_path(membership.entity))
+              link_to(membership.entry.title, admin_entry_path(membership.entry))
             end
             column :role, sortable: 'pageflow_memberships.role' do |membership|
               membership_role_with_tooltip(membership.role, scope: 'entries')
             end
             column :account, sortable: 'pageflow_accounts.name' do |membership|
-              if authorized?(:read, membership.entity.account)
-                link_to(membership.entity.account.name,
-                        admin_account_path(membership.entity.account))
+              if authorized?(:read, membership.entry.account)
+                link_to(membership.entry.account.name,
+                        admin_account_path(membership.entry.account))
               else
-                membership.entity.account.name
+                membership.entry.account.name
               end
             end
             column :created_at, sortable: 'pageflow_memberships.created_at'
