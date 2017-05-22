9c0c2346c81610c69d9df4355e35999b39e14f8b
Tim Fischbach <tfischbach@codevise.de>
2017-05-22 18:23:10 +0200

    Fix exception when sorting user accounts table by role
    When eager loading memberships entities, we need to ensure (using
    `references`) that ActiveRecord always does a join instead of separate
    query. Otherwise the conidition inside the `account`/`entry`
    associations references a missing column.
    
    Before the table only worked when sorting by entity name since the
    order requires a join.
    
    Also make sure to actually use the preloaded association to prevent
    N+1 queries.

---
+++ spec/views/components/pageflow/admin/user_accounts_tab_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+module Pageflow
+  describe Admin::UserAccountsTab, type: :view_component do
+    before do
+      helper.extend(ActiveAdmin::ViewHelpers)
+      helper.extend(Pageflow::Admin::MembershipsHelper)
+
+      allow(helper).to receive(:url_for)
+      allow(helper).to receive(:authorized?).and_return(true)
+    end
+
+    %w[
+      pageflow_accounts.name_desc
+      pageflow_memberships.role_desc
+      pageflow_memberships.created_at desc
+    ].each do |order|
+      it "can be sorted by #{order}" do
+        user = create(:user)
+        create(:account, with_member: user, name: 'Account 1')
+        create(:account, with_publisher: user, name: 'Account 2')
+
+        sign_in(user)
+        params[:order] = order
+
+        render(user)
+
+        expect(rendered).to have_selector('table tr td', text: 'Account 1')
+        expect(rendered).to have_selector('table tr td', text: 'Account 2')
+      end
+    end
+  end
+end
