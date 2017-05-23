de7307f9e7d983360fe8fb2da2831e9ab6604676
Tim Fischbach <tfischbach@codevise.de>
2017-05-23 09:51:40 +0200

    Improve view component test setup
    * Include Devise test helpers.
    
    * Simplify view component example group by calling `insert_tag` with
      the component class directly. No longer need to remember the builder
      method name.

--- lib/pageflow/view_component.rb
+++ lib/pageflow/view_component.rb
@@ -1,14 +1,5 @@
 module Pageflow
-  # Base class for Arbre components defined by Pageflow. Store the
-  # builder method name to facilitate calling it in specs.
+  # Base class for Arbre components defined by Pageflow.
   class ViewComponent < Arbre::Component
-    class << self
-      attr_reader :builder_method_name
-    end
-
-    def self.builder_method(name)
-      @builder_method_name = name
-      super
-    end
   end
 end
