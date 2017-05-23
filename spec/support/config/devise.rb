7f5ce8cfb4488308c758c75922a6a06087164e4f
Tim Fischbach <tfischbach@codevise.de>
2017-05-23 09:51:40 +0200

    Improve view component test setup
    * Include Devise test helpers.
    
    * Simplify view component example group by calling `insert_tag` with
      the component class directly. No longer need to remember the builder
      method name.

--- spec/support/config/devise.rb
+++ spec/support/config/devise.rb
@@ -1,5 +1,6 @@
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view_component
 
   module DeviseTestHelpersWithScope
     def sign_in(user)
@@ -7,5 +8,6 @@ RSpec.configure do |config|
     end
   end
 
-  config.include DeviseTestHelpersWithScope, :type => :controller
+  config.include DeviseTestHelpersWithScope, type: :controller
+  config.include DeviseTestHelpersWithScope, type: :view_component
 end
