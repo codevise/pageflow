81ebc5bae50e3601aead9bbefe67125a75a41a52
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-05-28 15:36:59 +0200

    Introduce dedicated preview thumbnails for themes
    To be stored at
    `app/assets/images/pageflow/themes/$THEME_NAME/preview_thumbnail.png`.

--- spec/javascripts/views/inputs/reference_input_view_spec.js
+++ spec/javascripts/views/inputs/reference_input_view_spec.js
@@ -9,6 +9,16 @@ describe('pageflow.ReferenceInputView', function() {
     }
   });
 
+  var TestViewWithChoose = TestView.extend({
+    choose: function() {
+      return $.Deferred(function(deferred) {
+        deferred.resolve(new Backbone.Model ({
+          perma_id: 'this id are belong to us'
+        }));
+      });
+    }
+  });
+
   describe('with disabled option', function() {
     it('sets disabled attributes on buttons', function() {
       var view = new TestView({
@@ -20,6 +30,33 @@ describe('pageflow.ReferenceInputView', function() {
       view.render();
 
       expect(view.ui.buttons).to.have.$attr('disabled');
+    });
+  });
+
+  describe('with hideUnsetButton', function() {
+    it('hides the unset button', function() {
+      var view = new TestView({
+        model: new Model(),
+        propertyName: 'some_id',
+        hideUnsetButton: true
+      });
+
+      view.render();
+
+      expect(view.ui.unsetButton.css('display')).to.eq('none');
+    });
+  });
+
+  describe('default chooseValue()', function() {
+    it('returns perma_id of the result of choose()', function() {
+      var view = new TestViewWithChoose({
+        model: new Model(),
+        propertyName: 'some_id'
+      });
+
+      view.render();
+
+      expect(view.chooseValue()).to.contain('belong to us');
     });
   });
 });
