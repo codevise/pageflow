0b8ee696b9f7c1f895783026b3026df2de43e749
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-05-28 15:36:59 +0200

    Add editor specs

--- app/assets/javascripts/pageflow/editor/models/theme.js
+++ app/assets/javascripts/pageflow/editor/models/theme.js
@@ -1,9 +1,13 @@
 pageflow.Theme = Backbone.Model.extend({
+  initialize: function(attributes, options) {
+    this.options = options || {};
+  },
+
   title: function() {
     return I18n.t('pageflow.' + this.get('name') + '_theme.name');
   },
 
   thumbnailUrl: function() {
-    return this.get('preview_image_url');
+    return this.options.thumbnailUrl || this.get('preview_image_url');
   }
 });
