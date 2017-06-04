dc206397b6124a6867a79887604180f4eef93179
Joost Baaij <joost@spacebabies.nl>
2017-06-04 21:48:09 +0200

    Paperclip :pageflow_filesystem_root interpolation
    Previously, this interpolation would only be defined in development, test and production environments. A staging environment did not have this interpolation which would lead to an error. Changed the existing code to a switch inside the definition, so that an interpolation value always exists.
    
    The special cases for Rails.env.test should probably move into a support file under spec.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,9 @@
 require 'socket'
+require 'rails'
 
 Pageflow.configure do |config|
   config.paperclip_attachments_version = 'v1'
-  config.paperclip_filesystem_root = Rails.root.join('tmp/attachments/production/')
+  config.paperclip_filesystem_root = Rails.root.join("tmp/attachments/#{Rails.env}/")
 
   if Rails.env.test?
     config.paperclip_s3_default_options.merge!({
@@ -71,35 +72,22 @@ Pageflow.configure do |config|
   }
 end
 
-if Rails.env.development?
-  Paperclip.interpolates(:pageflow_filesystem_root) do |attachment, style|
-    Rails.root.join('tmp/attachments/development/filesystem')
-  end
-
-  Paperclip.interpolates(:host) do |attachment, style|
-    ENV.fetch('PAGEFLOW_ATTACHMENTS_SCOPE_NAME') { Socket.gethostname }
-  end
+Paperclip.interpolates(:pageflow_filesystem_root) do |attachment, style|
+  Pageflow.config.paperclip_filesystem_root
 end
 
-if Rails.env.production?
-  Paperclip.interpolates(:pageflow_filesystem_root) do |attachment, style|
-    Pageflow.config.paperclip_filesystem_root
-  end
-
-  Paperclip.interpolates(:host) do |attachment, style|
+Paperclip.interpolates(:host) do |attachment, style|
+  case Rails.env
+  when "development"
+    ENV.fetch('PAGEFLOW_ATTACHMENTS_SCOPE_NAME') { Socket.gethostname }
+  when "test"
+    'test-host'
+  else
     'main'
   end
 end
 
 if Rails.env.test?
-  Paperclip.interpolates(:pageflow_filesystem_root) do |attachment, style|
-    Rails.root.join('tmp/attachments/test/filesystem')
-  end
-
-  Paperclip.interpolates(:host) do |attachment, style|
-    'test-host'
-  end
-
   class Paperclip::Attachment
     def bucket_name
       'test'
