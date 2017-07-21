83b80ae98c9c66e47d8c00fbcde3740651b7b052
Tim Fischbach <tfischbach@codevise.de>
2017-07-21 18:41:32 +0200

    Improve text tracks positioning
    Move text tracks up immediately when player controls are displayed,
    but only move them back down on next cue change when player controls
    are hidden again.
    
    When player controls are displayed, we have no other chance than
    moving the text tracks directly since they would otherwise be hidden
    by the player controls. After the player controls are hidden again, we
    can wait for the user to finish reading the current cue and only then
    move text tracks back down.

--- app/assets/javascripts/pageflow/video_player/cue_settings_methods.js
+++ app/assets/javascripts/pageflow/video_player/cue_settings_methods.js
@@ -1,6 +1,31 @@
 pageflow.VideoPlayer.cueSettingsMethods = function(player) {
-  player.updateCueLineSettings = function(line, forceUpdate) {
-    var value = line.split('.')[0];
+  /**
+   * Specify the display position of text tracks. This method can also
+   * be used to make VideoJS reposition the text tracks after the
+   * margins of the text track display have been changed (e.g. to
+   * translate text tracks when player controls are displayed).
+   *
+   * To force such an update, the passed string has to differ from the
+   * previously passed string. You can append a dot and an arbitrary
+   * string (e.g. `"auto.translated"`), to keep the current setting but
+   * still force repositioning.
+   *
+   * On the other hand, it is possible to change the positioning but
+   * have VideoJS apply the change only when the next cue is
+   * displayed. This way we can prevent moving a cue that the user
+   * might just be reading. Simply append the string `".lazy"`
+   * (e.g. `"auto.lazy"`).
+   *
+   * @param {string} line
+   *   Either `"top"` to move text tracks to the first line or
+   *   `"auto"` to stick with automatic positioning, followed by a tag
+   *   to either force or prevent immediate update.
+   */
+  player.updateCueLineSettings = function(line) {
+    var components = line.split('.');
+    var value = components[0];
+    var command = components[1];
+
     value = value == 'top' ? 1 : value;
 
     var changed = false;
@@ -24,7 +49,7 @@ pageflow.VideoPlayer.cueSettingsMethods = function(player) {
     // track changed since the last call, i.e. `line` has been changed
     // for a cue even though the previous call had the same
     // parameters.
-    if (this.prevLine !== line || changed) {
+    if ((this.prevLine !== line || changed) && (command != 'lazy')) {
       player.tech({IWillNotUseThisInPlugins: true}).trigger('texttrackchange');
     }
 
