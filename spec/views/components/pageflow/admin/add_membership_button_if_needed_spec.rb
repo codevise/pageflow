6f5445849e97dc66a3352409bfe2f6ba50872302
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-08-01 19:21:43 +0200

    Basic tests for UserAccountsTab add user button

--- spec/views/components/pageflow/admin/add_membership_button_if_needed_spec.rb
+++ spec/views/components/pageflow/admin/add_membership_button_if_needed_spec.rb
@@ -7,6 +7,7 @@ module Pageflow
         helper.extend(ActiveAdmin::ViewHelpers)
         helper.extend(Pageflow::Admin::MembershipsHelper)
         allow(helper).to receive(:new_admin_account_membership_path)
+        allow(helper).to receive(:new_admin_user_membership_path)
         allow(helper).to receive(:url_for)
       end
 
@@ -37,6 +38,38 @@ module Pageflow
 
         render do
           add_membership_button_if_needed(account_manager, account, 'account')
+        end
+
+        expect(rendered).to have_selector('a', class: 'disabled')
+      end
+
+      it 'renders add account button when account available' do
+        first_account = create(:account)
+        user = create(:user, :member, on: first_account)
+        second_account = create(:account)
+        account_manager = create(:user, :manager, on: first_account)
+        create(:membership,
+               user: account_manager,
+               entity: second_account,
+               role: 'manager')
+        sign_in(account_manager)
+
+        render do
+          add_membership_button_if_needed(user, user, 'account')
+        end
+
+        expect(rendered).to have_selector('a', text: 'Add Account')
+        expect(rendered).to_not have_selector('a', class: 'disabled')
+      end
+
+      it 'renders disabled add account button when account unavailable' do
+        first_account = create(:account)
+        user = create(:user, :member, on: first_account)
+        account_manager = create(:user, :manager, on: first_account)
+        sign_in(account_manager)
+
+        render do
+          add_membership_button_if_needed(user, user, 'account')
         end
 
         expect(rendered).to have_selector('a', class: 'disabled')
