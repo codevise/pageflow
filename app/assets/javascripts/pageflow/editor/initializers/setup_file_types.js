a55dbe23e2830d843abf5de9d69d53846a34a27d
Tim Fischbach <tfischbach@codevise.de>
2017-08-31 21:04:43 +0200

    Fix file type detection for vtt uploads on Safari
    Backport of #851
    
    Safari gives an empty string as `type` for vtt file uploads. Test for
    file extension instead. We already do this for `srt` files anyway.

--- app/assets/javascripts/pageflow/editor/initializers/setup_file_types.js
+++ app/assets/javascripts/pageflow/editor/initializers/setup_file_types.js
@@ -85,7 +85,7 @@ pageflow.app.addInitializer(function(options) {
   pageflow.editor.fileTypes.register('text_track_files', {
     model: pageflow.TextTrackFile,
     matchUpload: function(upload) {
-      return upload.type.match(/\/vtt$/) ||
+      return upload.name.match(/\.vtt$/) ||
         upload.name.match(/\.srt$/);
     },
     skipUploadConfirmation: true,
