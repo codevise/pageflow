d827d0d50896e5eb5b0e64cae46540f4c50b4dff
Tim Fischbach <tfischbach@codevise.de>
2017-10-17 13:27:55 +0200

    Add ManagedUserQuery::Scope

---
+++ app/models/pageflow/managed_user_query.rb
@@ -0,0 +1,44 @@
+module Pageflow
+  # @api private
+  class ManagedUserQuery < ApplicationQuery
+    class Scope < Scope
+      def initialize(current_user, scope)
+        @current_user = current_user
+        @scope = scope
+      end
+
+      def resolve
+        if current_user.admin?
+          scope.all
+        else
+          scope
+            .distinct
+            .joins(account_memberships)
+            .joins(account_manager_memberships_of_current_user)
+        end
+      end
+
+      private
+
+      attr_reader :current_user, :scope
+
+      def account_memberships
+        <<-SQL
+          INNER JOIN pageflow_memberships account_memberships ON
+          account_memberships.user_id = users.id AND
+          account_memberships.entity_type = "Pageflow::Account"
+        SQL
+      end
+
+      def account_manager_memberships_of_current_user
+        sanitize_sql(<<-SQL, user_id: current_user.id)
+          INNER JOIN pageflow_memberships account_manager_memberships ON
+          account_manager_memberships.entity_type = account_memberships.entity_type AND
+          account_manager_memberships.entity_id = account_memberships.entity_id AND
+          account_manager_memberships.user_id = :user_id AND
+          account_manager_memberships.role = "manager"
+        SQL
+      end
+    end
+  end
+end
---
+++ spec/models/pageflow/managed_user_query_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+module Pageflow
+  describe ManagedUserQuery::Scope do
+    describe '#resolve' do
+      it 'includes users of accounts managed by the current user' do
+        account = create(:account)
+        current_user = create(:user, :manager, on: account)
+        managed_user = create(:user, :member, on: account)
+
+        result = ManagedUserQuery::Scope.new(current_user, User).resolve
+
+        expect(result).to include(managed_user)
+      end
+
+      it 'does not include users that are not member of an account managed by user' do
+        account = create(:account)
+        current_user = create(:user, :manager, on: account)
+        other_user = create(:user)
+
+        result = ManagedUserQuery::Scope.new(current_user, User).resolve
+
+        expect(result).not_to include(other_user)
+      end
+
+      it 'does not include users of accounts the current_user is not manager of' do
+        account = create(:account)
+        current_user = create(:user, :publisher, on: account)
+        user = create(:user, :member, on: account)
+
+        result = ManagedUserQuery::Scope.new(current_user, User).resolve
+
+        expect(result).not_to include(user)
+      end
+
+      it 'does not include duplicate users if there are multiple common managed accounts' do
+        account = create(:account)
+        current_user = create(:user, :manager, on: account)
+        user = create(:user, :member, on: account)
+        other_account = create(:account)
+        create(:membership, user: current_user, entity: other_account, role: 'manager')
+        create(:membership, user: user, entity: other_account, role: 'member')
+
+        result = ManagedUserQuery::Scope.new(current_user, User).resolve
+
+        expect(result.size).to eq(2)
+      end
+
+      describe 'for admins' do
+        it 'includes all users' do
+          current_user = create(:user, :admin)
+          managed_user = create(:user)
+
+          result = ManagedUserQuery::Scope.new(current_user, User).resolve
+
+          expect(result).to include(managed_user)
+        end
+      end
+    end
+  end
+end
