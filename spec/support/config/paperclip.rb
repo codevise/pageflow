f9e944726a9f1f6022fa4dec8b58b0e0b56cc807
Joost Baaij <joost@spacebabies.nl>
2017-11-03 12:48:49 +0100

    move conditionals on RAILS_ENV=test into spec/support

--- spec/support/config/paperclip.rb
+++ spec/support/config/paperclip.rb
@@ -1,3 +1,20 @@
+Paperclip.interpolates(:host) do |attachment, style|
+  'test-host'
+end
+
+class Paperclip::Attachment
+  def bucket_name
+    'test'
+  end
+end
+
+Pageflow.configure do |config|
+  config.paperclip_s3_default_options.merge!({
+    storage: :filesystem,
+    path: ':rails_root/tmp/attachments/test/s3/:class/:attachment/:id_partition/:style/:filename'
+  })
+end
+
 RSpec.configure do |config|
   config.before(:each) do
     Dir.glob(Rails.root.join('tmp', 'attachments', 'test', '*')).each do |f|
