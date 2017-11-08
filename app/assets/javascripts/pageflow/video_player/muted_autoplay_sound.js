ea3205d6a1914a9430d456d44132194342d55732
Rafael Teixeira <rteixeira@codevise.de>
2017-11-08 15:18:08 +0100

    Lazy loads howler audio player on video playback

--- app/assets/javascripts/pageflow/video_player/muted_autoplay_sound.js
+++ app/assets/javascripts/pageflow/video_player/muted_autoplay_sound.js
@@ -1,34 +1,34 @@
 pageflow.VideoPlayer.mutedAutoplaySound = function(player) {
-  if (!pageflow.browser.has('native video player')) {
-    var originalReady = player.ready;
-    player.ready = function() {
-      var videoSrc = player.currentSources().find(function(i){
-        return i.type === 'video/mp4';
-      }).src;
+  if (pageflow.browser.has('mobile platform')) {
+    function setupHowler() {
+      if (!player.howler) {
+        var videoSrc = player.currentSources().find(function(i){
+          return i.type === 'video/mp4';
+        }).src;
 
-      player.howler = new Howl({
-        src: [ videoSrc ],
-        loop: true
-      });
-      return originalReady.apply(this, arguments);
-    };
+        player.howler = new Howl({
+          src: [ videoSrc ],
+          loop: true
+        });
+      }
+    }
+
+    player.ready(function() {
+      setupHowler();
+    });
 
     var originalPlay = player.play;
     player.play = function() {
+      setupHowler();
       player.howler.play();
       return originalPlay.apply(this, arguments);
     };
 
     var originalPause = player.pause;
     player.pause = function() {
+      setupHowler();
       player.howler.pause();
       return originalPause.apply(this, arguments);
-    };
-
-    var originalEnded = player.ended;
-    player.ended = function() {
-      player.howler.pause();
-      return originalEnded.apply(this, arguments);
     };
   }
 };
