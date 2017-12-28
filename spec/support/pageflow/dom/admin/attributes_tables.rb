0f8b5e76be8b83c6f4ff70dff24710ca096b72cc
Tim Fischbach <tfischbach@codevise.de>
2017-12-28 18:29:59 +0100

    Add Dominos to pageflow-support to test admin features
    Public Domino based API to be used by plugins to integration test
    admin extensions like additional form inputs or attributes table rows.

---
+++ spec/support/pageflow/dom/admin/attributes_tables.rb
@@ -0,0 +1,64 @@
+module Pageflow
+  module Dom
+    module Admin
+      # Base class for attribute table dominos.
+      #
+      # @since edge
+      class AttributesTable < Domino
+        # Visit the admin page of the given resource and find the
+        # attributes table.
+        def self.for(resource)
+          visit(url(resource))
+          find!
+        end
+
+        # Find the Capybara node of the contents cell for the given
+        # attribute.
+        def contents_of_row(name)
+          node.find(".row-#{name} td")
+        end
+
+        def self.url_helpers
+          Rails.application.routes.url_helpers
+        end
+        private_class_method :url_helpers
+      end
+
+      # The attributes table on the account page.
+      #
+      # @since edge
+      class AccountAttributesTable < AttributesTable
+        selector '.attributes_table.pageflow_account'
+
+        def self.url(account)
+          url_helpers.admin_account_path(account)
+        end
+        private_class_method :url
+      end
+
+      # The attributes table on the entry page
+      #
+      # @since edge
+      class EntryAttributesTable < AttributesTable
+        selector '.attributes_table.pageflow_entry'
+
+        def self.url(entry)
+          url_helpers.admin_entry_path(entry)
+        end
+        private_class_method :url
+      end
+
+      # The theming attributes table on the account page
+      #
+      # @since edge
+      class ThemingAttributesTable < AttributesTable
+        selector '.attributes_table.pageflow_theming'
+
+        def self.url(theming)
+          url_helpers.admin_account_path(theming.account)
+        end
+        private_class_method :url
+      end
+    end
+  end
+end
---
+++ spec/support/pageflow/dom/admin/sign_in_form.rb
@@ -0,0 +1,25 @@
+module Pageflow
+  module Dom
+    module Admin
+      # @api private
+      class SignInForm < Domino
+        selector '#login'
+
+        def submit_with(options)
+          within(id) do
+            fill_in 'user_email', with: options[:email]
+            fill_in 'user_password', with: options[:password]
+
+            find('[name="commit"]').click
+          end
+        end
+
+        def forgot_password_link
+          within(id) do
+            find('a')
+          end
+        end
+      end
+    end
+  end
+end
