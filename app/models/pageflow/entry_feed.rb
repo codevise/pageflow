13d22f5d7fbe3398ddb6897437eb978643a2d888
Joost Baaij <joost@spacebabies.nl>
2018-01-02 16:32:53 +0100

    fix Hound warnings

--- app/models/pageflow/entry_feed.rb
+++ app/models/pageflow/entry_feed.rb
@@ -4,7 +4,7 @@ module Pageflow
       attr_reader :scope, :page
       attr_accessor :per_page
 
-      def initialize(scope, page=nil)
+      def initialize(scope, page = nil)
         @scope = scope
         @page = page
         @per_page = 25
@@ -14,7 +14,7 @@ module Pageflow
         scope \
           .includes(:published_revision)
           .published_without_password_protection
-          .order("pageflow_revisions.published_at DESC")
+          .order('pageflow_revisions.published_at DESC')
           .page(page).per(per_page)
       end
     end
