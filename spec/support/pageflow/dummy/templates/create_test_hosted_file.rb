9772c3ae24fb3195ba73e4d38e3f2585db1e894f
Tim Fischbach <tfischbach@codevise.de>
2018-01-11 18:11:25 +0100

    Do not use deprecated migration helper for TestHostedFile

--- spec/support/pageflow/dummy/templates/create_test_hosted_file.rb
+++ spec/support/pageflow/dummy/templates/create_test_hosted_file.rb
@@ -1,7 +1,28 @@
 class CreateTestHostedFile < ActiveRecord::Migration[4.2]
   def change
     create_table :test_hosted_files do |t|
-      Pageflow::HostedFile.columns(t)
+      t.belongs_to(:entry, index: true)
+      t.belongs_to(:uploader, index: true)
+
+      t.integer(:parent_file_id)
+      t.string(:parent_file_model_type)
+
+      t.string(:state)
+      t.string(:rights)
+
+      t.string(:attachment_on_filesystem_file_name)
+      t.string(:attachment_on_filesystem_content_type)
+      t.integer(:attachment_on_filesystem_file_size, limit: 8)
+      t.datetime(:attachment_on_filesystem_updated_at)
+
+      t.string(:attachment_on_s3_file_name)
+      t.string(:attachment_on_s3_content_type)
+      t.integer(:attachment_on_s3_file_size, limit: 8)
+      t.datetime(:attachment_on_s3_updated_at)
+
+      t.timestamps
+
+      t.string :custom
     end
   end
 end
