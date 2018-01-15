7bc03daa9760cf61097ca617c62596d98f43b0c2
Rafael Teixeira <rteixeira@codevise.de>
2018-01-15 17:16:06 +0100

    Add fadeVolume method to proxy player

--- app/assets/javascripts/pageflow/media_player/volume_fading/fade_available.js
+++
@@ -1,9 +0,0 @@
-pageflow.mediaPlayer.volumeFading.fadeAvailable = function(player) {
-  player.fadeVolume = function(value, duration) {
-    var currentValue = this.volume();
-
-    this.fade(currentValue, value, duration);
-
-    return new $.Deferred().resolve().promise();
-  };
-};
