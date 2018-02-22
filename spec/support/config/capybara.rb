d691698530a6cba523a500253b5e2647b0a2c554
Tim Fischbach <tfischbach@codevise.de>
2018-02-22 11:19:48 +0100

    Use headless Chrome for Capybara browser tests

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,4 +1,14 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'selenium-webdriver'
 
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :selenium_chrome_headless_no_sandbox do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-gpu'
+  # Required for chrome to work in container based Travis environment
+  # (see https://docs.travis-ci.com/user/chrome)
+  browser_options.args << '--no-sandbox'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless_no_sandbox
