56528202307b7365b7bbb5907998c9dde3fcbb88
Tim Fischbach <tfischbach@codevise.de>
2018-10-10 16:17:05 +0200

    Introduce widget insert points
    Allow inserting widgets before entry. This is a prerequisite to
    introduce widget types for different loading spinners.

--- lib/pageflow/react/widget_type.rb
+++ lib/pageflow/react/widget_type.rb
@@ -1,15 +1,20 @@
 module Pageflow
   module React
     class WidgetType < Pageflow::WidgetType
-      attr_reader :name, :role
+      attr_reader :name, :role, :options
 
-      def initialize(name, role)
+      def initialize(name, role, options = {})
         @name = name
         @role = role
+        @options = options
       end
 
       def roles
         [role]
+      end
+
+      def insert_point
+        @options[:insert_point] || super
       end
 
       def render(template, _)
