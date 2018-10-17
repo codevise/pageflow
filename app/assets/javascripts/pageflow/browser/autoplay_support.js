80a94df649c0e9c3b7883a7e1d62d077004684a3
Tim Fischbach <tfischbach@codevise.de>
2018-10-17 13:48:15 +0200

    Update autoplay support feature
    * Disable for Safari 11 and above

--- app/assets/javascripts/pageflow/browser/autoplay_support.js
+++ app/assets/javascripts/pageflow/browser/autoplay_support.js
@@ -1,4 +1,4 @@
 pageflow.browser.feature('autoplay support', function(has) {
-  return !pageflow.browser.agent.matchesSafari11() &&
-    !pageflow.browser.agent.matchesMobilePlatform();
+  return !pageflow.browser.agent.matchesSafari11AndAbove() &&
+         !pageflow.browser.agent.matchesMobilePlatform();
 });
