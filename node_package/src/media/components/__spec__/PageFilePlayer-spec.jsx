6e192152ea97ca92a9f675be2beb88ccfa981d7b
Tim Fischbach <tfischbach@codevise.de>
2018-10-26 17:33:38 +0200

    Let page player render preload component
    Turn `createPageFilePlayer` HOC into `PageFilePlayer` that takes
    player and preload components via props.

---
+++ node_package/src/media/components/__spec__/PageFilePlayer-spec.jsx
@@ -0,0 +1,72 @@
+import {PageFilePlayer} from '../PageFilePlayer';
+
+import {shallow} from 'enzyme';
+import {expect} from 'support';
+
+describe('PageFilePlayer', () => {
+  it('renders player component if file is ready and page is is prepared', () => {
+    const Player = function() {};
+    const Preload = function() {};
+    const file = {isReady: true};
+
+    const wrapper = shallow(<PageFilePlayer file={file}
+                                            pageIsPrepared={true}
+                                            playerComponent={Player}
+                                            preloadComponent={Preload} />);
+
+    expect(wrapper).to.have.descendants(Player);
+  });
+
+  it('renders placeholder if page is is prepared but file not ready', () => {
+    const Player = function() {};
+    const Preload = function() {};
+    const file = {isReady: false};
+
+    const wrapper = shallow(<PageFilePlayer file={file}
+                                            pageIsPrepared={true}
+                                            playerComponent={Player}
+                                            preloadComponent={Preload} />);
+
+    expect(wrapper).to.have.descendants('noscript');
+  });
+
+  it('renders preload component if file is ready and page is preloaded', () => {
+    const Player = function() {};
+    const Preload = function() {};
+    const file = {isReady: true};
+
+    const wrapper = shallow(<PageFilePlayer file={file}
+                                            pageIsPrepared={false}
+                                            pageIsPreloaded={true}
+                                            playerComponent={Player}
+                                            preloadComponent={Preload} />);
+
+    expect(wrapper).to.have.descendants(Preload);
+  });
+
+  it('renders placeholder if page is is preloaded but file not ready', () => {
+    const Player = function() {};
+    const Preload = function() {};
+    const file = {isReady: false};
+
+    const wrapper = shallow(<PageFilePlayer file={file}
+                                            pageIsPrepared={false}
+                                            pageIsPreloaded={true}
+                                            playerComponent={Player}
+                                            preloadComponent={Preload} />);
+
+    expect(wrapper).to.have.descendants('noscript');
+  });
+
+  it('renders placeholder if page is is preloaded but preload component not present', () => {
+    const Player = function() {};
+    const file = {isReady: true};
+
+    const wrapper = shallow(<PageFilePlayer file={file}
+                                            pageIsPrepared={false}
+                                            pageIsPreloaded={true}
+                                            playerComponent={Player} />);
+
+    expect(wrapper).to.have.descendants('noscript');
+  });
+});
