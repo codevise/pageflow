9198425780b9d0c9737a9d9d7bb3ddeca52c2e03
Tim Fischbach <tfischbach@codevise.de>
2018-10-26 17:20:21 +0200

    Add component to preload video poster

--- node_package/spec/support/index.js
+++ node_package/spec/support/index.js
@@ -1,9 +1,11 @@
 import {expect} from './chai';
 import stubI18n from './stubI18n';
+import stubFeatureDetection from './stubFeatureDetection';
 import mockContext from './mockContext';
 
 export {
   expect,
   stubI18n,
+  stubFeatureDetection,
   mockContext
-}
+};
--- node_package/src/utils/index.js
+++ node_package/src/utils/index.js
@@ -3,7 +3,7 @@ import camelize from './camelize';
 import combine from './combine';
 import memoizedSelector, {combine as combineSelectors} from './memoizedSelector';
 import has from './has';
-import preloadBackgroundImage from './preloadBackgroundImage';
+import {preloadImage, preloadBackgroundImage} from './preload';
 
 export {
   camelize,
@@ -16,5 +16,6 @@ export {
 
   has,
 
+  preloadImage,
   preloadBackgroundImage
 };
