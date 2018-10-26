66d9b2f888e9b13b3f1b512f9a8628e2f35f588e
Tim Fischbach <tfischbach@codevise.de>
2018-10-26 17:20:21 +0200

    Add component to preload video poster

---
+++ node_package/src/media/components/__spec__/VideoFilePlayer-spec.jsx
@@ -0,0 +1,30 @@
+import {VideoFilePlayerPreload} from '../VideoFilePlayer';
+
+import {mount} from 'enzyme';
+import sinon from 'sinon';
+import {expect, stubFeatureDetection} from 'support';
+
+describe('VideoFilePlayerPreload', () => {
+  stubFeatureDetection();
+
+  it('calls preloadImage with poster url on mount', () => {
+    const preloadImage = sinon.spy();
+    const videoFile = {urls: {'poster_large': 'poster.png'}};
+
+    mount(<VideoFilePlayerPreload file={videoFile} preloadImage={preloadImage} />);
+
+    expect(preloadImage).to.have.been.calledWith('poster.png');
+  });
+
+  it('prefers custom poster', () => {
+    const preloadImage = sinon.spy();
+    const videoFile = {urls: {'poster_large': 'poster.png'}};
+    const posterImageFile = {urls: {'large': 'custom.png'}};
+
+    mount(<VideoFilePlayerPreload file={videoFile}
+                                  posterImageFile={posterImageFile}
+                                  preloadImage={preloadImage} />);
+
+    expect(preloadImage).to.have.been.calledWith('custom.png');
+  });
+});
