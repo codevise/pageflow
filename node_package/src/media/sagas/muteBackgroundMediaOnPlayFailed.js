13e3ab417a212615c387f984fa6e4df57b759a12
Tim Fischbach <tfischbach@codevise.de>
2018-11-19 11:45:55 +0100

    Rename playMuted action playingMuted
    Match naming of playing event.

--- node_package/src/media/sagas/muteBackgroundMediaOnPlayFailed.js
+++ node_package/src/media/sagas/muteBackgroundMediaOnPlayFailed.js
@@ -1,10 +1,10 @@
-import {PLAY_FAILED, PLAY_MUTED} from '../actions';
+import {PLAY_FAILED, PLAYING_MUTED} from '../actions';
 
 import {takeEvery} from 'redux-saga';
 import {call} from 'redux-saga/effects';
 
 export default function*() {
-  yield takeEvery([PLAY_FAILED, PLAY_MUTED], muteBackgoundAudio);
+  yield takeEvery([PLAY_FAILED, PLAYING_MUTED], muteBackgoundAudio);
 }
 
 function* muteBackgoundAudio() {
