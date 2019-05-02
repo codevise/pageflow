d1d0726da1fb4e98add083487f4611dc90941fd8
Tim Fischbach <tfischbach@codevise.de>
2019-05-02 10:45:10 +0200

    Prevent "element click intercepted" errors in feature specs
    * Wait for loading spinner to disappear in feature spec
    
    * Fix hiding of depublication date drop down.

--- spec/support/dominos/entry.rb
+++ spec/support/dominos/entry.rb
@@ -2,6 +2,11 @@ module Dom
   class Entry < Domino
     selector '#outer_wrapper'
 
+    def wait_until_loading_spinner_disappears
+      wait = Selenium::WebDriver::Wait.new(timeout: 5)
+      wait.until { node.all('.loading_spinner').empty? }
+    end
+
     def go_to_next_page
       node.find('.scroll_indicator').click
     end
