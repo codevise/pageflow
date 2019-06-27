e7b311a1548cdc55b167e7fa3abdd6cfbfa6a480
Awais <abajwa@codevise.de>
2019-06-27 14:14:34 +0200

    added image loading view.

--- node_package/src/loadingSpinner/index.js
+++ node_package/src/loadingSpinner/index.js
@@ -1,7 +1,8 @@
 import {register as registerClassicLoadingSpinner} from './components/ClassicLoadingSpinner';
 import {register as registerTitleLoadingSpinner} from './components/TitleLoadingSpinner';
-
+import {register as registerMediaLoadingSpinner} from './components/MediaLoadingSpinner';
 export function registerWidgetTypes() {
   registerClassicLoadingSpinner();
   registerTitleLoadingSpinner();
+  registerMediaLoadingSpinner();
 }
