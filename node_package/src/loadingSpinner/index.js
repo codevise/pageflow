e6f81c531a7338671b68ac3c2c4fa7730d3a1553
Awais <abajwa@codevise.de>
2019-07-23 14:40:26 +0200

    refactored title and media loading spinners: - changed the heirarchy moved media on top of title - Introduced composition between media and title loading view

--- node_package/src/loadingSpinner/index.js
+++ node_package/src/loadingSpinner/index.js
@@ -1,6 +1,6 @@
 import {register as registerClassicLoadingSpinner} from './components/ClassicLoadingSpinner';
-import {register as registerTitleLoadingSpinner} from './components/TitleLoadingSpinner';
 import {register as registerMediaLoadingSpinner} from './components/MediaLoadingSpinner';
+import {register as registerTitleLoadingSpinner} from './components/TitleLoadingSpinner';
 export function registerWidgetTypes() {
   registerClassicLoadingSpinner();
   registerTitleLoadingSpinner();
