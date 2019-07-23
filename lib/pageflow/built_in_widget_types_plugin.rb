846b2a57644e10f0d2ff2f7939bcb6cb1e0eeb00
Awais <abajwa@codevise.de>
2019-07-23 10:03:09 +0200

    Invert loading screen if the first page is inverted and first page background is used

--- lib/pageflow/built_in_widget_types_plugin.rb
+++ lib/pageflow/built_in_widget_types_plugin.rb
@@ -9,16 +9,16 @@ module Pageflow
                                                .phone_horizontal_slideshow_mode)
       end
 
-      config.widget_types.  ('loading_spinner',
+      config.widget_types.register_widget_defaults('loading_spinner',
                                                    'blur_strength' => 50,
                                                    'remove_logo' => false,
                                                    'invert' => false)
       config.widget_types.register(Pageflow::BuiltInWidgetType.classic_loading_spinner,
                                    default: true)
 
-      config.features.register('title_loading_spinner') do |feature_config|
-        feature_config.widget_types.register(Pageflow::BuiltInWidgetType.title_loading_spinner)
+      config.features.register('media_loading_spinner') do |feature_config|
         feature_config.widget_types.register(Pageflow::BuiltInWidgetType.media_loading_spinner)
+        feature_config.widget_types.register(Pageflow::BuiltInWidgetType.title_loading_spinner)
       end
 
       config.widget_types.register(Pageflow::BuiltInWidgetType.navigation, default: true)
