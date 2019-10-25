a7f03f5607c7ff9b4cbbfa589c9878e062522c8e
Tim Fischbach <tfischbach@codevise.de>
2019-10-25 16:38:36 +0200

    Prevent perma_id clashes for concurrently created revision components
    Use advisory lock to ensure only a single thread/passenger process
    calculates the next perma_id at a time. Otherwise the same maximum
    perma_id can be used to set the perma_ids of two concurrently created
    components.
    
    REDMINE-16809

--- spec/pageflow/revision_component_spec.rb
+++ spec/pageflow/revision_component_spec.rb
@@ -31,7 +31,7 @@ module Pageflow
       end
     end
 
-    describe '#from_perma_ids' do
+    describe '.from_perma_ids' do
       it 'returns list of RevisionComponents' do
         revision = create(:revision)
         revision_component = TestRevisionComponent.create!(revision: revision)
@@ -39,6 +39,76 @@ module Pageflow
         components = TestRevisionComponent.from_perma_ids(revision, [revision_component.perma_id])
 
         expect(components).to eq([revision_component])
+      end
+    end
+
+    describe 'creating records concurrently' do
+      # It is important to use different revisions inside threads to
+      # prevent deadlocks. Each test is wrapped in a
+      # transaction. Creating a revision creates a lock for its row.
+      # Creating a revision component tries to touch the revision. The
+      # update waits for the lock to be released, which does not
+      # happen before the test ends.
+      #
+      # See also
+      # https://dev.mysql.com/doc/refman/5.5/en/innodb-locks-set.html.
+
+      it 'does not assign duplicate perma_ids' do
+        perma_ids = Array.new(3) {
+          Thread.new do
+            revision = create(:revision)
+            TestRevisionComponent.create(revision: revision)
+          end
+        }.map(&:join).map(&:value).map(&:perma_id)
+
+        expect(perma_ids.uniq).to have(3).items
+      end
+
+      it 'acquires advisory lock to prevent perma id generation race condition' do
+        stub_const('Pageflow::RevisionComponent::ADVISORY_LOCK_TIMEOUT_SECONDS', 0)
+
+        revision = create(:revision)
+        component = TestRevisionComponent.new(revision: revision)
+
+        component.during_save_transaction do
+          Thread.new {
+            other_revision = create(:revision)
+            TestRevisionComponent.create(revision: other_revision)
+          }.join
+        end
+
+        expect {
+          component.save
+        }.to raise_error(RevisionComponent::PermaIdGenerationAdvisoryLockTimeout)
+      end
+
+      it 'does not acquire advisory lock if perma_id is already present' do
+        stub_const('Pageflow::RevisionComponent::ADVISORY_LOCK_TIMEOUT_SECONDS', 0)
+
+        revision = create(:revision)
+        component = TestRevisionComponent.new(perma_id: 5, revision: revision)
+
+        component.during_save_transaction do
+          Thread.new {
+            other_revision = create(:revision)
+            TestRevisionComponent.create(revision: other_revision)
+          }.join
+        end
+
+        expect { component.save }.not_to raise_error
+      end
+
+      it 'allows nested creates from same thread' do
+        stub_const('Pageflow::RevisionComponent::ADVISORY_LOCK_TIMEOUT_SECONDS', 0)
+        revision = create(:revision)
+
+        component = TestRevisionComponent.new(revision: revision)
+
+        component.during_save_transaction do
+          TestRevisionComponent.create(revision: revision)
+        end
+
+        expect { component.save }.not_to raise_error
       end
     end
   end
