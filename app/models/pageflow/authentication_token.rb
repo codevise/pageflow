587fce425fbdf73aaaef409dbff659c16684b2cc
Awais <abajwa@codevise.de>
2019-11-11 12:55:01 +0100

    model to store auth tokens

---
+++ app/models/pageflow/authentication_token.rb
@@ -0,0 +1,16 @@
+module Pageflow
+  # AuthenticationToken stores the tokens with expiry time against user and provider
+  class AuthenticationToken < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+    default_scope -> { where(['expiry_time > :expiry OR expiry_time is NULL', expiry: Time.zone.now]) }
+
+    def self.find_auth_token(current_user, provider)
+      where(user_id: current_user.id,
+            provider: provider)
+    end
+
+    def self.user_from_omniauth(auth)
+      User.find_by('email = ?', auth['info']['email'])
+    end
+  end
+end
