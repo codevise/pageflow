f0331ff95a0aed3b347552e276013f461ffa4fa6
Awais <abajwa@codevise.de>
2019-11-12 11:02:00 +0100

    moved oauth initializer to host app

--- config/initializers/oauth.rb
+++
@@ -1,7 +0,0 @@
-require 'omniauth'
-require 'omniauth-facebook'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET_ID']
-end
