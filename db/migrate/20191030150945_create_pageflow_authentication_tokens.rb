0d52be220c113593b5108bac1158ee8e3d23eb05
Awais <abajwa@codevise.de>
2019-11-13 12:50:42 +0100

    store larger strings in auth token

--- db/migrate/20191030150945_create_pageflow_authentication_tokens.rb
+++ db/migrate/20191030150945_create_pageflow_authentication_tokens.rb
@@ -3,7 +3,7 @@ class CreatePageflowAuthenticationTokens < ActiveRecord::Migration[5.2]
     create_table :pageflow_authentication_tokens do |t|
       t.references :user
       t.string :provider
-      t.string :auth_token
+      t.text :auth_token
       t.datetime :expiry_time
 
       t.timestamps
