e9fda342c80f966e66855ddde46512460ba9231c
Tim Fischbach <tfischbach@codevise.de>
2019-11-18 17:36:40 +0100

    Setup eslint for pageflow package

---
+++ packages/pageflow/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": ["import", "jest"],
+  "rules": {
+    "no-unused-vars": [ "warn", {"vars": "all", "args": "none"} ],
+  },
+  "settings": {
+    "import/resolver": {
+      "jest": {
+        "jestConfigFile": "./jest.config.js"
+      }
+    }
+  },
+  "overrides": [
+    {
+      "files": ["spec/**/*.js"],
+      "env": {
+        'jest/globals': true,
+      },
+      "rules": {
+        'jest/expect-expect': 'warn',
+        'jest/no-commented-out-tests': 'warn',
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-export': 'error',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/no-jest-import': 'error',
+        'jest/no-mocks-import': 'error',
+        'jest/no-jasmine-globals': 'warn',
+        'jest/no-standalone-expect': 'error',
+        'jest/no-test-callback': 'error',
+        'jest/no-test-prefixes': 'error',
+        'jest/no-try-expect': 'error',
+        'jest/valid-describe': 'error',
+        'jest/valid-expect': 'error',
+        'jest/valid-expect-in-promise': 'error',
+      },
+    }
+  ]
+};
