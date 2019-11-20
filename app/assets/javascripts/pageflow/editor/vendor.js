9fc9b329e1ceb123d9904fee3afe81422ed73a12
Tim Fischbach <tfischbach@codevise.de>
2019-11-20 10:33:38 +0100

    Extract pageflow/editor/vendor.js from pageflow/editor/base.js
    Use sprockets to compile a JavaScript file that contains the libraries
    used by the Pageflow editor. This can then also be used by other entry
    types.
    
    Empty `pageflow/editor/base.js` and keep it for compatibility with
    host applications. Instead make `pageflow_paged/editor.js` (which is
    the new entry point for the paged editor) require `pageflow/editor` to
    load plugins for paged entry type.

---
+++ app/assets/javascripts/pageflow/editor/vendor.js
@@ -0,0 +1,16 @@
+//= require i18n
+//= require i18n/translations
+
+//= require wysihtml-toolbar
+//= require jquery.minicolors
+
+//= require cocktail
+//= require backbone.marionette
+
+//= require jquery-ui
+//= require jquery/layout
+
+//= require jquery-fileupload/vendor/load-image
+//= require jquery-fileupload/vendor/canvas-to-blob
+//= require jquery-fileupload/jquery.fileupload
+//= require jquery-fileupload/jquery.fileupload-fp
--- entry_types/paged/app/assets/javascripts/pageflow_paged/editor.js
+++ entry_types/paged/app/assets/javascripts/pageflow_paged/editor.js
@@ -1,1 +1,7 @@
+//= require pageflow/editor/vendor
+//= require ./dist/editor
+//= require_self
+
 //= require pageflow/editor
+
+_.extend(pageflow, pageflow_paged);
