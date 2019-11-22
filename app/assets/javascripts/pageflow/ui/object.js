c08eff835fdd79eca8996429bdb85954bf4499bf
Tim Fischbach <tfischbach@codevise.de>
2019-11-22 17:24:39 +0100

    Move legacy frontend js from assets to pageflow package

--- app/assets/javascripts/pageflow/ui/object.js
+++ app/assets/javascripts/pageflow/ui/object.js
@@ -1,5 +1,3 @@
 // pageflow.Object is now part of pageflow/base.js and no longer needs
 // to be required by Pageflow extensions. Keep this file for backwards
 // compatibility.
-
-//= require pageflow/object
