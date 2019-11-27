bff032b47e9574ca8416b2c24942e3c48605e402
Tim Fischbach <tfischbach@codevise.de>
2019-11-27 11:08:43 +0100

    Import presentation example

--- entry_types/scrolled/package/src/frontend/index.js
+++ entry_types/scrolled/package/src/frontend/index.js
@@ -1,1 +1,20 @@
-export {default as Entry} from './Entry';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Entry from './Entry';
+
+import example from './example';
+
+import './global.module.css';
+
+const examples = {
+  example
+}
+
+export default function Example() {
+  return (
+    <Entry examples={examples} defaultExample="example" />
+  );
+}
+
+ReactDOM.render(<Example />, document.getElementById('root'));
