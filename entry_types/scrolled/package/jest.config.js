c103025dbfce427ff63b818c2cf74576c4f2bdac
Tim Fischbach <tfischbach@codevise.de>
2019-11-28 17:41:02 +0100

    Setup ESLint for entry type packages

--- entry_types/scrolled/package/jest.config.js
+++ entry_types/scrolled/package/jest.config.js
@@ -1,8 +1,14 @@
 const {moduleNameMapper, transform} = require('pageflow/config/jest')
 
 module.exports = {
+  // Required to make eslint-import-resolver-jest work with absolute
+  // paths in moduleNameMapper:
+  // https://github.com/JoinColony/eslint-import-resolver-jest/issues/55
+  rootDir: require('path').resolve(__dirname),
+
   testMatch: ["<rootDir>/spec/**/*-spec.js"],
   modulePaths: ["<rootDir>/src"],
+
   moduleNameMapper,
   transform
 };
