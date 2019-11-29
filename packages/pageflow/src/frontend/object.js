ab80c98a26579f2ccbbac1f662d7ff4ce5cc3ce2
Tim Fischbach <tfischbach@codevise.de>
2019-11-29 10:23:05 +0100

    Reuse Object and EntryData between ui and frontend
    `pageflow.Object` should be part of both `pageflow/ui` and the legacy
    frontend.

--- packages/pageflow/src/frontend/object.js
+++ packages/pageflow/src/frontend/object.js
@@ -1,17 +1,3 @@
-import Backbone from 'backbone';
-import _ from 'underscore';
+import Object from '../Object';
 
-(function() {
-  // Class-y constructor by github.com/opensas
-  // https://github.com/jashkenas/backbone/issues/2601
-
-  pageflow.Object = function(options) {
-      this.initialize.apply(this, arguments);
-  };
-  _.extend(pageflow.Object.prototype, Backbone.Events, {
-      initialize: function(options) {}
-  });
-
-  // The self-propagating extend function that Backbone classes use.
-  pageflow.Object.extend = Backbone.Model.extend;
-}());
+pageflow.Object = Object;
