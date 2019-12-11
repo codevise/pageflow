c7c49584f019f6c827daffc19163101d6b7128fe
Tim Fischbach <tfischbach@codevise.de>
2019-12-11 17:26:28 +0100

    Install rollup-plugin-react-svg
    Allow loading svg files as React components.

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import reactSvg from "rollup-plugin-react-svg";
 
 const pageflowPackageRoot = 'packages/pageflow';
 const pageflowPagedEngineRoot = 'entry_types/paged';
@@ -60,7 +61,10 @@ const plugins = [
   }),
   jst(),
   resolve(),
-  commonjs()
+  commonjs(),
+  reactSvg({
+    svgo: {multipass: true}
+  })
 ];
 
 const pageflowPackagePlugins = [
