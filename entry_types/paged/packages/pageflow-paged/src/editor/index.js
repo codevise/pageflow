71f2a574856aa437945083799ef04bd5e957ce58
Tim Fischbach <tfischbach@codevise.de>
2019-12-19 15:05:14 +0100

    Fix page selection view
    Had been extracted to paged entry type engine, but forgotten to be
    imported.

--- entry_types/paged/packages/pageflow-paged/src/editor/index.js
+++ entry_types/paged/packages/pageflow-paged/src/editor/index.js
@@ -2,6 +2,7 @@ import {editor} from 'pageflow/editor';
 
 import {EntryOutlineView} from './views/EntryOutlineView';
 import {EntryPreviewView} from './views/EntryPreviewView';
+import './views/PageSelectionView';
 
 editor.registerEntryType('paged', {
   previewView: EntryPreviewView,
