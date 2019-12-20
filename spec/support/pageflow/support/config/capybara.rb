23a4687fa5020cff0ee654eeb6a5a7d621337297
Tim Fischbach <tfischbach@codevise.de>
2019-12-20 15:35:31 +0100

    Move Capybara config to pageflow-support
    Allow reusing the config required to setup headless Chrome in Capybara
    specs.
    
    REDMINE-17289

---
+++ spec/support/pageflow/support/config/capybara.rb
@@ -0,0 +1,50 @@
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'capybara/chromedriver/logger'
+require 'webdrivers/chromedriver'
+
+Capybara.register_driver :selenium_chrome_headless_no_sandbox do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-gpu'
+  # Required for chrome to work in container based Travis environment
+  # (see https://docs.travis-ci.com/user/chrome)
+  browser_options.args << '--no-sandbox'
+
+  # see https://github.com/dbalatero/capybara-chromedriver-logger/issues/11
+  capabilities = {
+    chromeOptions: {
+      w3c: false
+    }
+  }
+
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 options: browser_options,
+                                 desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless_no_sandbox
+
+Capybara::Chromedriver::Logger.raise_js_errors = true
+Capybara::Chromedriver::Logger.filters = [
+  # Bandwidth probe files are not available in tests
+  /bandwidth_probe.*Failed to load resource/i,
+
+  # React does not like the server rendered "back to top" link inside
+  # page sections.
+  /Target node has markup rendered by React/i,
+
+  # Ignore failure of debounced request to save order of storylines
+  %r{storylines/order - Failed to load resource: the server responded with a status of 401},
+
+  # Ignore failure of debounced request to refresh partials while db
+  # has already been cleaned
+  /partials - Failed to load resource: the server responded with a status of 401/
+]
+
+RSpec.configure do |config|
+  config.after :each, js: true do
+    Capybara::Chromedriver::Logger::TestHooks.after_example!
+  end
+end
