0ab8b94da05c55051d40284c887e2ee0b0af580d
Tim Fischbach <tfischbach@codevise.de>
2020-01-06 10:24:35 +0100

    Only display selects for features registered for entry type
    Do not drop down boxes on the entry's feature tab that were registered
    for a different entry type then the entry's entry type.
    
    REDMINE-17275

---
+++ spec/views/components/pageflow/admin/features_tab_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+module Pageflow
+  describe Admin::FeaturesTab, type: :view_component do
+    before do
+      helper.extend(ActiveAdmin::ViewHelpers)
+      helper.extend(Pageflow::Admin::FormHelper)
+      helper.extend(Pageflow::Admin::FeaturesHelper)
+    end
+
+    it 'displays fields for account features of passed theming' do
+      pageflow_configure do |config|
+        config.features.register('some_feature')
+      end
+
+      theming = create(:theming)
+
+      render(theming)
+
+      expect(rendered).to have_selector('select[name*=some_feature]')
+    end
+
+    it 'displays fields for features of passed entry' do
+      pageflow_configure do |config|
+        config.features.register('some_feature')
+      end
+
+      entry = create(:entry)
+
+      render(entry)
+
+      expect(rendered).to have_selector('select[name*=some_feature]')
+    end
+
+    it 'displays fields for features registed for entry type of passed entry' do
+      some_entry_type = TestEntryType.new(name: 'some')
+
+      pageflow_configure do |config|
+        config.entry_types.register(some_entry_type)
+
+        config.for_entry_type(some_entry_type) do |entry_type_config|
+          entry_type_config.features.register('some_feature')
+        end
+      end
+
+      entry = create(:entry, type_name: 'some')
+
+      render(entry)
+
+      expect(rendered).to have_selector('select[name*=some_feature]')
+    end
+
+    it 'does not display fields for features registered for other entry type' do
+      some_entry_type = TestEntryType.new(name: 'some')
+      other_entry_type = TestEntryType.new(name: 'other')
+
+      pageflow_configure do |config|
+        config.entry_types.register(some_entry_type)
+        config.entry_types.register(other_entry_type)
+
+        config.for_entry_type(other_entry_type) do |entry_type_config|
+          entry_type_config.features.register('some_feature')
+        end
+      end
+
+      entry = create(:entry, type_name: 'some')
+
+      render(entry)
+
+      expect(rendered).not_to have_selector('select[name*=some_feature]')
+    end
+  end
+end
