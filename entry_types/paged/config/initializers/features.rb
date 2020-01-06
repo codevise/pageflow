63cece3822674d8b3326fcbcebcfcc8767a46944
Tim Fischbach <tfischbach@codevise.de>
2020-01-06 15:23:07 +0100

    Extract paged feature flags into paged entry type engine
    Make sure feature flags that only make sense for paged entries are not
    displayed in the admin tab of scrolled entries.
    
    REDMINE-17189

---
+++ entry_types/paged/config/initializers/features.rb
@@ -0,0 +1,16 @@
+Pageflow.configure do |config|
+  config.for_entry_type(PageflowPaged.entry_type) do |entry_type_config|
+    entry_type_config.features.register('auto_change_page')
+    entry_type_config.features.register('delayed_text_fade_in')
+
+    entry_type_config.features.register('storylines') do |feature_config|
+      feature_config.help_entries.register('pageflow.help_entries.storylines', priority: 7)
+    end
+
+    entry_type_config.features.register('editor_emulation_mode')
+    entry_type_config.features.register('waveform_player_controls')
+    entry_type_config.features.register('structured_data')
+
+    entry_type_config.features.enable_by_default('structured_data')
+  end
+end
