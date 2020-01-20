4361a805f8d685a93341f415ed36a5c22ca5d753
Tim Fischbach <tfischbach@codevise.de>
2020-01-20 16:51:06 +0100

    Add SectionThumbnail component
    Allow rendering a preview thumbnail for a single section.
    
    REDMINE-17298

---
+++ entry_types/scrolled/package/spec/frontend/SectionThumbnail-spec.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import {ScrolledEntry} from 'editor/models/ScrolledEntry';
+import {SectionThumbnail} from 'frontend/SectionThumbnail';
+import {watchCollections} from 'entryState';
+
+import {normalizeSeed} from 'support';
+import {render} from '@testing-library/react';
+import 'support/fakeIntersectionObserver';
+import {factories} from 'pageflow/testHelpers';
+
+describe('SectionThumbnail', () => {
+  it('renders section with content elements', () => {
+    const seed = normalizeSeed({
+      sections: [
+        {id: 3, permaId: 1},
+      ],
+      contentElements: [
+        {
+          sectionId: 3,
+          typeName: 'heading',
+          configuration: {children: 'Some Heading'}
+        }
+      ]
+    });
+    const {getByText} = render(<SectionThumbnail seed={seed} sectionPermaId={1} />);
+
+    expect(getByText('Some Heading')).toBeDefined();
+  });
+
+  it('supports subscribing to collection', () => {
+    const entry = factories.entry(ScrolledEntry, {}, {
+      entryTypeSeed: normalizeSeed({
+        sections: [
+          {id: 3, permaId: 1},
+        ],
+        contentElements: [
+          {
+            sectionId: 3,
+            typeName: 'heading',
+            configuration: {children: 'Some Heading'}
+          }
+        ]
+      })
+    });
+
+    const {getByText} = render(<SectionThumbnail
+                                   seed={normalizeSeed()}
+                                   subscribe={dispatch => watchCollections(
+                                       entry,
+                                       {dispatch}
+                                     )}
+                                   sectionPermaId={1} />);
+    expect(getByText('Some Heading')).toBeDefined();
+  });
+});
