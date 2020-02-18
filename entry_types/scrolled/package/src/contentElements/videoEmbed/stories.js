912739dcfd88e6bc0b62805838f4b85399a6512d
Daniel Kuhn <dannikuhn@gmail.com>
2020-02-18 14:32:52 +0100

    Add storybook variants

--- entry_types/scrolled/package/src/contentElements/videoEmbed/stories.js
+++ entry_types/scrolled/package/src/contentElements/videoEmbed/stories.js
@@ -5,5 +5,27 @@ storiesOfContentElement(module, {
   typeName: 'videoEmbed',
   baseConfiguration: {
     videoSource: "https://www.youtube.com/embed/G_-KPFsYMX4"
-  }
+  },
+  variants: [
+    {
+      name: 'With Caption',
+      configuration: {caption: 'Some text here'}
+    },
+    {
+      name: 'Aspect Ratio 16:9',
+      configuration: {aspectRatio: 'wide'}
+    },
+    {
+      name: 'Aspect Ratio 4:3',
+      configuration: {aspectRatio: 'narrow'}
+    },
+    {
+      name: 'Aspect Ratio 1:1',
+      configuration: {aspectRatio: 'square'}
+    },
+    {
+      name: 'Aspect Ratio 9:16',
+      configuration: {aspectRatio: 'portrait'}
+    }
+  ]
 });
