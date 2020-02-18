89c72e562bbfd74fafd19473f0df8b3fde4e5729
Daniel Kuhn <dannikuhn@gmail.com>
2020-02-18 16:57:41 +0100

    Extract configuration-based key to function

--- entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
+++ entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
@@ -5,11 +5,17 @@ import classNames from 'classnames';
 import {InlineCaption} from 'pageflow-scrolled/frontend';
 
 export function VideoEmbed({configuration}) {
+  // base64-encoded configuration
+  // => make component re-render on configuration changes
+  function keyFromConfiguration(config) {
+    return btoa(JSON.stringify(config))
+  }
+
   return (
     <div className={styles.VideoEmbed}>
       <div className={classNames(styles.embedWrapper, styles[configuration.aspectRatio])}>
         <ReactPlayer className={styles.embedPlayer}
-                     key={btoa(JSON.stringify(configuration))}
+                     key={keyFromConfiguration(configuration)}
                      url={configuration.videoSource}
                      playing={true}
                      light={true}
