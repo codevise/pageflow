3b8edab58af1f2c4ca0276013c2017c0f747c169
Tobias Fankhänel <tfankhaenel@codevise.de>
2020-03-02 17:56:57 +0100

    Add editor inputs
    - add translations and help entries
    - add inputs for before/after image and labels, for slider
      parameters (see storybook), and for position. Note: Alt text is set
      in the image's menu, not in the content element's menu
    - for now, we choose defaults so that they resemble
      react-compare-image defaults
    
    REDMINE-17372

--- entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/editor.js
+++ entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/editor.js
@@ -1,8 +1,37 @@
 import {editor} from 'pageflow-scrolled/editor';
+import {ColorInputView, FileInputView} from 'pageflow/editor';
+import {
+  CheckBoxInputView, SelectInputView, SliderInputView, TextInputView
+} from 'pageflow/ui';
 
 editor.contentElementTypes.register('inlineBeforeAfter', {
   configurationEditor() {
     this.tab('general', function() {
+      this.input('before_id', FileInputView, {
+        collection: 'image_files',
+        fileSelectionHandler: 'contentElementConfiguration',
+        positioning: false,
+      });
+      this.input('before_label', TextInputView);
+      this.input('after_id', FileInputView, {
+        collection: 'image_files',
+        fileSelectionHandler: 'contentElementConfiguration',
+        positioning: false,
+      });
+      this.input('after_label', TextInputView);
+      this.input('initial_slider_position', SliderInputView);
+      this.input('slider', CheckBoxInputView);
+      this.input('slider_handle', CheckBoxInputView, {
+        visibleBinding: 'slider',
+      });
+      this.input('slider_color', ColorInputView, {
+        visibleBinding: 'slider',
+      });
+      this.input('position', SelectInputView, {
+        attributeTranslationKeyPrefixes: ['pageflow_scrolled.editor.inputs'],
+        values: ['inline', 'sticky', 'full']
+      });
     });
-  }
+  },
+  defaultConfig: {slider: true, slider_handle: true, initial_slider_position: 50},
 });
