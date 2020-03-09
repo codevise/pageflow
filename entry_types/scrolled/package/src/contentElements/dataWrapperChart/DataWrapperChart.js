bfd48179956dd1a167f9411cbc9005f32d73fed8
Awais <abajwa@codevise.de>
2020-03-09 16:16:24 +0100

    use chart urls without protocol so that correct protocol gets selected based on the parent

--- entry_types/scrolled/package/src/contentElements/dataWrapperChart/DataWrapperChart.js
+++ entry_types/scrolled/package/src/contentElements/dataWrapperChart/DataWrapperChart.js
@@ -9,11 +9,15 @@ import styles from './DataWrapperChart.module.css';
 export function DataWrapperChart({configuration}) {  
   const ref = useRef();
   const onScreen = useOnScreen(ref, '25% 0px 25% 0px');
-  
+  // remove url protocol, so that it is selected by the browser itself
+  var srcURL = '';
+  if (configuration.url && onScreen) {
+    srcURL = configuration.url.replace(/http(s|):/, '');
+  }
   return (
     <div ref={ref} className={styles.container}>
       <iframe 
-        src={onScreen ? configuration.url : ''}
+        src={srcURL}
         scrolling='auto'
         frameBorder='0'
         align='aus'
