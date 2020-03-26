a49181fd93a78f410e7b4e8928098cc6664b4f3c
Awais <abajwa@codevise.de>
2020-03-26 17:48:02 +0100

    added useTextFromMediaFile hook to get text tracks again media file if they are present

--- entry_types/scrolled/package/src/entryState/index.js
+++ entry_types/scrolled/package/src/entryState/index.js
@@ -2,6 +2,7 @@ export {useShareProviders, useShareUrl} from './sharing';
 export {useEntryMetadata} from './metadata';
 export {useEntryStructure, useSectionStructure} from './structure';
 export {useFile} from './useFile';
+export {useTextFromMediaFile} from './useTextFromMediaFile';
 export {useFileRights, useLegalInfo, useCredits} from './legalInfo';
 
 export {EntryStateProvider, useEntryStateDispatch} from './EntryStateProvider';
