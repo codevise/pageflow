d472a9fd11665ddc6108482e3cf53deb7430dd65
Awais <abajwa@codevise.de>
2020-03-26 17:37:04 +0100

    Created a shared fronted package from pageflow javscript package. This package is supposed to contain the source required to be shared with the scrolled package.

---
+++ packages/pageflow/src/sharedFrontend/index.js
@@ -0,0 +1,9 @@
+import '../frontend/base';
+import '../frontend/events';
+import '../frontend/bandwidth';
+import '../frontend/browser';
+import '../frontend/audio_context';
+import '../frontend/video_player';
+import '../frontend/settings';
+
+export const pageflow = window.pageflow;
