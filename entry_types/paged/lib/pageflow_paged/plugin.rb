10563dbde569db9b27c76c0745b79343cce64c68
Tobias Fankhänel <tfankhaenel@codevise.de>
2020-03-27 16:13:41 +0100

    Wrap entry types in features
    REDMINE-17508

--- entry_types/paged/lib/pageflow_paged/plugin.rb
+++ entry_types/paged/lib/pageflow_paged/plugin.rb
@@ -2,7 +2,11 @@ module PageflowPaged
   # @api private
   class Plugin < Pageflow::Plugin
     def configure(config)
-      config.entry_types.register(PageflowPaged.entry_type)
+      config.features.register('paged_entry_type') do |feature_config|
+        feature_config.entry_types.register(PageflowPaged.entry_type)
+      end
+
+      config.features.enable_by_default('paged_entry_type')
     end
   end
 end
