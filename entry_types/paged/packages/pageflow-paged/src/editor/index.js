678b5018cd922028dad4a7136833e29a8abb0e08
Awais <abajwa@codevise.de>
2020-04-06 13:25:45 +0200

    minor changes in pageflow/editor

--- entry_types/paged/packages/pageflow-paged/src/editor/index.js
+++ entry_types/paged/packages/pageflow-paged/src/editor/index.js
@@ -1,11 +1,27 @@
 import {editor} from 'pageflow/editor';
 
 import {PagedEntry} from './models/PagedEntry';
+import {PreviewEntryData} from './models/PreviewEntryData';
+
 
 import {EntryOutlineView} from './views/EntryOutlineView';
 import {EntryPreviewView} from './views/EntryPreviewView';
 import {appearanceInputs} from './helpers/appearanceInputs';
 import './views/PageSelectionView';
+import {state} from '../frontend/state';
+
+export * from 'pageflow/editor';
+export * from './models/PreviewEntryData';
+
+
+state.createEntryData = function () {
+  state.entryData = new PreviewEntryData({
+    entry: state.entry,
+    storylines: state.storylines,
+    chapters: state.chapters,
+    pages: state.pages
+  });
+}
 
 editor.registerEntryType('paged', {
   entryModel: PagedEntry,
@@ -14,5 +30,3 @@ editor.registerEntryType('paged', {
   outlineView: EntryOutlineView,
   appearanceInputs: appearanceInputs
 });
-
-export * from 'pageflow/editor';
