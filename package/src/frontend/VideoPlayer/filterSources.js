7007ef9d0a64ec0243869396034dea5450061dc2
Daniel Kuhn <dannikuhn@gmail.com>
2020-04-20 17:59:52 +0200

    Refactor filterSources to vanilla JS

--- package/src/frontend/VideoPlayer/filterSources.js
+++ package/src/frontend/VideoPlayer/filterSources.js
@@ -1,8 +1,7 @@
 import {browser} from '../browser';
 
-// TODO: Rewrite to vanilla JS
 export const filterSources = function(playerElement) {
-  if (!$(playerElement).is('video')) {
+  if (playerElement.tagName !== 'Video') {
     return playerElement;
   }
 
@@ -10,22 +9,27 @@ export const filterSources = function(playerElement) {
 
   if (browser.has('mp4 support only')) {
     // keep only mp4 source
-    $(playerElement).find('source').not('source[type="video/mp4"]').remove();
+    playerElement.querySelectorAll('source').forEach(source => {
+      if(source.type !== 'video/mp4') {
+        playerElement.removeChild(source);
+      }
+    });
     changed = true;
   }
   else if (browser.has('mse and native hls support')) {
     // remove dash source to ensure hls is used
-    $(playerElement).find('source[type="application/dash+xml"]').remove();
+    const dashSource = playerElement.querySelector('source[type="application/dash+xml"]');
+    playerElement.removeChild(dashSource);
     changed = true;
   }
 
   if (changed) {
     // the video tags initially in the dom are broken since they "saw"
-    // the other sources. replace with clones
-    var clone = $(playerElement).clone(true);
-    $(playerElement).replaceWith(clone);
+    // the other sources. replace with clone
+    var clone = playerElement.cloneNode(true);
+    playerElement.replaceWith(clone);
 
-    return clone[0];
+    return clone;
   }
   else {
     return playerElement;
