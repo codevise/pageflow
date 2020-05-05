2b3448fde664e2225f0998c3cb2788cbe001806d
Awais <abajwa@codevise.de>
2020-05-05 14:57:39 +0200

    replaced Video with VideoPlayer in InlineVideo

--- entry_types/scrolled/package/src/contentElements/inlineVideo/InlineVideo.js
+++ entry_types/scrolled/package/src/contentElements/inlineVideo/InlineVideo.js
@@ -1,21 +1,24 @@
 import React, {useRef} from 'react';
 import classNames from 'classnames';
 
-import {Video, useOnScreen, InlineCaption} from 'pageflow-scrolled/frontend';
+import {VideoPlayer, useOnScreen, InlineCaption} from 'pageflow-scrolled/frontend';
 
 import styles from './InlineVideo.module.css';
 
 export function InlineVideo({configuration}) {
   const ref = useRef();
   const onScreen = useOnScreen(ref, '-50% 0px -50% 0px');
-
+  
   return (
     <div ref={ref} className={classNames(styles.root)}>
       <div style={{paddingTop: configuration.wideFormat ? '41.15%' : '56.25%'}}>
         <div className={styles.inner}>
-          <Video {...configuration}
-                 state={onScreen ? 'active' : 'inactive'}
-                 interactive={true} />
+          <VideoPlayer autoplay={configuration.autoplay}
+                       controls={configuration.controls}
+                       id={configuration.id}
+                       state={onScreen ? 'active' : 'inactive'}
+                       quality={'high'}
+                       interactive={true} />
         </div>
       </div>
       <InlineCaption text={configuration.caption} />
