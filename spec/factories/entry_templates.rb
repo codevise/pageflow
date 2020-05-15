42b4f7c4348e3fefc541bfd351aa275c7bef9b59
Tobias Fankhänel <tfankhaenel@codevise.de>
2020-05-15 12:54:35 +0200

    Add unique and not null constraints to entry templates
    We validate that every entry template has an account and an entry
    type.
    
    Also, for now, there must only be one entry template per account/entry
    type. Later on, there may be multiple per such combination and we
    might introduce something like a human-friendly entry template name.
    
    REDMINE-17306

--- spec/factories/entry_templates.rb
+++ spec/factories/entry_templates.rb
@@ -2,6 +2,7 @@ module Pageflow
   FactoryBot.define do
     factory :entry_template, class: Pageflow::EntryTemplate do
       account
+      entry_type { 'paged' }
       theme_name { 'default' }
     end
   end
