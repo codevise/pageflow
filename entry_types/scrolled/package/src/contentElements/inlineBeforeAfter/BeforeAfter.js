0607719dff44c742d7aabdd16af40a7907347088
Muhammad Usman <musman@codevise.de>
2020-05-19 13:49:05 +0200

    css custom properties replaced.

--- entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/BeforeAfter.js
+++ entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/BeforeAfter.js
@@ -1,20 +1,22 @@
-import React, {useEffect, useRef, useState} from 'react';
-import ReactCompareImage from 'react-compare-image';
-import styles from './BeforeAfter.module.css';
-import cx from 'classnames';
-import {useFile, useEditorSelection} from 'pageflow-scrolled/frontend';
+import React, { useEffect, useRef, useState } from "react";
+import ReactCompareImage from "react-compare-image";
+import styles from "./BeforeAfter.module.css";
+import cx from "classnames";
+import { useFile, useEditorSelection } from "pageflow-scrolled/frontend";
 
-export function BeforeAfter({state,
-                             before_id,
-                             before_label,
-                             after_id,
-                             after_label,
-                             initial_slider_position,
-                             slider,
-                             slider_color,
-                             slider_handle,
-                             contentElementId,
-                            }) {
+export function BeforeAfter({
+  state,
+  before_id,
+  before_label,
+  after_id,
+  after_label,
+  initial_slider_position,
+  position,
+  slider,
+  slider_color,
+  slider_handle,
+  contentElementId,
+}) {
   const beforeAfterRef = useRef();
   const current = beforeAfterRef.current;
 
@@ -26,7 +28,7 @@ export function BeforeAfter({state,
     if (node) {
       // Only wiggle once per element, when it is active for the first
       // time
-      let shouldWiggle = !wiggled && (state === 'active');
+      let shouldWiggle = !wiggled && state === "active";
       setWiggle(shouldWiggle);
       // If wiggle was just set, mark this element as one that already
       // wiggled
@@ -34,81 +36,130 @@ export function BeforeAfter({state,
     }
   }, [state, current]);
 
-  const beforeImage = useFile({collectionName: 'imageFiles', permaId: before_id});
-  const afterImage = useFile({collectionName: 'imageFiles', permaId: after_id});
+  //Since the slider wiggle only the first time, set the variable once for performance purpose.
+  useEffect(() => {
+    if (current) {
+      // Compute initial slider coordinate and pass it as a CSS
+      // variable, so that before/after images can wiggle together with
+      // the slider
+      const containerWidth = current.getBoundingClientRect().width;
+      const initialRectWidth = initialSliderPos * containerWidth;
+      current.style.setProperty("--initial-rect-width", initialRectWidth + "px");
+    }
+  }, [wiggled]);
 
-  const {isSelected} = useEditorSelection({id: contentElementId, type: 'contentElement'});
+  const beforeImage = useFile({
+    collectionName: "imageFiles",
+    permaId: before_id,
+  });
+  const afterImage = useFile({
+    collectionName: "imageFiles",
+    permaId: after_id,
+  });
+
+  const { isSelected } = useEditorSelection({
+    id: contentElementId,
+    type: "contentElement",
+  });
   const beforeImageUrl = beforeImage && beforeImage.urls.large;
   const beforeImageAlt = beforeImage && beforeImage.configuration.alt;
   const afterImageUrl = afterImage && afterImage.urls.large;
   const afterImageAlt = afterImage && afterImage.configuration.alt;
   const initialSliderPos = initial_slider_position / 100;
 
-  //If an image is missing give an arbitrary height to element so it can be selected.
-  if ((!before_id || !after_id) && current) {
-    current.children[0].style.setProperty('height', '500px');
+  if (current) {
+    //If before/left image is missing give a calculated height dependant on the height of right image to element so it can be selected and not vanished.
+    if (!before_id && after_id) {
+      const rightImageWidthHeightRatio = current.children[0].childNodes[0].naturalHeight / current.children[0].childNodes[0].naturalWidth;
+      const idealContainerHeight = current.getBoundingClientRect().width * rightImageWidthHeightRatio;
+      current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+    }
+    //If after/right image is missing give a calculated height dependant on the height of left image to element so it can be selected and not vanished.
+    if (!after_id && before_id) {
+      const leftImageWidthHeightRatio = current.children[0].childNodes[1].naturalHeight / current.children[0].childNodes[1].naturalWidth;
+      const idealContainerHeight = current.getBoundingClientRect().width * leftImageWidthHeightRatio;
+      current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+    }
+
+    current.children[0].style.setProperty('display', 'block');
+    //Add pillarbox classes for fullscreen width and remove otherwise.
+    if (position === 'full') {
+      current.children[0].classList.add(styles.pillarBox);
+      current.classList.add(styles.containerPadding);
+    }
+    else {
+      current.children[0].classList.remove(styles.pillarBox);
+      current.classList.remove(styles.containerPadding);
+    }
+    //add some height if no image is present.
+    if (before_id || after_id) {
+      current.style.removeProperty('height');
+    }
+    else {
+      current.style.setProperty('height', '20px');
+      current.children[0].style.setProperty('height', '0px');
+    }
   }
 
+  //Give inline height
   let opts = {};
   // Transform slider-related props into the format that
   // react-compare-image needs
   if (!slider) {
-    opts = {...opts, sliderLineWidth: 0, handle: <React.Fragment/>};
+    opts = { ...opts, sliderLineWidth: 0, handle: <React.Fragment /> };
   }
   if (slider) {
     if (!slider_handle) {
-      opts = {...opts, handle: <React.Fragment/>};
+      opts = { ...opts, handle: <React.Fragment /> };
     }
     if (slider_color) {
-      opts = {...opts, sliderLineColor: slider_color};
+      opts = { ...opts, sliderLineColor: slider_color };
     }
-  }
-
-  if (current) {
-    // Size labels according to initial slider position. Unit is %,
-    // left and right spacing is 5%, so we subtract 10. 90 = 100 - 10.
-    /*const beforeLabelWidth = initial_slider_position - 10;
-    const afterLabelWidth = 90 - initial_slider_position;*/
-    // Compute initial slider coordinate and pass it as a CSS
-    // variable, so that before/after images can wiggle together with
-    // the slider
-    const containerWidth = current.getBoundingClientRect().width;
-    const initialRectWidth = initialSliderPos * containerWidth;
-    /*current.style.setProperty('--before-label-max-width', beforeLabelWidth + '%');
-    current.style.setProperty('--after-label-max-width', afterLabelWidth + '%');*/
-    current.style.setProperty('--initial-rect-width', initialRectWidth + 'px');
   }
 
   return (
-    <div ref={beforeAfterRef}
-      className={cx({[styles.selected]: isSelected, [styles.wiggle]: wiggle},
-      styles.container)}>
-      <InitialSliderPositionIndicator parentSelected={isSelected}
-        position={initial_slider_position}/>
+    <div
+      ref={beforeAfterRef}
+      className={cx(
+        { [styles.selected]: isSelected, [styles.wiggle]: wiggle },
+        styles.container
+      )}
+    >
+      <InitialSliderPositionIndicator
+        parentSelected={isSelected}
+        position={initial_slider_position}
+      />
       {/* onSliderPositionChange: Prevent wiggle if user uses slider */}
-      <ReactCompareImage leftImage={beforeImageUrl} rightImage={afterImageUrl}
-                         leftImageLabel={before_label} rightImageLabel={after_label}
-                         leftImageAlt={beforeImageAlt} rightImageAlt={afterImageAlt}
-                         sliderPositionPercentage={initialSliderPos}
-                         onSliderPositionChange={() => setWiggle(false)}
-                         {...opts} />
+      <ReactCompareImage
+        leftImage={beforeImageUrl}
+        rightImage={afterImageUrl}
+        leftImageLabel={before_label}
+        rightImageLabel={after_label}
+        leftImageAlt={beforeImageAlt}
+        rightImageAlt={afterImageAlt}
+        sliderPositionPercentage={initialSliderPos}
+        onSliderPositionChange={() => setWiggle(false)}
+        {...opts}
+      />
     </div>
   );
-};
+}
 
-function InitialSliderPositionIndicator({parentSelected, position}) {
-  const indicatorWidth = '2px';
+function InitialSliderPositionIndicator({ parentSelected, position }) {
+  const indicatorWidth = "2px";
   const indicatorStyles = {
-          left: `calc(${position}% - ${indicatorWidth}/2)`,
-          width: `${indicatorWidth}`,
-          height: '100%',
-          borderLeft: '1px solid black',
-          borderRight: '1px solid black',
+    left: `calc(${position}% - ${indicatorWidth}/2)`,
+    width: `${indicatorWidth}`,
+    height: "100%",
+    borderLeft: "1px solid black",
+    borderRight: "1px solid black",
   };
 
   // In case this element is selected, and its initial slider position
   // is not in the middle, we show InitialSliderPositionIndicator
-  return parentSelected && (position !== 50) ?
-      <div className={styles.sliderStart} style={indicatorStyles}/> :
-        '';
+  return parentSelected && position !== 50 ? (
+    <div className={styles.sliderStart} style={indicatorStyles} />
+  ) : (
+    ""
+  );
 }
