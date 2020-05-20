8bfbd92495881c0f1b9d56404af0ac55fe4ffd51
Tim Fischbach <tfischbach@codevise.de>
2020-05-20 15:06:17 +0200

    Allow text, heading and sound disclaimer only inline
    REDMINE-17624

--- entry_types/scrolled/package/src/contentElements/heading/editor.js
+++ entry_types/scrolled/package/src/contentElements/heading/editor.js
@@ -2,6 +2,8 @@ import {editor} from 'pageflow-scrolled/editor';
 import {TextInputView} from 'pageflow/ui';
 
 editor.contentElementTypes.register('heading', {
+  supportedPositions: ['inline'],
+
   configurationEditor() {
     this.tab('general', function() {
       this.input('children', TextInputView);
