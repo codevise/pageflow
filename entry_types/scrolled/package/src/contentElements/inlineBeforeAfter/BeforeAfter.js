db535b3ceb6cb4e091b2472f52d384077d68f309
Muhammad Usman <musman@codevise.de>
2020-05-20 13:35:45 +0200

    fixed container height issue.

--- entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/BeforeAfter.js
+++ entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/BeforeAfter.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
-import ReactCompareImage from "react-compare-image";
-import styles from "./BeforeAfter.module.css";
-import cx from "classnames";
-import { useFile, useEditorSelection } from "pageflow-scrolled/frontend";
+import React, { useEffect, useRef, useState } from 'react';
+import ReactCompareImage from 'react-compare-image';
+import styles from './BeforeAfter.module.css';
+import cx from 'classnames';
+import { useFile, useEditorSelection } from 'pageflow-scrolled/frontend';
 
 export function BeforeAfter({
   state,
@@ -22,13 +22,14 @@ export function BeforeAfter({
 
   var [wiggled, setWiggled] = useState(false);
   var [wiggle, setWiggle] = useState(false);
+  var [containerHeight, setContainerHeight] = useState(true);
 
   useEffect(() => {
     var node = current;
     if (node) {
       // Only wiggle once per element, when it is active for the first
       // time
-      let shouldWiggle = !wiggled && state === "active";
+      let shouldWiggle = !wiggled && state === 'active';
       setWiggle(shouldWiggle);
       // If wiggle was just set, mark this element as one that already
       // wiggled
@@ -44,22 +45,22 @@ export function BeforeAfter({
       // the slider
       const containerWidth = current.getBoundingClientRect().width;
       const initialRectWidth = initialSliderPos * containerWidth;
-      current.style.setProperty("--initial-rect-width", initialRectWidth + "px");
+      current.style.setProperty('--initial-rect-width', initialRectWidth + 'px');
     }
   }, [wiggled]);
 
   const beforeImage = useFile({
-    collectionName: "imageFiles",
+    collectionName: 'imageFiles',
     permaId: before_id,
   });
   const afterImage = useFile({
-    collectionName: "imageFiles",
+    collectionName: 'imageFiles',
     permaId: after_id,
   });
 
   const { isSelected } = useEditorSelection({
     id: contentElementId,
-    type: "contentElement",
+    type: 'contentElement',
   });
   const beforeImageUrl = beforeImage && beforeImage.urls.large;
   const beforeImageAlt = beforeImage && beforeImage.configuration.alt;
@@ -67,39 +68,53 @@ export function BeforeAfter({
   const afterImageAlt = afterImage && afterImage.configuration.alt;
   const initialSliderPos = initial_slider_position / 100;
 
-  if (current) {
-    //If before/left image is missing give a calculated height dependant on the height of right image to element so it can be selected and not vanished.
-    if (!before_id && after_id) {
-      const rightImageWidthHeightRatio = current.children[0].childNodes[0].naturalHeight / current.children[0].childNodes[0].naturalWidth;
-      const idealContainerHeight = current.getBoundingClientRect().width * rightImageWidthHeightRatio;
-      current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+  //add some height if no image is present.
+  useEffect (() => {
+    if (current) {
+      if (!containerHeight) {
+        current.style.removeProperty('height');
+      }
+      else {
+        current.style.setProperty('height', '20px');
+        current.children[0].style.setProperty('height', '0px');
+      }
     }
-    //If after/right image is missing give a calculated height dependant on the height of left image to element so it can be selected and not vanished.
-    if (!after_id && before_id) {
-      const leftImageWidthHeightRatio = current.children[0].childNodes[1].naturalHeight / current.children[0].childNodes[1].naturalWidth;
-      const idealContainerHeight = current.getBoundingClientRect().width * leftImageWidthHeightRatio;
-      current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+  }, [current, containerHeight]);
+
+  useEffect(() => {
+    if (current) {
+      //If before/left image is missing give a calculated height dependant on the height of right image to element so it can be selected and not vanished.
+      if (!beforeImage && afterImage) {
+        setContainerHeight(false);
+        const rightImageWidthHeightRatio = afterImage.height / afterImage.width;
+        const idealContainerHeight = current.getBoundingClientRect().width * rightImageWidthHeightRatio;
+        current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+      }
+      //If after/right image is missing give a calculated height dependant on the height of left image to element so it can be selected and not vanished.
+      if (!afterImage && beforeImage) {
+        setContainerHeight(false);
+        const leftImageWidthHeightRatio = beforeImage.height / beforeImage.width;
+        const idealContainerHeight = current.getBoundingClientRect().width * leftImageWidthHeightRatio;
+        current.children[0].style.setProperty('height', `${idealContainerHeight}px`);
+      }
+  
+      current.children[0].style.setProperty('display', 'block');
+      //Add pillarbox classes for fullscreen width and remove otherwise.
+      if (position === 'full' && (beforeImage || afterImage)) {
+        setContainerHeight(false);
+        current.children[0].classList.add(styles.pillarBox);
+        current.style.setProperty('padding-top', '56.25%');
+      }
+      else {
+        current.children[0].classList.remove(styles.pillarBox);
+        current.style.removeProperty('padding-top', '56.25%');
+      }
     }
 
-    current.children[0].style.setProperty('display', 'block');
-    //Add pillarbox classes for fullscreen width and remove otherwise.
-    if (position === 'full') {
-      current.children[0].classList.add(styles.pillarBox);
-      current.classList.add(styles.containerPadding);
+    if (!beforeImage && !afterImage) {
+      setContainerHeight(true);
     }
-    else {
-      current.children[0].classList.remove(styles.pillarBox);
-      current.classList.remove(styles.containerPadding);
-    }
-    //add some height if no image is present.
-    if (before_id || after_id) {
-      current.style.removeProperty('height');
-    }
-    else {
-      current.style.setProperty('height', '20px');
-      current.children[0].style.setProperty('height', '0px');
-    }
-  }
+  }, [beforeImage, afterImage, position]);
 
   //Give inline height
   let opts = {};
@@ -146,13 +161,13 @@ export function BeforeAfter({
 }
 
 function InitialSliderPositionIndicator({ parentSelected, position }) {
-  const indicatorWidth = "2px";
+  const indicatorWidth = '2px';
   const indicatorStyles = {
     left: `calc(${position}% - ${indicatorWidth}/2)`,
     width: `${indicatorWidth}`,
-    height: "100%",
-    borderLeft: "1px solid black",
-    borderRight: "1px solid black",
+    height: '100%',
+    borderLeft: '1px solid black',
+    borderRight: '1px solid black',
   };
 
   // In case this element is selected, and its initial slider position
@@ -160,6 +175,6 @@ function InitialSliderPositionIndicator({ parentSelected, position }) {
   return parentSelected && position !== 50 ? (
     <div className={styles.sliderStart} style={indicatorStyles} />
   ) : (
-    ""
+    ''
   );
 }
