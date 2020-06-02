8237a5e2df9d56bea6229e364a7ab621f0eeb1f4
Tim Fischbach <tfischbach@codevise.de>
2020-06-02 13:28:07 +0200

    Improve chart content element
    * Use `isPrepared` to optionally render iframe.
    
    * Deactivate pointer events in editor, when not selected, to ensure
      element can be selected.
    
    * Set background color to make newly inserted element visible.
    
    REDMINE-17388

--- entry_types/scrolled/package/src/contentElements/dataWrapperChart/DataWrapperChart.js
+++ entry_types/scrolled/package/src/contentElements/dataWrapperChart/DataWrapperChart.js
@@ -1,29 +1,42 @@
-import React, {useRef} from 'react';
-import classNames from 'classnames';
+import React from 'react';
 
-import {Text, useOnScreen} from 'pageflow-scrolled/frontend';
+import {
+  useContentElementLifecycle,
+  useContentElementEditorState
+} from 'pageflow-scrolled/frontend';
 
 import styles from './DataWrapperChart.module.css';
 
+export function DataWrapperChart({configuration}) {
+  const {isPrepared} = useContentElementLifecycle();
+  const {isEditable, isSelected} = useContentElementEditorState();
 
-export function DataWrapperChart({configuration}) {  
-  const ref = useRef();
-  const onScreen = useOnScreen(ref, '25% 0px 25% 0px');
   // remove url protocol, so that it is selected by the browser itself
   var srcURL = '';
-  if (configuration.url && onScreen) {
+  if (configuration.url && isPrepared) {
     srcURL = configuration.url.replace(/http(s|):/, '');
   }
   return (
-    <div ref={ref} className={styles.container} data-percy="hide">
-      <iframe
-        src={srcURL}
-        scrolling='auto'
-        frameBorder='0'
-        align='aus'
-        allowFullScreen={true}
-        mozallowfullscreen='true'
-        webkitallowfullscreen='true' />
+    <div className={styles.container}
+         style={{pointerEvents: isEditable && !isSelected ? 'none' : undefined}}
+         data-percy="hide">
+      {renderIframe(srcURL)}
     </div>
   );
+}
+
+function renderIframe(url) {
+  if (!url) {
+    return null;
+  }
+
+  return (
+    <iframe src={url}
+            scrolling='auto'
+            frameBorder='0'
+            align='aus'
+            allowFullScreen={true}
+            mozallowfullscreen='true'
+            webkitallowfullscreen='true' />
+    )
 }
