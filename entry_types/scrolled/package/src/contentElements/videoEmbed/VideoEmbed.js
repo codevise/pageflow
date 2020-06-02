91b9424d6beb798b51cc924a1175592e3890f1d1
Tim Fischbach <tfischbach@codevise.de>
2020-06-02 14:48:16 +0200

    Improve video embed content element
    * Ensure element can be selected in editor.
    
    * Use `isPrepared` state to lazy load player.
    
    REDMINE-17386

--- entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
+++ entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
@@ -2,38 +2,58 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import styles from './VideoEmbed.module.css';
 import classNames from 'classnames';
-import {InlineCaption} from 'pageflow-scrolled/frontend';
+
+import {
+  InlineCaption,
+  useContentElementLifecycle,
+  useContentElementEditorState
+} from 'pageflow-scrolled/frontend';
 
 export function VideoEmbed({configuration}) {
+  const {isPrepared} = useContentElementLifecycle();
+  const {isEditable, isSelected} = useContentElementEditorState();
+
   // base64-encoded configuration
   // => make component re-render on configuration changes
   function keyFromConfiguration(config) {
     return btoa(JSON.stringify(config))
   }
 
+  function renderPlayer() {
+    if (!isPrepared) {
+      return null;
+    }
+
+    return (
+      <ReactPlayer className={styles.embedPlayer}
+                   key={keyFromConfiguration(configuration)}
+                   url={configuration.videoSource}
+                   playing={true}
+                   light={true}
+                   width='100%'
+                   height='100%'
+                   controls={!configuration.hideControls}
+                   config={{
+                     youtube: {
+                       playerVars: {
+                         showinfo: !configuration.hideInfo
+                       }
+                     },
+                     vimeo: {
+                       playerOptions: {
+                         byline: !configuration.hideInfo
+                       }
+                     }
+                   }} />
+    );
+  }
+
   return (
     <div className={styles.VideoEmbed}>
-      <div className={classNames(styles.embedWrapper, styles[configuration.aspectRatio])}>
-        <ReactPlayer className={styles.embedPlayer}
-                     key={keyFromConfiguration(configuration)}
-                     url={configuration.videoSource}
-                     playing={true}
-                     light={true}
-                     width='100%'
-                     height='100%'
-                     controls={!configuration.hideControls}
-                     config={{
-                       youtube: {
-                         playerVars: {
-                           showinfo: !configuration.hideInfo
-                         }
-                       },
-                       vimeo: {
-                         playerOptions: {
-                           byline: !configuration.hideInfo
-                         }
-                       }
-                     }} />
+
+      <div className={classNames(styles.embedWrapper, styles[configuration.aspectRatio])}
+           style={{pointerEvents: isEditable && !isSelected ? 'none' : undefined}}>
+        {renderPlayer()}
       </div>
       <InlineCaption text={configuration.caption} />
     </div>
