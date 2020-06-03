c67537c6153fc1c42f92985d906af5372e42f705
Tim Fischbach <tfischbach@codevise.de>
2020-06-03 17:32:08 +0200

    Improve AudioPlayer/VideoPlayer specs
    * Remove test cases for partial or extra url templates since those do
      not mirror real life scenarios.
    
    * Extract `requiredProps` to make tests less verbose.
    
    * Change seed helper to take parameters so that relevant data is
      visible in each test.

--- entry_types/scrolled/package/spec/frontend/VideoPlayer-spec.js
+++ entry_types/scrolled/package/spec/frontend/VideoPlayer-spec.js
@@ -13,60 +13,59 @@ describe('VideoPlayer', () => {
     jest.clearAllMocks();
   });
 
-  let getVideoSeed = ()=>{
+  function getVideoFileSeed({
+    id = 1,
+    permaId = 100,
+    basename = 'video'
+  } = {}) {
     return {
       fileUrlTemplates: {
         videoFiles: {
-          high: ':id_partition/video.mp4'
+          high: ':id_partition/:basename.mp4'
         }
       },
       videoFiles: [
-        {id: 1, permaId: 100, isReady: true}
+        {id, permaId, isReady: true, basename}
       ]
     };
   }
+
+  function requiredProps() {
+    return {
+      playerState: getInitialPlayerState(),
+      playerActions: getPlayerActions()
+    };
+  }
+
   it('renders video with provided file id', () => {
-    let state = getInitialPlayerState();
-    let actions = getPlayerActions();
     const result =
-      renderInEntry(<VideoPlayer id={100} playerState={state} playerActions={actions} />, {
-        seed: getVideoSeed()
+      renderInEntry(<VideoPlayer {...requiredProps()} id={100} />, {
+        seed: getVideoFileSeed({permaId: 100})
       });
+
     expect(result.container.querySelector('video')).toBeDefined();
   });
 
   it('passes correct sources to media API', () => {
-    let state = getInitialPlayerState();
-    let actions = getPlayerActions();
     const spyMedia = jest.spyOn(media, 'getPlayer');
-    renderInEntry(<VideoPlayer id={100} playerState={state} playerActions={actions} />, {
-      seed: getVideoSeed()
+
+    renderInEntry(<VideoPlayer {...requiredProps()} id={100} />, {
+      seed: getVideoFileSeed({
+        basename: 'video',
+        id: 1,
+        permaId: 100
+      })
     });
+
     expect(spyMedia).toHaveBeenCalledWith(
-      expect.objectContaining([ { type: 'video/mp4', src: '000/000/001/video.mp4?u=1' } ]),
+      [{type: 'video/mp4', src: '000/000/001/video.mp4?u=1'}],
       expect.anything()
     );
   });
 
-  it('does not accept any source other than "high" ', () => {
-    let state = getInitialPlayerState();
-    let actions = getPlayerActions();
+  it('without id no media player is request', () => {
     const spyMedia = jest.spyOn(media, 'getPlayer');
-    let videoSeed = getVideoSeed();
-    videoSeed.fileUrlTemplates.videoFiles['mp3'] = '000/000/001/video.mp3?u=1'
-    renderInEntry(<VideoPlayer id={100} playerState={state} playerActions={actions} />, {
-      seed: videoSeed
-    });
-    expect(spyMedia).toHaveBeenCalledWith(
-      expect.not.objectContaining([ { type: 'video/mp3', src: '000/000/001/video.mp3?u=1' } ]),
-      expect.anything()
-    );
-  });
-  
-  it('without sources no media player is request', () => {
-    const spyMedia = jest.spyOn(media, 'getPlayer');
-    renderInEntry(<VideoPlayer />);
+    renderInEntry(<VideoPlayer {...requiredProps()} />);
     expect(spyMedia).not.toHaveBeenCalled();
   });
-
 });
