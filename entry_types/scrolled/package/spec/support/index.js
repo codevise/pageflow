ca855e4a4834f6fbeeec437ef9d290a6985ed2d8
Tim Fischbach <tfischbach@codevise.de>
2020-06-05 08:43:39 +0200

    Add renderInEntryWithSectionLifecycle test helper
    Allow controlling result of `useSectionLifecycle` hook to simulate
    scrolling components in and out of the viewport. This approach gives
    more control than using fake intersection observers since it can
    simulate elements being near the vieport or inside but not in the
    center.
    
    REDMINE-17548

--- entry_types/scrolled/package/spec/support/index.js
+++ entry_types/scrolled/package/spec/support/index.js
@@ -3,5 +3,6 @@ export * from './factories';
 export * from './fakeWindows';
 export * from './normalizeSeed';
 export * from './rendering';
+export * from './scrollPositionLifecycle';
 export * from './tick';
 export * from './useFakeXhr';
---
+++ entry_types/scrolled/package/spec/support/scrollPositionLifecycle.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from 'react';
+import BackboneEvents from 'backbone-events-standalone';
+import {act} from '@testing-library/react'
+
+import {renderInEntry} from './rendering';
+import {SectionLifecycleContext} from 'frontend/useSectionLifecycle';
+
+export function renderInEntryWithSectionLifecycle(ui, options = {}) {
+  const emitter = createEmitter();
+
+  return withSimulateScrollPositionHelper(
+    emitter,
+    renderInEntry(ui, {
+      wrapper: createScrollPositionProvider(SectionLifecycleContext, emitter),
+      ...options
+    })
+  );
+}
+
+function createScrollPositionProvider(Context, emitter) {
+  return function ScrollPositionProvider({children}) {
+    const [value, setValue] = useState({isPrepared: false, isVisible: false, isActive: false});
+
+    useEffect(() => {
+      function handle(scrollPosition) {
+        switch (scrollPosition) {
+        case 'near viewport':
+          setValue({isPrepared: true, isVisible: false, isActive: false});
+          break;
+        case 'in viewport':
+          setValue({isPrepared: true, isVisible: true, isActive: false});
+          break;
+        case 'center of viewport':
+          setValue({isPrepared: true, isVisible: true, isActive: true});
+          break;
+        default:
+          setValue({isPrepared: false, isVisible: false, isActive: false});
+          break;
+        }
+      }
+
+      emitter.on('scroll', handle);
+
+      return () => emitter.off('scroll', handle);
+    })
+
+    return (
+      <Context.Provider value={value}>
+        {children}
+      </Context.Provider>
+    );
+  };
+}
+
+const allowedScrollPositions = ['outside viewport', 'near viewport', 'in viewport', 'center of viewport'];
+
+function withSimulateScrollPositionHelper(emitter, result) {
+  return {
+    ...result,
+
+    simulateScrollPosition(scrollPosition) {
+      if (!allowedScrollPositions.includes(scrollPosition)) {
+        throw new Error(`Invalid scrollPosition '${scrollPosition}'. ` +
+                        `Allowed values: ${allowedScrollPositions.join(', ')}`)
+      }
+
+      act(() => {
+        emitter.trigger('scroll', scrollPosition)
+      });
+    }
+  }
+}
+
+function createEmitter() {
+  return {...BackboneEvents};
+}
