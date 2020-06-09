159f35dfe97a4ed387cc0dc5742063d269fd5f25
Tim Fischbach <tfischbach@codevise.de>
2020-06-09 12:55:17 +0200

    Lazy load images in external links
    REDMINE-17687

--- entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLinkList.js
+++ entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLinkList.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {useContentElementLifecycle} from 'pageflow-scrolled/frontend';
 import {ExternalLink} from './ExternalLink';
 import styles from './ExternalLinkList.module.css';
 
 export function ExternalLinkList(props) {
-  var linkList = props.configuration.links || [];
+  const linkList = props.configuration.links || [];
+  const {isPrepared} = useContentElementLifecycle();
 
   return (
     <div className={styles.ext_links_container}>
       {linkList.map(function(link, index){
-        return <ExternalLink {...link} key={link.id} sectionProps={props.sectionProps}/>
+        return <ExternalLink {...link} key={link.id} sectionProps={props.sectionProps} isPrepared={isPrepared} />
       })}
     </div>
   );
