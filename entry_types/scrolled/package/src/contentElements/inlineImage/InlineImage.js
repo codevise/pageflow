1fcbe2d7b5958272d4662e7768144703bbf52e97
Tim Fischbach <tfischbach@codevise.de>
2020-06-09 12:54:50 +0200

    Lazy load inline images
    REDMINE-17687

--- entry_types/scrolled/package/src/contentElements/inlineImage/InlineImage.js
+++ entry_types/scrolled/package/src/contentElements/inlineImage/InlineImage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import {Image, InlineCaption} from 'pageflow-scrolled/frontend';
+import {Image, InlineCaption, useContentElementLifecycle} from 'pageflow-scrolled/frontend';
 
 import styles from './InlineImage.module.css';
 
 export function InlineImage({configuration}) {
+  const {isPrepared} = useContentElementLifecycle();
+
   return (
     <div className={classNames(styles.root)}>
       <div className={styles.container}>
         <div className={styles.spacer}>
           <div className={styles.inner}>
-            <Image {...configuration} />
+            <Image {...configuration} isPrepared={isPrepared} />
           </div>
         </div>
       </div>
--- entry_types/scrolled/package/src/contentElements/inlineImage/frontend.js
+++ entry_types/scrolled/package/src/contentElements/inlineImage/frontend.js
@@ -2,5 +2,6 @@ import {frontend} from 'pageflow-scrolled/frontend';
 import {InlineImage} from './InlineImage';
 
 frontend.contentElementTypes.register('inlineImage', {
-  component: InlineImage
+  component: InlineImage,
+  lifecycle: true
 });
