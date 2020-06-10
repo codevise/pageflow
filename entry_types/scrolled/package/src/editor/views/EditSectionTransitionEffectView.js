dff8c1ae924cc23db3158d7af7c87856eb9c3aa5
Muhammad Usman <musman@codevise.de>
2020-06-10 17:47:29 +0200

    Highlight the current selected transition

--- entry_types/scrolled/package/src/editor/views/EditSectionTransitionEffectView.js
+++ entry_types/scrolled/package/src/editor/views/EditSectionTransitionEffectView.js
@@ -1,14 +1,11 @@
-
 import $ from 'jquery';
 import I18n from 'i18n-js';
 import Marionette from 'backbone.marionette';
 import _ from 'underscore';
-//import {app, cssModulesUtils} from 'pageflow/editor';
 
 import {inputView} from 'pageflow/ui';
 
 import styles from './EditSectionTransitionEffectView.module.css';
-
 
 export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
   mixins: [inputView],
@@ -18,7 +15,7 @@ export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
         <span class="name"></span>
         <span class="inline_help"></span>
     </label>
-    <div class="radio_buttons_container" />
+    <div class="transitions_container" />
     `,
 
   events: {
@@ -27,7 +24,7 @@ export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
 
   ui: {
     label: 'label',
-    container: ".radio_buttons_container"
+    container: ".transitions_container"
   },
 
   initialize: function() {
@@ -38,10 +35,16 @@ export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
                 return translationKeyPrefix + '.transition.values.' + value;
             }, this);
         }
-  
+
         this.options.texts = _.map(this.options.translationKeys, function(key) {
           return I18n.t(key);
         });
+    }
+
+    if (!this.options.groups) {
+      this.options.groups = _.map(this.options.groupTanslationKeys, function(key) {
+        return I18n.t(key);
+      });
     }
   },
 
@@ -49,24 +52,22 @@ export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
     this.ui.label.attr('for', this.cid);
     this.appendOptions();
     this.load();
-    //this.select();
     this.listenTo(this.model, 'change:' + this.options.propertyName, this.load);
   },
 
   appendOptions: function () {
     _.each(this.options.values, function(value, index) {
       var option = `<div class="${styles.transition} ${this.options.optionDisabled(value) ? styles['disabled'] : 'enabled'}">
-                   <div class="${styles[value]}"><div class="${styles.upper_section}">A</div><div class="${styles.lower_section}">B</div></div>
-                   <label><input type="radio" value="${value}" name="transitions"/>
-                   ${this.options.texts[index]}</label></div>`;
+                      <div class="${styles[value]}">
+                        <div class="${styles.upper_section}">A</div>
+                        <div class="${styles.lower_section}">B</div>
+                      </div>
+                      <label>
+                        <input type="radio" value="${value}" name="transitions"  ${this.options.optionDisabled(value) ? 'disabled' : ''}"/>
+                        ${this.options.texts[index]}
+                      </label>
+                    </div>`;
       this.ui.container.append($(option));
-        
-    //   if (this.options.optionDisabled &&
-    //     this.options.optionDisabled(value)) {
-    //   //option.setAttribute('disabled', true);
-    //   option.classList.add('disable');
-    // }
-    // console.log(this);
     }, this);
   },
 
@@ -80,28 +81,24 @@ export const EditSectionTransitionEffectView = Marionette.ItemView.extend({
     this.model.set(this.options.propertyName, configured.attr('value'));
   },
 
-  // select: function() {
-  //   var select = {};
-  //   _.each(this.ui.container.find('div'), function(div) {
-  //       if ($(div).hasClass(styles.active)) {
-  //           select = $(div);
-  //       }
-  //       else {
-  //           $(div).context.parentElement.classList.remove(styles.active);          
-  //       }
-  //   });
-  //   //select.context.parentElement.classList.add(styles.active);
-  //   console.log(select);
-  //},
-
   load: function() {
     if (!this.isClosed) {
-      _.each(this.options.values, function(value) {
-        this.ui.container
-          .find('input[value="'+value+'"]')
-          .prop('checked', this.model.get(this.options.propertyName)[value]);
-      }, this);
+      var value = this.model.get(this.options.propertyName);
+
+      if (this.model.has(this.options.propertyName) &&
+          this.ui.container.find('input[value="' + value +'"]:not([checked])').length) {
+
+        var select = {};
+        _.each(this.ui.container.find('div'), function(div) {
+                if (div.classList.contains(styles[value])) {
+                    select = $(div);
+                }
+                else {
+                    $(div).context.parentElement.classList.remove(styles.active);
+                }
+            });
+        select.context.parentElement.classList.add(styles.active);
+      }
     }
   }
 });
-
