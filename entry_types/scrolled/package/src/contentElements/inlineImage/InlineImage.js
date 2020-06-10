29bcb2843a1dc81cc9bcbe9c883a8603bbb5e0a5
Tim Fischbach <tfischbach@codevise.de>
2020-06-10 11:48:27 +0200

    Display pillar boxes for full width inline images
    Prevent inline images from becoming taller than the viewport.
    
    REDMINE-17708

--- entry_types/scrolled/package/src/contentElements/inlineImage/InlineImage.js
+++ entry_types/scrolled/package/src/contentElements/inlineImage/InlineImage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import {Image, InlineCaption, useContentElementLifecycle} from 'pageflow-scrolled/frontend';
+import {
+  Image,
+  InlineCaption,
+  ViewportDependentPillarBoxes,
+  useContentElementLifecycle
+} from 'pageflow-scrolled/frontend';
 
 import styles from './InlineImage.module.css';
 
@@ -9,16 +14,17 @@ export function InlineImage({configuration}) {
   const {isPrepared} = useContentElementLifecycle();
 
   return (
-    <div className={classNames(styles.root)}>
-      <div className={styles.container}>
-        <div className={styles.spacer}>
-          <div className={styles.inner}>
-            <Image {...configuration}
-                   isPrepared={isPrepared}
-                   variant={configuration.position === 'full' ?  'large' : 'medium'} />
+    <div className={classNames(styles.root, {[styles.box]: configuration.position !== 'full'})}>
+      <ViewportDependentPillarBoxes aspectRatio={0.75}
+                                    position={configuration.position}>
+          <div className={styles.spacer}>
+            <div className={styles.inner}>
+              <Image {...configuration}
+                     isPrepared={isPrepared}
+                     variant={configuration.position === 'full' ?  'large' : 'medium'} />
+            </div>
           </div>
-        </div>
-      </div>
+      </ViewportDependentPillarBoxes>
       <InlineCaption text={configuration.caption} />
     </div>
   )
