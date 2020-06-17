419ada23f111fa75f7dabddf1d0552cd22a810a2
Tim Fischbach <tfischbach@codevise.de>
2020-06-17 14:29:54 +0200

    Add aria label to play/pause button
    Improve accessibilty and get rid of some `data-testid` attributes.
    
    REDMINE-17673

--- entry_types/scrolled/package/src/frontend/PlayerControls/PlayPauseButton.js
+++ entry_types/scrolled/package/src/frontend/PlayerControls/PlayPauseButton.js
@@ -3,10 +3,16 @@ import controlBarStyles from "./ControlBar.module.css";
 import PlayIcon from "../assets/images/playerControls/play_arrow_24px.svg";
 import PauseIcon from "../assets/images/playerControls/pause_24px.svg";
 
+import {useI18n} from '../i18n';
+
 export function PlayPauseButton(props) {
+  const {t} = useI18n();
+
   return (
     <a className={controlBarStyles.playControl}
        href="#"
+       aria-label={t(props.isPlaying ? 'pause' : 'play',
+                     {scope: 'pageflow_scrolled.public.player_controls'})}
        onClick={clickHandler(props)}>
       {pausePlayIcon(props)}
     </a>
@@ -15,11 +21,9 @@ export function PlayPauseButton(props) {
 
 function pausePlayIcon(props) {
   if (props.isPlaying) {
-    return <PauseIcon className={controlBarStyles.controlsIcon}
-                      data-testid="pause-icon"/>
+    return <PauseIcon className={controlBarStyles.controlsIcon} />
   } else {
-    return <PlayIcon className={controlBarStyles.controlsIcon}
-                     data-testid="play-icon"/>
+    return <PlayIcon className={controlBarStyles.controlsIcon} />
   }
 }
 
