45088894d949dd5bfa8d93406a0114817d33b341
Tim Fischbach <tfischbach@codevise.de>
2020-06-18 12:03:51 +0200

    Rewrite player controls progress to hooks

--- entry_types/scrolled/package/src/frontend/PlayerControls/ProgressIndicators.js
+++ entry_types/scrolled/package/src/frontend/PlayerControls/ProgressIndicators.js
@@ -1,80 +1,56 @@
-import React from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import {DraggableCore} from 'react-draggable';
 import Measure from 'react-measure';
 import classNames from 'classnames';
 
 import styles from './ProgressIndicators.module.css';
 
-export class ProgressIndicators extends React.Component {
-  constructor(props) {
-    super(props);
+export function ProgressIndicators({currentTime, duration, bufferedEnd, scrubTo, seekTo}) {
+  const [dragging, setDragging] = useState();
+  const progressBarsContainerWidth = useRef();
 
-    this.state = {
-      progressBarsContainerWidth: null,
-      dragging: false
-    };
-
-    this.measureProgressIndicatorsContainer = (width) => {
-      this.setState({progressBarsContainerWidth: width});
-    };
-
-    this.handleStop = (mouseEvent, dragEvent) => {
-      this.setState({dragging: false});
-      props.seekTo(this.positionToTime(dragEvent.x));
-    };
-
-    this.handleDrag = (mouseEvent, dragEvent) => {
-      this.setState({dragging: true});
-      props.scrubTo(this.positionToTime(dragEvent.x));
-    };
-  }
-
-  loadProgress() {
-    return this.props.duration > 0 ? (this.props.bufferedEnd / this.props.duration) : 0;
-  }
-
-  playProgress() {
-    return this.props.duration > 0 ? (this.props.currentTime / this.props.duration) : 0;
-  }
-
-  handlePosition() {
-    if (this.state.progressBarsContainerWidth) {
-      return ((this.state.progressBarsContainerWidth) * this.playProgress());
-    } else {
-      return toPercent(this.playProgress());
-    }
-  }
-
-  positionToTime(x) {
-    if (this.props.duration && this.state.progressBarsContainerWidth) {
-      const fraction = Math.max(0, Math.min(1, x / this.state.progressBarsContainerWidth));
-      return fraction * this.props.duration;
+  const positionToTime = useCallback((x) => {
+    if (duration && progressBarsContainerWidth.current) {
+      const fraction = Math.max(0, Math.min(1, x / progressBarsContainerWidth.current));
+      return fraction * duration;
     } else {
       return 0;
     }
-  }
+  }, [duration]);
 
-  render() {
-    return (
-      <div className={classNames(styles.container, {[styles.dragging]: this.state.dragging})}>
+  const handleStop = useCallback((mouseEvent, dragEvent) => {
+    setDragging(false);
+    seekTo(positionToTime(dragEvent.x));
+  }, [seekTo, positionToTime]);
+
+  const handleDrag = useCallback((mouseEvent, dragEvent) => {
+    setDragging(true);
+    scrubTo(positionToTime(dragEvent.x));
+  }, [scrubTo, positionToTime]);
+
+  const loadProgress = duration > 0 ? (bufferedEnd / duration) : 0;
+  const playProgress = duration > 0 ? (currentTime / duration) : 0;
+
+  return (
+    <div className={classNames(styles.container, {[styles.dragging]: dragging})}
+         tabIndex="0">
+      <div className={styles.wrapper}>
         <Measure client
-                 onResize={contentRect => {
-                     this.measureProgressIndicatorsContainer(contentRect.client.width)
-                   }}>
+                 onResize={contentRect => progressBarsContainerWidth.current = contentRect.client.width}>
           {({measureRef}) =>
-            <DraggableCore onStart={this.handleDrag}
-                           onDrag={this.handleDrag}
-                           onStop={this.handleStop}>
+            <DraggableCore onStart={handleDrag}
+                           onDrag={handleDrag}
+                           onStop={handleStop}>
               <div className={classNames(styles.draggable)}>
                 <div ref={measureRef} className={styles.bars}>
                   <div className={styles.loadingProgressBar}
-                       style={{width: toPercent(this.loadProgress())}}
+                       style={{width: toPercent(loadProgress)}}
                        data-testid="loading-progress-bar"/>
                   <div className={styles.playProgressBar}
-                       style={{width: toPercent(this.playProgress())}}
+                       style={{width: toPercent(playProgress)}}
                        data-testid="play-progress-bar"/>
                   <div className={styles.sliderHandle}
-                       style={{left: this.handlePosition()}}
+                       style={{left: toPercent(playProgress)}}
                        data-testid="slider-handle"/>
                 </div>
               </div>
@@ -82,8 +58,8 @@ export class ProgressIndicators extends React.Component {
           }
         </Measure>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 function toPercent(value) {
