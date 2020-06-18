6a64cab0a8441ee985e4531e6e46db9d89932b97
Tim Fischbach <tfischbach@codevise.de>
2020-06-18 11:16:10 +0200

    Make seeking work in player controls storybook

--- entry_types/scrolled/package/src/frontend/__stories__/playerControls-stories.js
+++ entry_types/scrolled/package/src/frontend/__stories__/playerControls-stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {storiesOf} from '@storybook/react';
 
 import {PlayerControls} from 'pageflow-scrolled/frontend';
@@ -12,13 +12,27 @@ function addControlbarStory(name, type, style) {
       return (
         <div style={{fontFamily: 'Source Sans Pro, sans-serif'}}>
           <div style={{background: 'rgba(0, 0, 0, 0.6)', width: '100%', height: '150px'}}></div>
-          <PlayerControls type={type} style={style}/>
+          <PlayerControlsDemo type={type} style={style}/>
         </div>
       );
     },
     {
       percy: {skip: false}
     }
+  );
+}
+
+function PlayerControlsDemo(props) {
+  const [currentTime, setCurrentTime] = useState(200);
+  const [scrubbingAt, setScrubbingAt] = useState();
+
+  return (
+    <PlayerControls {...props}
+                    duration={500}
+                    bufferedEnd={300}
+                    currentTime={scrubbingAt === undefined ? currentTime : scrubbingAt}
+                    scrubTo={setScrubbingAt}
+                    seekTo={time => { setScrubbingAt(undefined); setCurrentTime(time) }} />
   );
 }
 
