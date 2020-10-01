fd6d672f44ff62f7c754412ea1c7255505b1e140
Tim Fischbach <tfischbach@codevise.de>
2020-10-01 17:22:11 +0200

    Store motif area in section configuration
    Store most recently selected motif area in file configuration and use
    it as a default when assigning a file as a backdrop to prevent having
    to select the same area over again when using a file in a different
    section.
    
    REDMINE-17966

--- entry_types/scrolled/package/spec/editor/models/SectionConfiguration-spec.js
+++ entry_types/scrolled/package/spec/editor/models/SectionConfiguration-spec.js
@@ -1,4 +1,9 @@
-import {SectionConfiguration} from 'editor/models/SectionConfiguration';
+import 'pageflow-scrolled/editor';
+import {SectionConfiguration, FileSelectionHandler} from 'editor/models/SectionConfiguration';
+import {ScrolledEntry} from 'editor/models/ScrolledEntry';
+
+import {factories, setupGlobals} from 'pageflow/testHelpers';
+import {useFakeXhr, normalizeSeed} from 'support';
 
 describe('SectionConfiguration', () => {
   describe('get backdropType', () => {
@@ -101,6 +106,22 @@ describe('SectionConfiguration', () => {
     });
   });
 
+  describe('set backdropImageMotifArea', () => {
+    it('sets backdrop imageMotifArea to value', () => {
+      const sectionConfiguration = new SectionConfiguration();
+      const motifArea = {left: 1, top: 1, width: 10, height: 10};
+
+      sectionConfiguration.set('backdropType', 'image');
+      sectionConfiguration.set('backdropImage', 1);
+      sectionConfiguration.set('backdropImageMotifArea', motifArea);
+
+      expect(sectionConfiguration.attributes.backdrop).toEqual({
+        image: 1,
+        imageMotifArea: motifArea
+      });
+    });
+  });
+
   describe('set backdropImageMobile', () => {
     it('sets backdrop imageMobile to value', () => {
       const sectionConfiguration = new SectionConfiguration();
@@ -151,6 +172,22 @@ describe('SectionConfiguration', () => {
     });
   });
 
+  describe('set backdropImageMobileMotifArea', () => {
+    it('sets backdrop imageMobileMotifArea to value', () => {
+      const sectionConfiguration = new SectionConfiguration();
+      const motifArea = {left: 1, top: 1, width: 10, height: 10};
+
+      sectionConfiguration.set('backdropType', 'image');
+      sectionConfiguration.set('backdropImageMobile', 1);
+      sectionConfiguration.set('backdropImageMobileMotifArea', motifArea);
+
+      expect(sectionConfiguration.attributes.backdrop).toEqual({
+        imageMobile: 1,
+        imageMobileMotifArea: motifArea
+      });
+    });
+  });
+
   describe('set backdropVideo', () => {
     it('overrides backdrop image settings when setting backdrop video', () => {
       const sectionConfiguration = new SectionConfiguration();
@@ -163,6 +200,22 @@ describe('SectionConfiguration', () => {
 
       expect(sectionConfiguration.attributes.backdrop).toEqual({
         'video': 1
+      });
+    });
+  });
+
+  describe('set backdropVideoMotifArea', () => {
+    it('sets backdrop videoMotifArea to value', () => {
+      const sectionConfiguration = new SectionConfiguration();
+      const motifArea = {left: 1, top: 1, width: 10, height: 10};
+
+      sectionConfiguration.set('backdropType', 'video');
+      sectionConfiguration.set('backdropVideo', 1);
+      sectionConfiguration.set('backdropVideoMotifArea', motifArea);
+
+      expect(sectionConfiguration.attributes.backdrop).toEqual({
+        video: 1,
+        videoMotifArea: motifArea
       });
     });
   });
@@ -198,6 +251,94 @@ describe('SectionConfiguration', () => {
         image: 1,
         imageMobile: 2
       });
+    });
+
+    it('restores previous motif area', () => {
+      const sectionConfiguration = new SectionConfiguration();
+      const motifAreaA = {left: 1, top: 1, width: 10, height: 10};
+      const motifAreaB = {left: 2, top: 2, width: 20, height: 0};
+
+      sectionConfiguration.set('backdropType', 'image');
+      sectionConfiguration.set('backdropImage', 1);
+      sectionConfiguration.set('backdropImageMotifArea', motifAreaA);
+      sectionConfiguration.set('backdropType', 'video');
+      sectionConfiguration.set('backdropVideo', 2);
+      sectionConfiguration.set('backdropVideoMotifArea', motifAreaB);
+      sectionConfiguration.set('backdropType', 'image');
+
+      expect(sectionConfiguration.attributes.backdrop).toEqual({
+        image: 1,
+        imageMotifArea: motifAreaA
+      });
+    });
+  });
+
+  describe('FileSelectionHandler', () => {
+    let testContext;
+
+    beforeEach(() => {
+      const entry = factories.entry(ScrolledEntry, {}, {
+        fileTypes: factories.fileTypesWithImageFileType(),
+        filesAttributes: {
+          image_files: [
+            {
+              id: 1,
+              configuration: {
+                motifArea: {left: 10, top: 20, width: 50, height: 40}
+              }
+            },
+            {
+              id: 2
+            }
+          ]
+        },
+        entryTypeSeed: normalizeSeed({
+          sections: [
+            {
+              id: 10,
+              configuration: {backdropType: 'image'}
+            }
+          ]
+        })
+      });
+
+      testContext = {
+        entry,
+        imageFileWithMotifArea: entry.getFileCollection('image_files').get(1),
+        imageFileWithoutMotifArea: entry.getFileCollection('image_files').get(2),
+        section: entry.sections.first()
+      };
+    });
+
+    setupGlobals({
+      entry: () => testContext.entry
+    });
+
+    useFakeXhr(() => testContext);
+
+    it('copies default motif area from file to section configuration', () => {
+      const fileSelectionHandler = new FileSelectionHandler({
+        entry: testContext.entry,
+        id: 10,
+        attributeName: 'backdropImage'
+      });
+      fileSelectionHandler.call(testContext.imageFileWithMotifArea);
+
+      expect(testContext.section.configuration.get('backdrop').imageMotifArea).toEqual({
+        left: 10, top: 20, width: 50, height: 40
+      });
+    });
+
+    it('resets motif area from in section configuration when file changes', () => {
+      const fileSelectionHandler = new FileSelectionHandler({
+        entry: testContext.entry,
+        id: 10,
+        attributeName: 'backdropImage'
+      });
+      fileSelectionHandler.call(testContext.imageFileWithMotifArea);
+      fileSelectionHandler.call(testContext.imageFileWithoutMotifArea);
+
+      expect(testContext.section.configuration.get('backdrop').imageMotifArea).toBeUndefined();
     });
   });
 });
--- entry_types/scrolled/package/spec/editor/views/EditMotifAreaDialogView-spec.js
+++ entry_types/scrolled/package/spec/editor/views/EditMotifAreaDialogView-spec.js
@@ -1,6 +1,7 @@
 import {EditMotifAreaDialogView} from 'editor/views/EditMotifAreaDialogView';
 
 import jQuery from 'jquery';
+import Backbone from 'backbone';
 
 import {getByText, fireEvent} from '@testing-library/dom';
 import {useFakeTranslations} from 'pageflow/testHelpers';
@@ -12,20 +13,21 @@ describe('EditMotifAreaDialogView', () => {
     'pageflow_scrolled.editor.edit_motif_area.reset': 'Reset'
   });
 
-  it('ready motif area from model using derived property name', () => {
+  it('reads motif area from model using derived property name', () => {
+    const model = new Backbone.Model({
+      imageMotifArea: {
+        left: 5, top: 10, width: 25, height: 50
+      }
+    });
     const file = factories.imageFile({
       width: 200,
-      height: 100,
-      configuration: {
-        motifArea: {
-          left: 5, top: 10, width: 25, height: 50
-        }
-      }
+      height: 100
     });
 
     const view = new EditMotifAreaDialogView({
+      model,
       file,
-      propertyName: 'imageId'
+      propertyName: 'image'
     });
 
     const imgAreaSelect = fakeImgAreaSelect();
@@ -36,12 +38,14 @@ describe('EditMotifAreaDialogView', () => {
     expect(imgAreaSelect.getSelection()).toEqual({x1: 10, y1: 10, x2: 60, y2: 60});
   });
 
-  it('allows setting the motif area property', () => {
+  it('allows setting motif area property in model and default motif area in file', () => {
+    const model = new Backbone.Model();
     const file = factories.imageFile({width: 200, height: 100});
 
     const view = new EditMotifAreaDialogView({
+      model,
       file,
-      propertyName: 'imageId'
+      propertyName: 'image'
     });
 
     const imgAreaSelect = fakeImgAreaSelect();
@@ -52,6 +56,12 @@ describe('EditMotifAreaDialogView', () => {
     imgAreaSelect.select({x1: 10.01, y1: 10.02, x2: 60, y2: 60});
     fireEvent.click(getByText(view.el, 'Save'));
 
+    expect(model.get('imageMotifArea')).toEqual({
+      left: 5,
+      top: 10,
+      width: 25,
+      height: 50
+    });
     expect(file.configuration.get('motifArea')).toEqual({
       left: 5,
       top: 10,
@@ -61,6 +71,11 @@ describe('EditMotifAreaDialogView', () => {
   });
 
   it('allows resetting the motif area property', () => {
+    const model = new Backbone.Model({
+      imageMotifArea: {
+        left: 5, top: 10, width: 25, height: 50
+      }
+    });
     const file = factories.imageFile({
       width: 200,
       height: 100,
@@ -72,8 +87,9 @@ describe('EditMotifAreaDialogView', () => {
     });
 
     const view = new EditMotifAreaDialogView({
+      model,
       file,
-      propertyName: 'imageId'
+      propertyName: 'image'
     });
 
     view.render();
@@ -82,6 +98,7 @@ describe('EditMotifAreaDialogView', () => {
     fireEvent.click(getByText(view.el, 'Reset'));
     fireEvent.click(getByText(view.el, 'Save'));
 
+    expect(model.get('imageMotifArea')).toBe(null);
     expect(file.configuration.get('motifArea')).toBe(null);
   });
 });
