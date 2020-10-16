af07cf48f0cbb86226433c9bab3f31971badbce3
Tim Fischbach <tfischbach@codevise.de>
2020-10-16 16:35:40 +0200

    Add docs and story for motif area handling
    Explain goals, approach and challanges of motif area logic.
    
    REDMINE-17865

--- entry_types/scrolled/package/spec/support/stories.js
+++ entry_types/scrolled/package/spec/support/stories.js
@@ -245,7 +245,7 @@ export function exampleHeading({sectionId, text}) {
 
 const lorem = 'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr. Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.';
 
-export function exampleTextBlock({sectionId}) {
+export function exampleTextBlock({sectionId, text = lorem}) {
   return {
     sectionId,
     typeName: 'textBlock',
@@ -254,7 +254,7 @@ export function exampleTextBlock({sectionId}) {
         {
           type: 'paragraph',
           children: [
-            {text: lorem}
+            {text}
           ]
         }
       ]
