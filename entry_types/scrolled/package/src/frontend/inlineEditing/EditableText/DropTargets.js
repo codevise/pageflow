a2f53868fa72b12ea54baa10b7e63a9c035aa662
Tim Fischbach <tfischbach@codevise.de>
2020-10-29 09:38:08 +0100

    Render custom drop targets for text blocks
    To allow moving content elements between paragraphs (or other
    children) of a text block, we need to render custom drop targets. Soon
    as an element is dragged over a text block, we measure the children
    and cover the element with divs for each "gap" beween two text block
    children. That way dragging an element to the lower half of child or
    the upper half of the next child results in the same drop indicator
    being displayed between the two children.
    
    REDMINE-17331

---
+++ entry_types/scrolled/package/src/frontend/inlineEditing/EditableText/DropTargets.js
@@ -0,0 +1,122 @@
+import React, {useEffect, useRef, useState} from 'react';
+import classNames from 'classnames';
+import {Node} from 'slate';
+import {useSlate, ReactEditor} from 'slate-react';
+import {useDrop} from 'react-dnd';
+
+import {postMoveContentElementMessage} from '../postMessage';
+
+import styles from './DropTargets.module.css';
+
+export function DropTargets({contentElementId}) {
+  const editor = useSlate();
+  const [targets, setTargets] = useState();
+  const containerRef = useRef();
+
+  useEffect(() => {
+    if (!targets) {
+      setTargets(measureHeights(editor, containerRef.current));
+    }
+  }, [targets, editor]);
+
+  return (
+    <div ref={containerRef} className={styles.container}>
+      {renderDropTargets(targets || [], contentElementId)}
+    </div>
+  );
+}
+
+function renderDropTargets(targets, contentElementId) {
+  function handleDrop(itemId, index) {
+    if (index === 0) {
+      postMoveContentElementMessage({
+        id: itemId,
+        to: {
+          at: 'before',
+          id: contentElementId
+        }
+      });
+    }
+    else if (index === targets.length - 1) {
+      postMoveContentElementMessage({
+        id: itemId,
+        to: {
+          at: 'after',
+          id: contentElementId
+        }
+      });
+    }
+    else {
+      postMoveContentElementMessage({
+        id: itemId,
+        to: {
+          at: 'split',
+          id: contentElementId,
+          splitPoint: index
+        }
+      });
+    }
+  }
+
+  return targets.map((target, index) =>
+    <DropTarget {...target}
+                key={index}
+                onDrop={itemId => handleDrop(itemId, index)} />
+  );
+}
+
+function DropTarget({top, height, indicatorTop, onDrop}) {
+  const [{isOver}, drop] = useDrop({
+    accept: 'contentElement',
+    collect: monitor => ({
+      isOver: monitor.isOver()
+    }),
+    drop: item => onDrop(item.id)
+  });
+
+  return (
+    <div ref={drop}
+         className={classNames(styles.dropTarget, {[styles.isOver]: isOver})}
+         style={{top, height}}>
+      <div className={styles.dropIndicator}
+           style={{top: indicatorTop}}/>
+    </div>
+  );
+}
+
+function measureHeights(editor, container) {
+  const containerRect = container.getBoundingClientRect();
+  let lastTargetDimensions = {top: 0, height: 0};
+  let lastRectBottom = 0;
+
+  const targetDimensions = editor.children.map((child, index) => {
+    const node = Node.get(editor, [index]);
+    const domNode = ReactEditor.toDOMNode(editor, node);
+    const rect = domNode.getBoundingClientRect();
+
+    const top = lastTargetDimensions.top + lastTargetDimensions.height;
+    const bottom = rect.top + rect.height / 2 - containerRect.top;
+
+    const targetDimensions = {
+      top,
+      height: bottom - top,
+      indicatorTop: index > 0 ?
+                    lastRectBottom + (rect.top - lastRectBottom) / 2 - containerRect.top - top :
+                    0
+    };
+
+    lastRectBottom = rect.bottom;
+    lastTargetDimensions = targetDimensions;
+
+    return targetDimensions;
+  });
+
+  return [
+    ...targetDimensions,
+    {
+      top: lastTargetDimensions.top + lastTargetDimensions.height,
+      height: containerRect.height - (lastTargetDimensions.top + lastTargetDimensions.height),
+      indicatorTop: containerRect.height - (lastTargetDimensions.top + lastTargetDimensions.height)
+    }
+  ];
+}
