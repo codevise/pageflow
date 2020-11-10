b616652f84edbc30b5a7a75854e28a0ce0850f34
Awais <abajwa@codevise.de>
2020-11-10 11:58:15 +0100

    Turn navigation bar logo into url if logo_url theme option is provided

--- entry_types/scrolled/package/src/frontend/navigation/Logo.js
+++ entry_types/scrolled/package/src/frontend/navigation/Logo.js
@@ -7,14 +7,15 @@ import styles from './AppHeader.module.css';
 
 export function Logo() {
   const theme = useTheme();
-
   return (
-    <picture>
-      <source media="(max-width: 780px)" srcSet={theme.assets.logoMobile} />
-      <source media="(min-width: 781px)" srcSet={theme.assets.logoDesktop} />
-      <img src={theme.assets.logoDesktop}
-           alt={theme.options.logoAltText}
-           className={classNames(styles.menuIcon, styles.logo)}/>
-    </picture>
+    <a href={theme.options.logoUrl}>
+      <picture>
+        <source media="(max-width: 780px)" srcSet={theme.assets.logoMobile} />
+        <source media="(min-width: 781px)" srcSet={theme.assets.logoDesktop} />
+        <img src={theme.assets.logoDesktop}
+            alt={theme.options.logoAltText}
+            className={classNames(styles.menuIcon, styles.logo)}/>
+      </picture>
+    </a>
   );
 }
