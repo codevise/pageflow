0798fddcf2af90a462832c163ff6decd32d84708
Tim Fischbach <tfischbach@codevise.de>
2020-11-12 11:00:51 +0100

    Use focus-within to open tooltips
    Compared to the previous JS based approach this has a couple of
    advantages:
    
    * Clicking outside of a tooltip closes it. Before, a tooltip could
      only be closed by clicking the icon again.
    
    * Closing other tooltips is taken care of automatically since opening
      a tooltip means the other tooltip loses focus.
    
    * Less JS logic, less things to test.
    
    * Also works in non-JS case.
    
    As a result, tooltips now already open when the menu icon is
    focused. Given that we have a skip link to skip the whole navigation
    this seems ok to me.
    
    Remove some unneeded code:
    
    * `hasTrigger` was always true.
    
    * Use CSS parent selector to make icons of expanded tooltips blue
      instead of cloning children and setting an additional class.
    
    REDMINE-17849

--- entry_types/scrolled/package/spec/frontend/PageflowTooltip-spec.js
+++ entry_types/scrolled/package/spec/frontend/PageflowTooltip-spec.js
@@ -1,10 +1,9 @@
 import React from "react"
-import {render as testingLibraryRenderer, fireEvent} from '@testing-library/react'
+import {render as testingLibraryRenderer} from '@testing-library/react'
 
 import {PageflowTooltip} from "frontend/PageflowTooltip";
 
 describe("PageflowTooltip", () => {
-  
   const render = props =>
     testingLibraryRenderer(
       <PageflowTooltip fixed={props.fixed} content={props.content} closeOther={props.closeOther} classWhenOpen={props.classWhenOpen}>
@@ -12,80 +11,10 @@ describe("PageflowTooltip", () => {
       </PageflowTooltip>
     )
 
-  const renderWithoutChildren = props =>
-    testingLibraryRenderer(<PageflowTooltip fixed={props.fixed} content={props.content} closeOther={props.closeOther}/>)
-
   it("should render the content", () => {
     const wrapper = render({
-      content: <p data-testid='tooltip_content'>tooltip content</p>,
-      fixed: true
+      content: <p data-testid='tooltip_content'>tooltip content</p>
     })
     expect(wrapper.getByTestId('tooltip_content')).toBeDefined();
   });
-
-  it("should not render the content when fixed is false", () => {
-    const wrapper = render({
-      content: <p data-testid='tooltip_content'>tooltip content</p>,
-      fixed: false
-    })
-    expect(wrapper.queryByTestId('tooltip_content')).toBeNull();
-  });
-
-  it("should render the content when trigger is clicked", () => {
-    const wrapper = render({
-      content: <p data-testid='tooltip_content'>tooltip content</p>,
-      fixed: false
-    })
-    expect(wrapper.queryByTestId('tooltip_content')).toBeNull();
-    
-    fireEvent.click(wrapper.queryByTestId('tooltip_trigger'));
-
-    expect(wrapper.queryByTestId('tooltip_content')).toBeDefined();
-  });
-
-  it("should render the content without children", () => {
-    const wrapper = renderWithoutChildren({
-      content: <p data-testid='tooltip_content'>tooltip content</p>,
-      fixed: true
-    })
-    expect(wrapper.getByTestId('tooltip_content')).toBeDefined();
-  });
-
-  it("should apply the given class to content when expanded", () => {
-    const wrapper = render({
-      content: <p data-testid='tooltip_content'>tooltip content</p>,
-      fixed: false,
-      classWhenOpen: 'classWhenOpen'
-    })    
-    let trigger = wrapper.queryByTestId('tooltip_trigger')
-    fireEvent.click(trigger);
-    expect(trigger.className).toBe('classWhenOpen')
-    
-  });
-
-  it("should close other tooltips when clicked", () => {
-    const wrapper1 = render({
-      content: <p data-testid='tooltip_content1'>tooltip content</p>,
-      fixed: false,
-      triggerId: 'tooltip_trigger1',
-      closeOther: true
-    });
-    const wrapper2 = render({
-      content: <p data-testid='tooltip_content2'>tooltip content</p>,
-      fixed: false,
-      triggerId: 'tooltip_trigger2',
-      closeOther: true
-    });
-    
-    fireEvent.click(wrapper1.queryByTestId('tooltip_trigger1'));
-    expect(wrapper1.queryByTestId('tooltip_content1')).toBeDefined();
-    expect(wrapper2.queryByTestId('tooltip_content2')).toBeNull();
-    
-    fireEvent.click(wrapper2.queryByTestId('tooltip_trigger2'));
-    expect(wrapper2.queryByTestId('tooltip_content2')).toBeDefined();
-    expect(wrapper1.queryByTestId('tooltip_content1')).toBeNull();
-  });
-  
-
-
-})
+});
