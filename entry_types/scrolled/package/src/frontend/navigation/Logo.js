82d91ae83cdb26f5185377380586d4a34a332147
Tim Fischbach <tfischbach@codevise.de>
2020-11-12 16:35:54 +0100

    Display focus outline for logo
    REDMINE-18044

--- entry_types/scrolled/package/src/frontend/navigation/Logo.js
+++ entry_types/scrolled/package/src/frontend/navigation/Logo.js
@@ -8,13 +8,15 @@ import styles from './AppHeader.module.css';
 export function Logo() {
   const theme = useTheme();
   return (
-    <a target="_blank" rel="noopener noreferrer" href={theme.options.logoUrl}>
+    <a target="_blank"
+       rel="noopener noreferrer"
+       href={theme.options.logoUrl}
+       className={classNames(styles.menuIcon, styles.logo)}>
       <picture>
         <source media="(max-width: 780px)" srcSet={theme.assets.logoMobile} />
         <source media="(min-width: 781px)" srcSet={theme.assets.logoDesktop} />
         <img src={theme.assets.logoDesktop}
-            alt={theme.options.logoAltText}
-            className={classNames(styles.menuIcon, styles.logo)}/>
+             alt={theme.options.logoAltText} />
       </picture>
     </a>
   );
