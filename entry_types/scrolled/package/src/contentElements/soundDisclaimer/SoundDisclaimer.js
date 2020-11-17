d503a9750da3038404557973a3d0fd156d28def5
Tim Fischbach <tfischbach@codevise.de>
2020-11-17 07:52:04 +0100

    Improve sound disclaimer
    Display mute/unmute icon and change text when sound has been
    activated.
    
    REDMINE-18075

--- entry_types/scrolled/package/src/contentElements/soundDisclaimer/SoundDisclaimer.js
+++ entry_types/scrolled/package/src/contentElements/soundDisclaimer/SoundDisclaimer.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import {useI18n} from 'pageflow-scrolled/frontend';
 import classNames from 'classnames';
+import {useI18n, useMediaMuted, useContentElementEditorState} from 'pageflow-scrolled/frontend';
 import {media} from 'pageflow/frontend';
 import styles from './SoundDisclaimer.module.css';
 
+import MutedIcon from './muted.svg';
+import UnmutedIcon from './unmuted.svg';
+
 export function SoundDisclaimer() {
+  const {isEditable, isSelected} = useContentElementEditorState();
   const {t} = useI18n();
+  const muted = useMediaMuted();
 
   return (
     <div className={classNames(styles.soundDisclaimer)}
-         onClick={() => media.mute(false)}>
-      <p dangerouslySetInnerHTML={{__html: t('pageflow_scrolled.public.sound_disclaimer.help_text')}} />
+         style={{pointerEvents: isEditable && !isSelected ? 'none' : undefined}}>
+      <button className={classNames(styles.unmute, {[styles.active]: muted})}
+              onClick={() => media.mute(false)}>
+        <MutedIcon />
+        <p dangerouslySetInnerHTML={{__html: t('pageflow_scrolled.public.sound_disclaimer.help_muted')}} />
+      </button>
+      <div className={classNames(styles.unmuted, {[styles.active]: !muted})}>
+        <UnmutedIcon />
+        <p dangerouslySetInnerHTML={{__html: t('pageflow_scrolled.public.sound_disclaimer.help_unmuted')}} />
+      </div>
     </div>
   );
 }
