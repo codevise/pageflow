cf43f367c04577c60044fba084d83781f4cabc67
Tim Fischbach <tfischbach@codevise.de>
2020-11-17 08:38:35 +0100

    Improve handling of untitled chapters
    * Display chapters if there is more than one or if the only chapter
      either has title or summary.
    
    * Hide mobile burger button when there are no chapters.
    
    * Display fallback title for untitled chapters.
    
    * Display tooltip only if summary is present.
    
    REDMINE-18074

--- entry_types/scrolled/package/src/frontend/navigation/ChapterLink.js
+++ entry_types/scrolled/package/src/frontend/navigation/ChapterLink.js
@@ -4,14 +4,31 @@ import styles from "./ChapterLink.module.css";
 
 import {Tooltip} from '../Tooltip';
 import {useI18n} from '../i18n';
+import {isBlank, presence} from '../utils/blank';
 
 export function ChapterLink(props) {
   const {t} = useI18n();
 
+  const item = (
+    <div>
+      <a className={classNames(styles.chapterLink, {[styles.chapterLinkActive]: props.active})}
+         href={`#chapter-${props.permaId}`}
+         onClick={() => props.handleMenuClick(props.chapterLinkId)}>
+        {presence(props.title) || t('pageflow_scrolled.public.navigation.chapter', {number: props.chapterIndex})}
+      </a>
+      {!isBlank(props.summary) && <p className={styles.summary}
+                                     dangerouslySetInnerHTML={{__html: props.summary}} />}
+    </div>
+  );
+
+  if (isBlank(props.summary)) {
+    return item;
+  }
+
   const content = (
     <>
       <h3 className={styles.tooltipHeadline}>
-        {t('pageflow_scrolled.public.navigation.chapter')} {props.chapterIndex}
+        {t('pageflow_scrolled.public.navigation.chapter', {number: props.chapterIndex})}
       </h3>
       <p dangerouslySetInnerHTML={{__html: props.summary}} />
     </>
@@ -19,15 +36,7 @@ export function ChapterLink(props) {
 
   return (
     <Tooltip content={content} openOnHover={true} highlight={true} bubbleClassName={styles.tooltipBubble}>
-      <div>
-        <a className={classNames(styles.chapterLink, {[styles.chapterLinkActive]: props.active})}
-          href={`#chapter-${props.permaId}`}
-          onClick={() => props.handleMenuClick(props.chapterLinkId)}>
-          {props.title}
-        </a>
-        <p className={styles.summary}
-          dangerouslySetInnerHTML={{__html: props.summary}} />
-      </div>
+      {item}
     </Tooltip>
   )
 }
