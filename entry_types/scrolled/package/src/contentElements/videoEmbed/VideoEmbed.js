6696c6add29cb5d180b2b9e51bb01caf5d5e8b96
Tim Fischbach <tfischbach@codevise.de>
2020-11-24 10:38:15 +0100

    Make opt out info stand out less
    * Use dark dark background for tooltip.
    
    * Fade out when hiding.
    
    * Unify alignment. Button intersected with Datawrapper download link
      when placed at the bottom.
    
    REDMINE-17863

--- entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
+++ entry_types/scrolled/package/src/contentElements/videoEmbed/VideoEmbed.js
@@ -30,7 +30,6 @@ export function VideoEmbed({contentElementId, configuration}) {
       <Figure caption={configuration.caption}>
         <ThirdPartyOptOutInfo providerName="video"
                               hide={playerState === 'playing'}
-                              placement="bottom-25"
                               contentElementPosition={configuration.position}>
           <ViewportDependentPillarBoxes aspectRatio={aspectRatios[configuration.aspectRatio || 'wide']}
                                         position={configuration.position}
