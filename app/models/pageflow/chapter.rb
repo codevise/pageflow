f7edc3de39893e0a85a879fb425cb4e625f04f65
Tim Fischbach <tfischbach@codevise.de>
2021-01-15 16:44:45 +0100

    Register paged storylines as revision components
    Remove special case handling for paged storylines, chapters and pages
    when copying revisions and during entry export/import.
    
    REDMINE-17280

--- app/models/pageflow/chapter.rb
+++ app/models/pageflow/chapter.rb
@@ -1,9 +1,12 @@
 module Pageflow
   class Chapter < ApplicationRecord
     include SerializedConfiguration
+    include NestedRevisionComponent
 
     belongs_to :storyline, touch: true
     has_many :pages, -> { order('position ASC') }, dependent: :destroy, inverse_of: :chapter
+
+    nested_revision_components :pages
 
     attr_accessor :is_first
 
@@ -11,15 +14,6 @@ module Pageflow
 
     def pages
       super.tap { |p| p.first.is_first = true if is_first && p.present? }
-    end
-
-    def copy_to(storyline)
-      chapter = dup
-      storyline.chapters << chapter
-
-      pages.each do |page|
-        page.copy_to(chapter)
-      end
     end
   end
 end
