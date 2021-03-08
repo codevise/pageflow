40dac49247c28ef06dd9d601f702da9e63cb6efe
Tim Fischbach <tfischbach@codevise.de>
2021-03-08 17:06:11 +0100

    Allow non latin characters in video embed caption
    Previously used hash function only supported limited range of
    characters as input.
    
    REDMINE-18456

--- entry_types/scrolled/package/src/contentElements/heading/editor.js
+++ entry_types/scrolled/package/src/contentElements/heading/editor.js
@@ -1,11 +1,11 @@
 import {editor, NoOptionsHintView} from 'pageflow-scrolled/editor';
 
 editor.contentElementTypes.register('heading', {
-  supportedPositions: ['inline'],
+  supportedPositions: ['inline', 'full'],
 
   configurationEditor() {
     this.tab('general', function() {
-      this.view(NoOptionsHintView);
+      this.group('ContentElementPosition');
     });
   }
 });
