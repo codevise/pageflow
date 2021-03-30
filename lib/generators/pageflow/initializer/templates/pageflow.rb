d0701dd2ea16482cf0bf75ddb283040a019772e0
Tim Fischbach <tfischbach@codevise.de>
2021-03-30 17:55:56 +0200

    Allow changing navigation colors via theme options
    REDMINE-18425

--- lib/generators/pageflow/initializer/templates/pageflow.rb
+++ lib/generators/pageflow/initializer/templates/pageflow.rb
@@ -30,6 +30,18 @@ Pageflow.configure do |config|
                                         entry: '"Source Sans Pro", sans-serif',
                                         navigation: '"Source Sans Pro", sans-serif'
                                       },
+                                      colors: {
+                                        accent: '#e10028',
+                                        navigation: {
+                                          surface: '#fff',
+                                          on_surface: '#000',
+                                          primary_on_surface: '#00375a',
+                                          secondary_on_surface: '#c2c2c2',
+                                          background: 'rgba(255, 255, 255, 0.95)',
+                                          on_background: '#000',
+                                          primary_on_background: '#00375a'
+                                        }
+                                      },
                                       logo_alt_text: 'Pageflow',
                                       theme_color: '#ffffff')
   end
