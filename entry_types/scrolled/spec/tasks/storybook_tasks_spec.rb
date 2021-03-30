93000c45d97b8315d2bbe645718a8d8cff3b5452
Tim Fischbach <tfischbach@codevise.de>
2021-03-30 17:58:45 +0200

    Make theme custom properties available in storybook
    Generate `preview-head.html` to include the same style tag defining
    custom properties that would otherwise be rendered by the Rails
    server.
    
    REDMINE-18425

--- entry_types/scrolled/spec/tasks/storybook_tasks_spec.rb
+++ entry_types/scrolled/spec/tasks/storybook_tasks_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 require 'rake'
 
 RSpec.describe 'storybook:seed' do
-  let(:test_output_file_name) do
-    Rails.root.join('tmp', 'seed.json').to_s
+  let(:test_output_dir) do
+    Rails.root.join('tmp', 'storybook_task_test').to_s
   end
 
   before do
     Rake::Task.define_task(:environment)
     Rake.application.rake_require 'tasks/pageflow_scrolled_tasks'
 
-    FileUtils.rm(test_output_file_name) if File.exist?(test_output_file_name)
+    FileUtils.rm_r(test_output_dir) if File.exist?(test_output_dir)
+    FileUtils.mkdir(test_output_dir)
 
     stub_request(:get, /amazonaws.com/)
       .to_return(status: 200,
@@ -26,7 +27,7 @@ RSpec.describe 'storybook:seed' do
     it 'recreates entry' do
       rake 'pageflow_scrolled:storybook:seed:create_entry'
       old_entry_id = Pageflow::Entry.find_by_title('Storybook seed').id
-      rake 'pageflow_scrolled:storybook:seed:setup', test_output_file_name
+      rake 'pageflow_scrolled:storybook:seed:setup', test_output_dir
       new_entry_id = Pageflow::Entry.find_by_title('Storybook seed').id
 
       expect(new_entry_id).not_to eq(old_entry_id)
@@ -55,23 +56,39 @@ RSpec.describe 'storybook:seed' do
   describe 'generate_json task' do
     it 'writes seed containing files data' do
       rake 'pageflow_scrolled:storybook:seed:create_entry'
-      rake 'pageflow_scrolled:storybook:seed:generate_json', test_output_file_name
+      rake 'pageflow_scrolled:storybook:seed:generate_json', test_output_dir
 
-      expect(File.read(test_output_file_name)).to include('"testReferenceName":"turtle"')
+      seed_json = File.read(File.join(test_output_dir, 'seed.json'))
+
+      expect(seed_json).to include('"testReferenceName":"turtle"')
     end
 
     it 'sets locale to entry locale' do
       rake 'pageflow_scrolled:storybook:seed:create_entry'
       Pageflow::Entry.where(title: 'Storybook seed').first.draft.update(locale: 'fr')
-      rake 'pageflow_scrolled:storybook:seed:generate_json', test_output_file_name
+      rake 'pageflow_scrolled:storybook:seed:generate_json', test_output_dir
 
-      expect(File.read(test_output_file_name))
+      seed_json = File.read(File.join(test_output_dir, 'seed.json'))
+
+      expect(seed_json)
         .to include_json(i18n: {
                            locale: 'fr',
                            translations: {
                              fr: {}
                            }
                          })
+    end
+  end
+
+  describe 'generate_head_html task' do
+    it 'writes preview-head.html containing theme custom properties and static template' do
+      rake 'pageflow_scrolled:storybook:seed:create_entry'
+      rake 'pageflow_scrolled:storybook:seed:generate_head_html', test_output_dir
+
+      html = File.read(File.join(test_output_dir, 'preview-head.html'))
+
+      expect(html).to include('--theme-accent-color:')
+      expect(html).to include('[data-percy=hide]')
     end
   end
 
