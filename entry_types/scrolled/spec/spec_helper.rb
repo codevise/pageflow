7728cf90dd3c1a0e75221f92647feaf1a2aa247e
Tim Fischbach <tfischbach@codevise.de>
2021-06-01 09:18:49 +0200

    Allow customizing scrolled logos
    REDMINE-18710

--- entry_types/scrolled/spec/spec_helper.rb
+++ entry_types/scrolled/spec/spec_helper.rb
@@ -22,6 +22,8 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
+  config.fixture_path = './spec/fixtures'
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
