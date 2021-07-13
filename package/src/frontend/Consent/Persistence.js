a3b7ed3a30696fbf6a3dcb225dbc3c08277e4e1a
Tim Fischbach <tfischbach@codevise.de>
2021-07-13 11:42:18 +0200

    Set path of consent cookie
    If not currently visiting a site on the root directory, a default path
    is used. This can lead to cookies not beeing available in entries when
    being set from something like a privacy page.
    
    REDMINE-18920

--- package/src/frontend/Consent/Persistence.js
+++ package/src/frontend/Consent/Persistence.js
@@ -18,7 +18,7 @@ export class Persistence {
                            signal === 'denied' ? false :
                            signal[vendor.name]);
                         return result;
-                      }, {})));
+                      }, {})), null, '/');
     });
   }
 
@@ -31,7 +31,7 @@ export class Persistence {
                          JSON.stringify({
                            ...flags,
                            [vendor.cookieKey || vendor.name]: signal
-                         }));
+                         }), null, '/');
   }
 
   read(vendor) {
