bafd419b7fca770b06edaa212aea2484220e182c
Tim Fischbach <tfischbach@codevise.de>
2021-09-08 10:55:32 +0200

    Improve supported host handling in url input
    Do not expect protocols in host strings. This prevents having to
    include both http and https versions of a supported host if
    `permitHttps` is set to true. Ignore protocols in host names for
    backwards compatibility.
    
    REDMINE-19000

--- package/spec/ui/views/inputs/UrlInputView-spec.js
+++ package/spec/ui/views/inputs/UrlInputView-spec.js
@@ -1,5 +1,7 @@
 import Backbone from 'backbone';
 
+import '@testing-library/jest-dom/extend-expect';
+
 import {UrlInputView} from 'pageflow/ui';
 
 describe('UrlInputView', () => {
@@ -130,7 +132,7 @@ describe('UrlInputView', () => {
     expect(listener).toHaveBeenCalledTimes(1);
   });
 
-  it('triggers single model change event if valid', () => {
+  it('triggers single model change event if invalid', () => {
     var model = new Backbone.Model({
       url: 'http://example.com/some',
       displayUrl: 'http://example.com/some'
@@ -150,5 +152,144 @@ describe('UrlInputView', () => {
     input.trigger('change');
 
     expect(listener).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be invalid if not a URL', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      supportedHosts: ['example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('not-a-url');
+    input.trigger('change');
+
+    expect(input[0]).toBeInvalid();
+  });
+
+  it('should be invalid if URL has unsupported host', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      supportedHosts: ['example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('http://other.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeInvalid();
+  });
+
+  it('should be valid if URL with supported host', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      supportedHosts: ['example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('http://example.com/foo');
+    input.trigger('change');
+
+    expect(input[0]).toBeValid();
+  });
+
+  it('should ignore http protocol in supported hosts', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      supportedHosts: ['http://example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('http://example.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeValid();
+  });
+
+  it('should ignore https protocol in supported hosts', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      permitHttps: true,
+      supportedHosts: ['https://example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('https://example.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeValid();
+  });
+
+  it('should be invalid if URL uses https', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      supportedHosts: ['example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('https://example.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeInvalid();
+  });
+
+  it('should be valid if https is permitted and URL uses https', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      permitHttps: true,
+      supportedHosts: ['example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('https://example.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeValid();
+  });
+
+  it('should ignore protocol of supported Hosts if https permitted', () => {
+    var model = new Backbone.Model();
+    var view = new UrlInputView({
+      model: model,
+      propertyName: 'url',
+      displayPropertyName: 'displayUrl',
+      permitHttps: true,
+      supportedHosts: ['http://example.com']
+    });
+
+    view.render();
+    var input = view.$el.find('input');
+    input.val('https://example.com');
+    input.trigger('change');
+
+    expect(input[0]).toBeValid();
   });
 });
