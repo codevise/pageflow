6b4d9030c725e3957cf1fc18de9872d23cf65815
Tim Fischbach <tfischbach@codevise.de>
2021-09-08 11:12:27 +0200

    Allow passing traits to create_used_file
    Match FactoryBot API.
    
    REDMINE-19000

--- spec/support/pageflow/used_file_test_helper.rb
+++ spec/support/pageflow/used_file_test_helper.rb
@@ -1,8 +1,8 @@
 module UsedFileTestHelper
   # creates a file with usage in entry
   # and sets the magic @entry-variable (always present in views) for file lookup
-  def create_used_file(model, entry: nil, **attributes)
-    file = create(model, attributes)
+  def create_used_file(model, *traits, entry: nil, **attributes)
+    file = create(model, *traits, attributes)
     @entry = entry || Pageflow::PublishedEntry.new(create(:entry, :published))
     usage = file.usages.create(revision: @entry.revision)
     Pageflow::UsedFile.new(file, usage)
