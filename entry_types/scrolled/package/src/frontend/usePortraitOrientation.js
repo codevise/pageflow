a6694f7b959136a50e434fbc7ee38947ad107ea4
Tim Fischbach <tfischbach@codevise.de>
2021-09-16 11:58:21 +0200

    Add portrait image option to inline image
    Make `usePortraitOrientation` public API. Allow rendering different
    image based on orientation.
    
    REDMINE-19186

--- entry_types/scrolled/package/src/frontend/usePortraitOrientation.js
+++ entry_types/scrolled/package/src/frontend/usePortraitOrientation.js
@@ -1,5 +1,9 @@
 import useMediaQuery from './useMediaQuery';
 
+/**
+ * Returns boolean indicating whether viewport orientation is currently
+ * portrait.
+ */
 export function usePortraitOrientation() {
   return useMediaQuery('(orientation: portrait)');
 }
