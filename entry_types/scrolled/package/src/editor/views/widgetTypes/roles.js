09139c56c4fe374e2b97f85e119c512e1ce2d4fd
Tim Fischbach <tfischbach@codevise.de>
2021-09-22 10:25:47 +0200

    Replace navigation with widget
    Allow registering components for widget types.
    
    REDMINE

---
+++ entry_types/scrolled/package/src/editor/views/widgetTypes/roles.js
@@ -0,0 +1,9 @@
+import {editor} from 'pageflow/editor';
+
+editor.widgetTypes.registerRole('header', {
+  isOptional: false
+});
+
+editor.widgetTypes.registerRole('footer', {
+  isOptional: true
+});
