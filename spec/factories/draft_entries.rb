c140c6b6ca0eae8c597bda054b891fb086e5e65a
Tim Fischbach <tfischbach@codevise.de>
2021-10-20 09:56:01 +0200

    Use canonical entry url prefix in pretty urls
    Allow specifying a custom prefix that is used in pretty entry
    urls. This helps with cases, where entries are meant to be published
    below a custom path of a third party domain using a custom proxy
    setup.
    
    REDMINE-19272

--- spec/factories/draft_entries.rb
+++ spec/factories/draft_entries.rb
@@ -1,8 +1,26 @@
 module Pageflow
   FactoryBot.define do
     factory :draft_entry, class: DraftEntry do
+      transient do
+        title
+        account
+        theming
+        type_name { 'paged' }
+        revision_attributes { {} }
+
+        with_feature { nil }
+        without_feature { nil }
+      end
+
       initialize_with do
-        DraftEntry.new(create(:entry))
+        DraftEntry.new(create(:entry,
+                              title: title,
+                              account: account,
+                              theming: theming,
+                              type_name: type_name,
+                              draft_attributes: revision_attributes,
+                              with_feature: with_feature,
+                              without_feature: without_feature))
       end
 
       to_create { |draft_entry| draft_entry.entry.save! }
