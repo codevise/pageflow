2ffa234897bd7b34e27920ff5de064faa5c2f455
8aviav <8aviav@gmail.com>
2021-10-27 12:06:03 +0200

    Support proc as asset host
    REDMINE-19284

--- entry_types/scrolled/app/helpers/pageflow_scrolled/webpack_public_path_helper.rb
+++ entry_types/scrolled/app/helpers/pageflow_scrolled/webpack_public_path_helper.rb
@@ -2,12 +2,17 @@ module PageflowScrolled
   # @api private
   module WebpackPublicPathHelper
     def scrolled_webpack_public_path_script_tag
-      content_tag(:script, WebpackPublicPathHelper.js_snippet.html_safe)
+      content_tag(:script, WebpackPublicPathHelper.js_snippet(request).html_safe)
     end
 
-    def self.js_snippet
-      asset_host = Rails.configuration.action_controller.asset_host
+    def self.js_snippet(request = nil)
+      config_host = Rails.configuration.action_controller.asset_host
       packs_dir = Webpacker.config.public_output_path.basename
+      asset_host = if config_host.respond_to?(:call)
+                     config_host.call(packs_dir, request)
+                   else
+                     config_host
+                   end
 
       "var __webpack_public_path__ = '#{asset_host}/#{packs_dir}/';"
     end
