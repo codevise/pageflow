447147ba6588318363e116252aa57bbda947b986
Tim Fischbach <tfischbach@codevise.de>
2021-12-28 10:37:59 +0100

    Integrate content element types with feature flags
    Allow specifying feature name when registering content element type
    and only offer it when inserting content elements if feature is
    enabled.
    
    REDMINE-19406

--- entry_types/scrolled/package/spec/editor/api/ContentElementTypeRegistry-spec.js
+++ entry_types/scrolled/package/spec/editor/api/ContentElementTypeRegistry-spec.js
@@ -1,9 +1,10 @@
 import {ContentElementTypeRegistry} from 'editor/api/ContentElementTypeRegistry';
+import {Features} from 'pageflow/frontend';
 
 describe('ContentElementTypeRegistry', () => {
   describe('#toArray', () => {
     it('returns array of with options passed to register', () => {
-      const registry = new ContentElementTypeRegistry();
+      const registry = new ContentElementTypeRegistry({features: new Features()});
       registry.register('textBlock', {supportedPositions: ['inline']});
 
       const types = registry.toArray();
@@ -11,6 +12,35 @@ describe('ContentElementTypeRegistry', () => {
       expect(types).toMatchObject([{
         typeName: 'textBlock',
         supportedPositions: ['inline']
+      }]);
+    });
+
+    it('filters out element types that reference disabled feature flag', () => {
+      const features = new Features();
+      const registry = new ContentElementTypeRegistry({features});
+      registry.register('special', {
+        supportedPositions: ['inline'],
+        featureName: 'special_content_element'
+      });
+
+      const types = registry.toArray();
+
+      expect(types).toEqual([]);
+    });
+
+    it('does not filter out element types that reference enabled feature flag', () => {
+      const features = new Features();
+      const registry = new ContentElementTypeRegistry({features});
+      registry.register('special', {
+        supportedPositions: ['inline'],
+        featureName: 'special_content_element'
+      });
+      features.enable('editor', ['special_content_element'])
+
+      const types = registry.toArray();
+
+      expect(types).toMatchObject([{
+        typeName: 'special'
       }]);
     });
   });
