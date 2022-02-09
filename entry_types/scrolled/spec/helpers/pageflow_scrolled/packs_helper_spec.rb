f810559a8f4bc88f68c54fbf1141803116b01014
Tim Fischbach <tfischbach@codevise.de>
2022-02-09 10:06:43 +0100

    Allow registering additional frontend/editor packs
    Load chunks conditionally based on feature flags and present content
    elements.
    
    REDMINE-19525

--- entry_types/scrolled/spec/helpers/pageflow_scrolled/packs_helper_spec.rb
+++ entry_types/scrolled/spec/helpers/pageflow_scrolled/packs_helper_spec.rb
@@ -14,6 +14,61 @@ module PageflowScrolled
         expect(result).to include('pageflow-scrolled-frontend')
       end
 
+      it 'includes additional frontend packs in editor' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.additional_frontend_packs.register(
+              'pageflow-scrolled/contentElements/extra',
+              content_element_type_names: ['extra']
+            )
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled')
+
+        result = helper.scrolled_frontend_packs(entry,
+                                                widget_scope: :editor)
+
+        expect(result).to include('pageflow-scrolled/contentElements/extra')
+      end
+
+      it 'includes additional frontend packs of used content elements outside of editor' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.additional_frontend_packs.register(
+              'pageflow-scrolled/contentElements/extra',
+              content_element_type_names: ['extra']
+            )
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled')
+        create(:content_element, revision: entry.revision, type_name: 'extra')
+
+        result = helper.scrolled_frontend_packs(entry,
+                                                widget_scope: :published)
+
+        expect(result).to include('pageflow-scrolled/contentElements/extra')
+      end
+
+      it 'does not include additional frontend packs of unused elements outside of editor' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.additional_frontend_packs.register(
+              'pageflow-scrolled/contentElements/extra',
+              content_element_type_names: ['extra']
+            )
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled')
+
+        result = helper.scrolled_frontend_packs(entry,
+                                                widget_scope: :published)
+
+        expect(result).not_to include('pageflow-scrolled/contentElements/extra')
+      end
+
       it 'includes all react widget type packs in editor' do
         pageflow_configure do |config|
           config.widget_types.register(ReactWidgetType.new(name: 'customNavigation',
@@ -81,6 +136,68 @@ module PageflowScrolled
                                                 widget_scope: :editor)
 
         expect(result).not_to include('pageflow-scrolled/widgets/test')
+      end
+    end
+
+    describe 'scrolled_editor_packs' do
+      it 'includes editor pack' do
+        entry = create(:published_entry, type_name: 'scrolled')
+
+        result = helper.scrolled_editor_packs(entry)
+
+        expect(result).to include('pageflow-scrolled-editor')
+      end
+
+      it 'includes additional editor packs in editor' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.additional_editor_packs.register(
+              'pageflow-scrolled/contentElements/extra-editor'
+            )
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled')
+
+        result = helper.scrolled_editor_packs(entry)
+
+        expect(result).to include('pageflow-scrolled/contentElements/extra-editor')
+      end
+
+      it 'includes additional editor pack registered in active feature' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.features.register('extra') do |feature_config|
+              feature_config.additional_editor_packs.register(
+                'pageflow-scrolled/contentElements/extra-editor'
+              )
+            end
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled', with_feature: 'extra')
+
+        result = helper.scrolled_editor_packs(entry)
+
+        expect(result).to include('pageflow-scrolled/contentElements/extra-editor')
+      end
+
+      it 'does not include additional editor pack registered in inactive feature' do
+        pageflow_configure do |config|
+          config.for_entry_type(PageflowScrolled.entry_type) do |entry_type_config|
+            entry_type_config.features.register('extra') do |feature_config|
+              feature_config.additional_editor_packs.register(
+                'pageflow-scrolled/contentElements/extra-editor'
+              )
+            end
+          end
+        end
+
+        entry = create(:published_entry, type_name: 'scrolled')
+
+        result = helper.scrolled_editor_packs(entry)
+
+        expect(result).not_to include('pageflow-scrolled/contentElements/extra-editor')
       end
     end
   end
