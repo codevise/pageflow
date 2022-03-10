60c3daf4c20c45e5a5a1fe741c131edc635710d2
Tim Fischbach <tfischbach@codevise.de>
2022-03-10 10:10:26 +0100

    Add transientState model to content elements
    Make it easier to wire up transient state in configuration editor
    inputs and post content element command on change.
    
    REDMINE-19573

--- entry_types/scrolled/package/spec/editor/models/ContentElement-spec.js
+++ entry_types/scrolled/package/spec/editor/models/ContentElement-spec.js
@@ -7,8 +7,7 @@ describe('ContentElement', () => {
     beforeEach(() => {
       editor.contentElementTypes.register('inlineImage', {});
       editor.contentElementTypes.register('soundDisclaimer', {supportedPositions: ['inline']});
-    })
-
+    });
 
     it('returns positions for left layout by default', () => {
       const entry = factories.entry(
@@ -109,5 +108,76 @@ describe('ContentElement', () => {
 
       expect(contentElement.getAvailablePositions()).toEqual(['inline']);
     });
-  })
+  });
+
+  describe('transientState', () => {
+    it('stays in sync with transientState attribute', () => {
+      editor.contentElementTypes.register('someElement', {});
+
+      const entry = factories.entry(
+        ScrolledEntry,
+        {},
+        {
+          entryTypeSeed: normalizeSeed({
+            contentElements: [
+              {id: 5, typeName: 'someElement'}
+            ]
+          })
+        }
+      );
+      const contentElement = entry.contentElements.get(5);
+
+      contentElement.set('transientState', {some: 'value'});
+
+      expect(contentElement.transientState.get('some')).toEqual('value');
+    });
+
+    it('posts TRANSIENT_STATE_UPDATE command when model is changed', () => {
+      const listener = jest.fn();
+
+      const entry = factories.entry(
+        ScrolledEntry,
+        {},
+        {
+          entryTypeSeed: normalizeSeed({
+            contentElements: [
+              {id: 5, typeName: 'someElement'}
+            ]
+          })
+        }
+      );
+      const contentElement = entry.contentElements.get(5);
+      contentElement.on('postCommand', listener)
+      contentElement.transientState.set('text', 'some text');
+
+      expect(listener).toHaveBeenCalledWith(contentElement.id, {
+        type: 'TRANSIENT_STATE_UPDATE',
+        payload: {
+          text: 'some text'
+        }
+      });
+    });
+
+    it('does not post command when state is changed from preivew', () => {
+      const listener = jest.fn();
+
+      const entry = factories.entry(
+        ScrolledEntry,
+        {},
+        {
+          entryTypeSeed: normalizeSeed({
+            contentElements: [
+              {id: 5, typeName: 'someElement'}
+            ]
+          })
+        }
+      );
+      const contentElement = entry.contentElements.get(5);
+      contentElement.on('postCommand', listener)
+      contentElement.set('transientState', {text: 'some text'});
+
+      expect(listener).not.toHaveBeenCalled();
+    });
+
+  });
 });
