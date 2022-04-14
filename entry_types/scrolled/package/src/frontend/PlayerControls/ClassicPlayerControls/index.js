614ff03a53463eed9d67eae9a9fdfa2ec789b1df
Tim Fischbach <tfischbach@codevise.de>
2022-04-14 21:23:38 +0200

    Add hideControlBar prop to ClassicPlayerControls
    REDMINE-19624

--- entry_types/scrolled/package/src/frontend/PlayerControls/ClassicPlayerControls/index.js
+++ entry_types/scrolled/package/src/frontend/PlayerControls/ClassicPlayerControls/index.js
@@ -24,32 +24,39 @@ export function ClassicPlayerControls(props) {
                            lastControlledVia={props.lastControlledVia}
                            hideCursor={props.isPlaying && props.inactive}
                            onClick={props.onPlayerClick} />}
-      <div onFocus={props.onFocus}
-           onBlur={props.onBlur}
-           onMouseEnter={props.onMouseEnter}
-           onMouseLeave={props.onMouseLeave}
-           className={classNames(styles.controlBarContainer,
-                                 darkBackground ? styles.darkBackground : styles.lightBackground,
-                                 {
-                                   [styles.inset]: !props.standAlone,
-                                   [styles.transparent]: transparent
-                                 })}>
-        <PlayPauseButton isPlaying={props.isPlaying}
-                         play={props.play}
-                         pause={props.pause}/>
-        <ProgressIndicators currentTime={props.currentTime}
-                            duration={props.duration}
-                            bufferedEnd={props.bufferedEnd}
-                            scrubTo={props.scrubTo}
-                            seekTo={props.seekTo}/>
-        <TimeDisplay currentTime={props.currentTime}
-                     duration={props.duration}/>
-        <TextTracksMenu items={props.textTracksMenuItems}
-                        onItemClick={props.onTextTracksMenuItemClick} />
-        <QualityMenu items={props.qualityMenuItems}
-                     onItemClick={props.onQualityMenuItemClick}
-                     subMenuExpanded={props.qualityMenuExpanded} />
-      </div>
+      {!props.hideControlBar &&
+       renderControlBar(props, darkBackground, transparent)}
+    </div>
+  );
+}
+
+function renderControlBar(props, darkBackground, transparent) {
+  return (
+    <div onFocus={props.onFocus}
+         onBlur={props.onBlur}
+         onMouseEnter={props.onMouseEnter}
+         onMouseLeave={props.onMouseLeave}
+         className={classNames(styles.controlBarContainer,
+                               darkBackground ? styles.darkBackground : styles.lightBackground,
+                               {
+                                 [styles.inset]: !props.standAlone,
+                                 [styles.transparent]: transparent
+         })}>
+      <PlayPauseButton isPlaying={props.isPlaying}
+                       play={props.play}
+                       pause={props.pause}/>
+      <ProgressIndicators currentTime={props.currentTime}
+                          duration={props.duration}
+                          bufferedEnd={props.bufferedEnd}
+                          scrubTo={props.scrubTo}
+                          seekTo={props.seekTo}/>
+      <TimeDisplay currentTime={props.currentTime}
+                   duration={props.duration}/>
+      <TextTracksMenu items={props.textTracksMenuItems}
+                      onItemClick={props.onTextTracksMenuItemClick} />
+      <QualityMenu items={props.qualityMenuItems}
+                   onItemClick={props.onQualityMenuItemClick}
+                   subMenuExpanded={props.qualityMenuExpanded} />
     </div>
   );
 }
