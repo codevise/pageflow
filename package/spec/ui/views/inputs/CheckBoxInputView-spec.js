b3cd86d8e1efec2ba2c6556b1f7b0bb3b67ff344
Tim Fischbach <tfischbach@codevise.de>
2022-04-14 21:29:01 +0200

    Allow inverting check box inputs
    Use alternate property and read/store inverted boolean value.
    
    REDMINE-19624

--- package/spec/ui/views/inputs/CheckBoxInputView-spec.js
+++ package/spec/ui/views/inputs/CheckBoxInputView-spec.js
@@ -17,6 +17,20 @@ describe('pageflow.CheckBoxInputView', () => {
     expect(view.ui.input.is(':checked')).toEqual(true);
   });
 
+  it('saves value on change', () => {
+    var model = new Model();
+    var view = new CheckBoxInputView({
+      model: model,
+      propertyName: 'value'
+    });
+
+    view.render();
+    view.$el.find('input').prop('checked', true);
+    view.$el.find('input').trigger('change');
+
+    expect(model.get('value')).toEqual(true);
+  });
+
   it('supports displaying unchecked if disabled', () => {
     var model = new Model({value: true});
     var view = new CheckBoxInputView({
@@ -62,4 +76,60 @@ describe('pageflow.CheckBoxInputView', () => {
     model.set('disabled', false);
     expect(view.ui.input.is(':checked')).toEqual(true);
   });
+
+  describe('with storeInverted option', () => {
+    it('displays checked by default', () => {
+      var model = new Model();
+      var view = new CheckBoxInputView({
+        model: model,
+        propertyName: 'value',
+        storeInverted: 'invertedValue'
+      });
+
+      view.render();
+
+      expect(view.ui.input.is(':checked')).toEqual(true);
+    });
+
+    it('displays unchecked if true in model', () => {
+      var model = new Model({invertedValue: true});
+      var view = new CheckBoxInputView({
+        model: model,
+        propertyName: 'value',
+        storeInverted: 'invertedValue'
+      });
+
+      view.render();
+
+      expect(view.ui.input.is(':checked')).toEqual(false);
+    });
+
+    it('displays checked if false in model', () => {
+      var model = new Model({invertedValue: false});
+      var view = new CheckBoxInputView({
+        model: model,
+        propertyName: 'value',
+        storeInverted: 'invertedValue'
+      });
+
+      view.render();
+
+      expect(view.ui.input.is(':checked')).toEqual(true);
+    });
+
+    it('saves inverted value on change', () => {
+      var model = new Model();
+      var view = new CheckBoxInputView({
+        model: model,
+        propertyName: 'value',
+        storeInverted: 'invertedValue'
+      });
+
+      view.render();
+      view.$el.find('input').prop('checked', true);
+      view.$el.find('input').trigger('change');
+
+      expect(model.get('invertedValue')).toEqual(false);
+    });
+  })
 });
