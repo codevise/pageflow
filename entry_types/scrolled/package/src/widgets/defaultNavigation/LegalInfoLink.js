80150fb09978275c8f46506cdae5d93f75f454e0
Tim Fischbach <tfischbach@codevise.de>
2022-06-30 10:47:13 +0200

    Improve styling of default navigation bar credits box
    * Display scroller if credits text gets long
    
    * Add spacing to links
    
    REDMINE-19484

--- entry_types/scrolled/package/src/widgets/defaultNavigation/LegalInfoLink.js
+++ entry_types/scrolled/package/src/widgets/defaultNavigation/LegalInfoLink.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import styles from "./LegalInfoMenu.module.css";
 
 export function LegalInfoLink(props) {
   return (
     <div>
       {props.label && props.url &&
-      <a target="_blank" href={props.url}
-         dangerouslySetInnerHTML={{__html: props.label}}>
-      </a>
+       <a target="_blank"
+          rel="noreferrer noopener"
+          href={props.url}
+          className={styles.legalInfoLink}
+          dangerouslySetInnerHTML={{__html: props.label}}>
+       </a>
       }
     </div>
   )
