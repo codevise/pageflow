f6426ac1e7817ffbbf2a081daa0039bdb908a2ba
Tim Fischbach <tfischbach@codevise.de>
2022-07-19 13:32:42 +0200

    Allow adding id in data attribute when adding editor main menu item
    REDMINE-19870

--- package/spec/editor/views/EditEntryView-spec.js
+++ package/spec/editor/views/EditEntryView-spec.js
@@ -2,9 +2,13 @@ import Marionette from 'backbone.marionette';
 
 import {EditEntryView, editor} from 'pageflow/editor';
 
-import {factories} from '$support';
+import {factories, useFakeTranslations} from '$support';
 
 describe('EditEntryView', () => {
+  useFakeTranslations({
+    'some.key': 'some translation'
+  });
+
   it('renders entry type specific outline', () => {
     const entry = factories.entry();
     editor.registerEntryType('test', {
@@ -17,5 +21,19 @@ describe('EditEntryView', () => {
     view.render();
 
     expect(view.$el).toHaveDescendant('.test_outline');
+  });
+
+  it('renders additional menu items', () => {
+    const entry = factories.entry();
+    editor.registerMainMenuItem({
+      translationKey: 'some.key',
+      id: 'some-id'
+    });
+    const view = new EditEntryView({model: entry});
+
+    view.render();
+    const item = view.$el.find('[data-main-menu-item="some-id"]');
+
+    expect(item).toHaveText('some translation');
   });
 });
