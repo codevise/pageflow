c908205f5f4b8bef7e7e525e50d4161f95cfbab2
Tim Fischbach <tfischbach@codevise.de>
2022-08-19 16:38:15 +0200

    Allow adding consent opt-in story for content elements
    Allow rendering content elements the way they look before consent has
    been given in storybook. So far the storybook relies on the fact, that
    consent is only required when a consent cookie name has been
    configured. Since this is not configured by default, consent opt-in is
    never displayed in the storybook.
    
    Content elements which interact with consent, can now pass the
    `consent` option when adding content element stories. We then set a
    consent cookie name when constructing the consent object used in an
    additional "Consent - Opt In" story.

--- entry_types/scrolled/package/src/frontend/thirdPartyConsent/registerVendors.js
+++ entry_types/scrolled/package/src/frontend/thirdPartyConsent/registerVendors.js
@@ -1,8 +1,10 @@
 import I18n from 'i18n-js';
 
-export function registerVendors({contentElementTypes, seed, consent}) {
+export function registerVendors({contentElementTypes, seed, consent, cookieName}) {
   const options = seed.config.theme.options.thirdPartyConsent;
   const locale = seed.collections.entries[0].locale;
+
+  cookieName = cookieName || options?.cookieName;
 
   contentElementTypes
     .consentVendors({
@@ -13,8 +15,8 @@ export function registerVendors({contentElementTypes, seed, consent}) {
       consent.registerVendor(vendor.name, {
         displayName: vendor.displayName,
         description: vendor.description,
-        paradigm: options?.cookieName ? (vendor.paradigm || 'opt-in') : 'skip',
-        cookieName: options?.cookieName,
+        paradigm: cookieName ? (vendor.paradigm || 'opt-in') : 'skip',
+        cookieName: cookieName,
         cookieKey: options?.cookieProviderNameMapping?.[vendor.name],
         cookieDomain: options?.cookieDomain
       });
