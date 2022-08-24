cae2a2b01448ec9a65cf77c40722e5d9d1ff2bf6
Tim Fischbach <tfischbach@codevise.de>
2022-08-24 11:37:55 +0200

    Use aspect ratio classes based on thumbnail size in outline
    Do not switch to portrait background/motif position in section
    thumbnails, when the editor viewport is portrait. Instead rely on
    fullscreen dimensions measured in section thumbnail.
    
    REDMINE-19888

--- entry_types/scrolled/package/spec/frontend/v2/useBackdropSectionClassNames-spec.js
+++ entry_types/scrolled/package/spec/frontend/v2/useBackdropSectionClassNames-spec.js
@@ -1,5 +1,8 @@
+import React from 'react';
+
 import {useBackdropSectionClassNames} from 'frontend/v2/useBackdropSectionClassNames';
 import {useBackdropFile} from 'frontend/v2/useBackdrop';
+import {FullscreenDimensionProvider} from 'frontend/Fullscreen';
 
 import {renderHookInEntry} from 'support';
 
@@ -75,7 +78,6 @@ describe('useBackdropSectionClassNames', () => {
 
     expect(result.current).not.toContain(/aspectRatio/);
   });
-
 
   it('includes aspect ratio class name for mobile file', () => {
     const {result} = renderHookInEntry(
@@ -184,6 +186,120 @@ describe('useBackdropSectionClassNames', () => {
     );
 
     expect(document.head.querySelectorAll('style').length).toEqual(0);
+  });
+
+  describe('when wrapped in FullscreenDimensionProvider', () => {
+    describe('with fullscreen aspect ratio > backdrop aspect ratio', () => {
+      it('includes media-query-independant aspect ratio class name', () => {
+        const {result} = renderHookInEntry(
+          () => useBackdropSectionClassNames({
+            file: useBackdropFile({collectionName: 'imageFiles', permaId: 10})
+          }),
+          {
+            wrapper: ({children}) =>
+              <FullscreenDimensionProvider width={1600} height={900}>
+                {children}
+              </FullscreenDimensionProvider>,
+            seed: {
+              imageFiles: [{id: 100, permaId: 10, width: 400, height: 300}]
+            }
+          }
+        );
+
+        expect(result.current).not.toContain(/aspectRatio/);
+        expect(result.current).toContain('minAspectRatio');
+      });
+    });
+
+    describe('with fullscreen aspect ratio < backdrop aspect ratio', () => {
+      it('includes no aspect ratio class name', () => {
+        const {result} = renderHookInEntry(
+          () => useBackdropSectionClassNames({
+            file: useBackdropFile({collectionName: 'imageFiles', permaId: 10})
+          }),
+          {
+            wrapper: ({children}) =>
+              <FullscreenDimensionProvider width={300} height={400}>
+                {children}
+              </FullscreenDimensionProvider>,
+            seed: {
+              imageFiles: [{id: 100, permaId: 10, width: 400, height: 300}]
+            }
+          }
+        );
+
+        expect(result.current).not.toContain(/aspectRatio/);
+      });
+    });
+
+    describe('with fullscreen aspect ratio > mobile backdrop aspect ratio', () => {
+      it('include aspect ratio class name if fullscreen is portrait', () => {
+        const {result} = renderHookInEntry(
+          () => useBackdropSectionClassNames({
+            file: useBackdropFile({collectionName: 'imageFiles', permaId: 10}),
+            mobileFile: useBackdropFile({collectionName: 'imageFiles', permaId: 11})
+          }),
+          {
+            wrapper: ({children}) =>
+              <FullscreenDimensionProvider width={201} height={400}>
+                {children}
+              </FullscreenDimensionProvider>,
+            seed: {
+              imageFiles: [{id: 100, permaId: 10, width: 400, height: 300},
+                           {id: 101, permaId: 11, width: 200, height: 400}]
+            }
+          }
+        );
+
+        expect(result.current).not.toContain(/aspectRatio/);
+        expect(result.current).toContain('minAspectRatio');
+      });
+
+      it('does not include aspect ratio class name if fullscreen is landscape', () => {
+        const {result} = renderHookInEntry(
+          () => useBackdropSectionClassNames({
+            file: useBackdropFile({collectionName: 'imageFiles', permaId: 10}),
+            mobileFile: useBackdropFile({collectionName: 'imageFiles', permaId: 11})
+          }),
+          {
+            wrapper: ({children}) =>
+              <FullscreenDimensionProvider width={390} height={300}>
+                {children}
+              </FullscreenDimensionProvider>,
+            seed: {
+              imageFiles: [{id: 100, permaId: 10, width: 400, height: 300},
+                           {id: 101, permaId: 11, width: 200, height: 400}]
+            }
+          }
+        );
+
+        expect(result.current).not.toContain(/aspectRatio/);
+        expect(result.current).not.toContain('minAspectRatio');
+      });
+    });
+
+    it('appends style tag with media-query-independant aspect ratio class', () => {
+      renderHookInEntry(
+        () => useBackdropSectionClassNames({
+          file: useBackdropFile({collectionName: 'imageFiles', permaId: 10})
+        }),
+        {
+          wrapper: ({children}) =>
+            <FullscreenDimensionProvider width={500} height={300}>
+              {children}
+            </FullscreenDimensionProvider>,
+          seed: {
+            imageFiles: [
+              {id: 100, permaId: 10, width: 400, height: 300},
+            ]
+          }
+        }
+      );
+
+      expect(document.head).toHaveStyleTagIncluding(
+        '.minAspectRatio {'
+      );
+    });
   });
 });
 
--- entry_types/scrolled/package/src/frontend/v2/useBackdropSectionClassNames.js
+++ entry_types/scrolled/package/src/frontend/v2/useBackdropSectionClassNames.js
@@ -1,4 +1,5 @@
 import {useIsomorphicLayoutEffect} from '../useIsomorphicLayoutEffect';
+import {useFullscreenDimensions} from '../Fullscreen';
 
 import styles from './Section.module.css';
 
@@ -6,62 +7,111 @@ export function useBackdropSectionClassNames(
   backdrop,
   {layout, exposeMotifArea, empty} = {}
 ) {
+  const fullscreenDimensions = useFullscreenDimensions();
+
   return [
     styles.section,
     styles[`layout-${layout || 'left'}`],
     exposeMotifArea && !empty && styles.exposeMotifArea,
-    useBackdropSectionClassName(backdrop.file),
-    useBackdropSectionClassName(backdrop.mobileFile,
-                                'Mobile',
-                                '(orientation: portrait) and '),
+    useAspectRatioClassName(backdrop.file,
+                            fullscreenDimensions),
+    useAspectRatioClassName(backdrop.mobileFile,
+                            fullscreenDimensions,
+                            {mobile: true})
   ].filter(Boolean);
 }
 
-function useBackdropSectionClassName(file, suffix = '', orientation = '') {
-  const aspectRatio = file && file.isReady &&
-                      Math.round(file.width / file.height * 1000);
-  const className = aspectRatio && `aspectRatio${suffix}${aspectRatio}`;
+function useAspectRatioClassName(
+  file, fullscreenDimensions, {mobile} = {}
+) {
+  const aspectRatio = file && file.isReady && getAspectRatio(file);
+  const className = getAspectRatioClassName(aspectRatio,
+                                            fullscreenDimensions,
+                                            mobile);
 
-  useAspectRatioMediaQueryStyleTag(aspectRatio, className, orientation);
+  useAspectRatioStyleTag(
+    aspectRatio,
+    className,
+    {orientation: mobile ? '(orientation: portrait) and ' : ''}
+  );
 
   return className;
 }
 
-function useAspectRatioMediaQueryStyleTag(aspectRatio, className, orientation) {
+function getAspectRatioClassName(aspectRatio, fullscreenDimensions, mobile) {
+  if (fullscreenDimensions.height) {
+    return (!mobile || isPortrait(fullscreenDimensions)) &&
+           hasAspectRatio(fullscreenDimensions, {min: aspectRatio}) &&
+           'minAspectRatio';
+  }
+  else {
+    const suffix = mobile ? 'Mobile' : '';
+    return aspectRatio && `aspectRatio${suffix}${aspectRatio}`;
+  }
+}
+
+function hasAspectRatio(rect, {min}) {
+  return min && (getAspectRatio(rect) > min);
+}
+
+function isPortrait(rect) {
+  return rect.width < rect.height;
+}
+
+function getAspectRatio(rect) {
+  return Math.round(rect.width / rect.height * 1000);
+}
+
+function useAspectRatioStyleTag(aspectRatio, className, {orientation}) {
   useIsomorphicLayoutEffect(() => {
     // global variable is set in script tag inserted by
     // GeneratedMediaQueriesHelper when style tags have been rendered
     // on the server
     if (!global.pageflowScrolledSSRAspectRatioMediaQueries &&
-        aspectRatio) {
-      ensureAspectRatioMediaQueryStyleTag(aspectRatio, className, orientation)
+        aspectRatio &&
+        className) {
+      ensureAspectRatioStyleTag(aspectRatio, className, orientation)
     }
   }, [aspectRatio, className]);
 }
 
-function ensureAspectRatioMediaQueryStyleTag(aspectRatio, className, orientation) {
+function ensureAspectRatioStyleTag(aspectRatio, className, orientation) {
   if (!document.head.querySelector(`[data-for="${className}"]`)) {
     const el = document.createElement('style');
     el.setAttribute('data-for', className);
-
-    // WARNING: This CSS snippet needs to be kept in sync with the
-    // corresponding snippet in GeneratedMediaQueriesHelper
-    el.innerHTML = `
-      @media ${orientation}(min-aspect-ratio: ${aspectRatio}/1000) {
-        section.${className} {
-           --backdrop-positioner-transform: var(--backdrop-positioner-min-ar-transform);
-           --backdrop-positioner-width: var(--backdrop-positioner-min-ar-width);
-           --backdrop-positioner-height: var(--backdrop-positioner-min-ar-height);
-
-           --motif-placeholder-width: var(--motif-placeholder-min-ar-width);
-
-           --motif-display-top: var(--motif-display-min-ar-top);
-           --motif-display-bottom: var(--motif-display-min-ar-bottom);
-           --motif-display-height: var(--motif-display-min-ar-height);
-        }
-      }
-    `;
+    el.innerHTML = getAspectRatioCSS(aspectRatio, className, orientation);
 
     document.head.appendChild(el);
   }
+}
+
+function getAspectRatioCSS(aspectRatio, className, orientation) {
+  if (className === 'minAspectRatio') {
+    return getAspectRatioRule('minAspectRatio');
+  }
+  else {
+    return `
+      @media ${orientation}(min-aspect-ratio: ${aspectRatio}/1000) {
+        ${getAspectRatioRule(className)}
+      }
+    `;
+  }
+}
+
+function getAspectRatioRule(className) {
+  // WARNING: This CSS snippet needs to be kept in sync with the
+  // corresponding snippet in GeneratedMediaQueriesHelper
+  return `
+    section.${className} {
+      --backdrop-positioner-transform: var(--backdrop-positioner-min-ar-transform);
+      --backdrop-positioner-width: var(--backdrop-positioner-min-ar-width);
+      --backdrop-positioner-height: var(--backdrop-positioner-min-ar-height);
+
+      --motif-placeholder-width: var(--motif-placeholder-min-ar-width);
+
+      --motif-display-top: var(--motif-display-min-ar-top);
+      --motif-display-bottom: var(--motif-display-min-ar-bottom);
+      --motif-display-height: var(--motif-display-min-ar-height);
+    }
+  `;
 }
