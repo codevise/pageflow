d682c5aa29e16102f4b02f93bbc58a9a382d6051
Tim Fischbach <tfischbach@codevise.de>
2022-10-12 15:05:40 +0200

    Fix info box position when all share providers are disabled
    Account for the fact that the share button will not be displayed in
    this case.
    
    REDMINE-19999

--- entry_types/scrolled/package/src/widgets/defaultNavigation/SharingMenu.js
+++ entry_types/scrolled/package/src/widgets/defaultNavigation/SharingMenu.js
@@ -5,17 +5,12 @@ import styles from "./SharingMenu.module.css";
 
 import {
   ThemeIcon,
-  useShareProviders,
   useShareUrl,
   useI18n,
-  Tooltip,
-  usePhonePlatform
+  Tooltip
 } from 'pageflow-scrolled/frontend';
 
-export function SharingMenu() {
-  const isPhonePlatform = usePhonePlatform();
-  const shareProviders = useShareProviders({isPhonePlatform : isPhonePlatform});
-
+export function SharingMenu({shareProviders}) {
   const shareUrl = useShareUrl();
   const {t} = useI18n();
 
@@ -37,18 +32,14 @@ export function SharingMenu() {
     })
   };
 
-  if(shareProviders.length > 0) {
-    return (
-      <Tooltip horizontalOffset={-70}
-               arrowPos={160}
-               content={renderShareLinks(shareProviders)}>
-        <button className={classNames(headerStyles.contextIcon)}
-                title={t('pageflow_scrolled.public.navigation.share')}>
-          <ThemeIcon name="share" />
-        </button>
-      </Tooltip>
-    )
-  } else {
-    return (null);
-  }
+  return (
+    <Tooltip horizontalOffset={-70}
+             arrowPos={160}
+             content={renderShareLinks(shareProviders)}>
+      <button className={classNames(headerStyles.contextIcon)}
+              title={t('pageflow_scrolled.public.navigation.share')}>
+        <ThemeIcon name="share" />
+      </button>
+    </Tooltip>
+  );
 }
