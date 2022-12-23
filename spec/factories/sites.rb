5538cdc5165b159e794cffd53d7d3d4b03cf6143
Tim Fischbach <tfischbach@codevise.de>
2022-12-23 09:53:41 +0100

    Ensure site factory does not create additional site
    Use the created site as default site of the site's account instead of
    creating an additional default site. This makes the creates fixture
    simpler and will make our life easier once we start to add uniqueness
    validations for site names in Hosted Pageflow to allow using site
    names to generate host names.
    
    REDMINE-20090

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -1,7 +1,9 @@
 module Pageflow
   FactoryBot.define do
-    factory :site, :class => Pageflow::Site do
-      account
+    factory :site, class: Pageflow::Site do
+      after(:build) do |site|
+        site.account ||= build(:account, default_site: site)
+      end
     end
   end
 end
