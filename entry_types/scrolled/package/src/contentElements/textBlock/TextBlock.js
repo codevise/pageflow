d9b782bd72a600f2053a7825db9db8277da6939a
Tim Fischbach <tfischbach@codevise.de>
2023-02-24 10:36:22 +0100

    Support quote design properties in text block quotes
    REDMINE-20172

--- entry_types/scrolled/package/src/contentElements/textBlock/TextBlock.js
+++ entry_types/scrolled/package/src/contentElements/textBlock/TextBlock.js
@@ -3,7 +3,8 @@ import classNames from 'classnames';
 import {
   EditableText,
   useContentElementConfigurationUpdate,
-  useI18n
+  useI18n,
+  useTheme
 } from 'pageflow-scrolled/frontend';
 
 import styles from './TextBlock.module.css';
@@ -11,8 +12,10 @@ import styles from './TextBlock.module.css';
 export function TextBlock(props) {
   const updateConfiguration = useContentElementConfigurationUpdate();
   const {t} = useI18n({locale: 'ui'});
+  const theme = useTheme();
 
   const className = classNames(styles.text,
+                               styles[`quoteDesign-${theme.options.quoteDesign || 'largeHanging'}`],
                                styles[`layout-${props.sectionProps.layout}`]);
   return (
     <EditableText value={props.configuration.value}
