6d28f448bb815ade2a1f450c438ad95aa8f9d23e
Tim Fischbach <tfischbach@codevise.de>
2023-05-16 16:29:38 +0200

    Strip FullscreenImage to basics
    Most of this functionality has now been moved into the pinch-zoom
    library.

--- entry_types/scrolled/package/src/contentElements/inlineImage/FullscreenImage.js
+++ entry_types/scrolled/package/src/contentElements/inlineImage/FullscreenImage.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react';
-import QuickPinchZoom, { make3dTransformValue } from 'react-quick-pinch-zoom';
+import React, {useRef, useCallback} from 'react';
+import QuickPinchZoom, {make3dTransformValue} from 'react-quick-pinch-zoom';
 import styles from "./FullscreenImage.module.css";
 
 export function FullscreenImage({
@@ -7,100 +7,26 @@ export function FullscreenImage({
   imageFile,
 }) {
   const imgRef = useRef();
-  const wrapperRef = useRef();
-  const [resetScaleValue, setResetScaleValue] = useState(false);
-  const url = imageFile.urls["large"];
-
-  useEffect(() => {
-    const { current: img } = imgRef;
-    if ( img ) {
-      img.classList.add(styles.transition);
-    }
-  }, [])
-
-  useEffect(() => {
-    function wheelEvent(event) {
-      setIsFullscreen(false)
-    }
-
-    window.addEventListener('wheel', wheelEvent);
-
-    return () => {
-      window.removeEventListener("wheel", wheelEvent)
-    };
-  }, [])
-
-  useEffect(() => {
-    let start = null;
-
-    function touchStart(event) {
-      start = event.changedTouches[0];
-    }
-
-    function touchEnd(event) {
-      let end = event.changedTouches[0];
-
-      if(end.screenY - start.screenY > 0 || end.screenY - start.screenY < 0)
-      {
-        setIsFullscreen(false);
-      }
-    }
-
-    window.addEventListener('touchstart', touchStart);
-    window.addEventListener('touchend', touchEnd);
-
-    return () => {
-      window.removeEventListener("touchstart", touchStart);
-      window.removeEventListener("touchend", touchEnd);
-    }
-  }, [])
 
   const onUpdate = useCallback(({ x, y, scale }) => {
-    const { current: img } = imgRef;
-
-    if (img) {
-      let value;
-
-      if ( scale === 5 && !resetScaleValue ) {
-        const x = 0, y = 0, scale = 1;
-
-        value = make3dTransformValue({ x, y, scale });
-        setResetScaleValue(!resetScaleValue)
-      }
-      else {       
-        value = make3dTransformValue({ x, y, scale });
-      }
-      img.style.setProperty("transform", value);
+    if (imgRef.current) {
+      imgRef.current.style.setProperty(
+        'transform',
+        make3dTransformValue({ x, y, scale })
+      );
     }
-  }, [resetScaleValue]);
-
-  function onClick(event) {
-    if (event.target === event.currentTarget) {
-      setIsFullscreen(false)
-    }
-  }
-
-  function onDoubleTap({x, y, scale}) {
-    alert(x, y, scale)
-  }
-
-  const isTouch = () => true
+  }, []);
 
   return (
-    <div className={styles.full}
-      onClick={onClick}
-      ref={wrapperRef}>
+    <div className={styles.full}>
       <QuickPinchZoom onUpdate={onUpdate}
-                isTouch={isTouch}
-                maxZoom={5}
-                wheelScaleFactor={500}
-                verticalPadding={10}
-                draggableUnZoomed={false}
-                onDoubleTap={onDoubleTap}>
-        <img src={url}
-          alt="img"
-          ref={imgRef}
-          className={styles.img}/>
+                      isTouch={() => true}
+                      maxZoom={5}
+                      draggableUnZoomed={false}>
+        <img src={imageFile.urls.large}
+             alt="img"
+             ref={imgRef}
+             className={styles.img}/>
       </QuickPinchZoom>
     </div>
   )
