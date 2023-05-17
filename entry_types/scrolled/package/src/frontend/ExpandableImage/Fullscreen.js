2ec785b9164c5be0cb75f3ec02489266a5acd477
Tim Fischbach <tfischbach@codevise.de>
2023-05-17 14:44:27 +0200

    Adjust padding for scroll bar when toggling image fullscreen
    Prevent document from changing its width and, thus, content from
    moving when the scroll bar is hidden on desktop browsers.

--- entry_types/scrolled/package/src/frontend/ExpandableImage/Fullscreen.js
+++ entry_types/scrolled/package/src/frontend/ExpandableImage/Fullscreen.js
@@ -7,8 +7,17 @@ export function Fullscreen({children}) {
   const root = useMemo(() => document.getElementById('fullscreenRoot'), []);
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => document.body.style.overflow = 'initial';
+    const resetScrollbarPadding = adjustScrollbarPadding(() => {
+      document.getElementById('root').setAttribute('inert', true);
+      document.body.style.overflow = 'hidden'
+    });
+
+    return () => {
+      resetScrollbarPadding();
+
+      document.getElementById('root').removeAttribute('inert', true)
+      document.body.style.overflow = 'initial';
+    }
   }, []);
 
   return ReactDOM.createPortal(
@@ -17,4 +26,22 @@ export function Fullscreen({children}) {
     </div>,
     root
   );
+}
+
+// Adapted from
+// https://github.com/tailwindlabs/headlessui/blob/9dff5456fa196cdc304e2ed17ef47962a9364ce7/packages/%40headlessui-react/src/hooks/document-overflow/adjust-scrollbar-padding.ts
+function adjustScrollbarPadding(hideScrollbar) {
+  const documentElement = document.documentElement
+  const ownerWindow = document.defaultView || window
+
+  const scrollbarWidthBefore = ownerWindow.innerWidth - documentElement.clientWidth
+
+  hideScrollbar();
+
+  const scrollbarWidthAfter = documentElement.clientWidth - documentElement.offsetWidth
+  const scrollbarWidth = scrollbarWidthBefore - scrollbarWidthAfter
+
+  document.documentElement.style.paddingRight = `${scrollbarWidth}px`;
+
+  return () => document.documentElement.style.paddingRight = '0';
 }
