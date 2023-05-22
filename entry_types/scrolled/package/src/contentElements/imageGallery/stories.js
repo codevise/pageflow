7335918ccd14685f95f9c802e856f644ca0f35e5
Tim Fischbach <tfischbach@codevise.de>
2023-05-22 15:27:12 +0200

    Add base storybook entry
    REDMINE-30325

---
+++ entry_types/scrolled/package/src/contentElements/imageGallery/stories.js
@@ -0,0 +1,8 @@
+import './frontend';
+import {storiesOfContentElement, filePermaId} from 'pageflow-scrolled/spec/support/stories';
+
+storiesOfContentElement(module, {
+  typeName: 'imageGallery',
+  baseConfiguration: {
+  }
+});
