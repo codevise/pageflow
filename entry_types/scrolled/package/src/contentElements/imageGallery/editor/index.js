7e8b863bff8ba2798397b93674e14850c5c9ac34
Tim Fischbach <tfischbach@codevise.de>
2023-07-11 09:35:03 +0200

    Remove feature flag for image gallery
    Make content element generally available.
    
    REDMINE-20325

--- entry_types/scrolled/package/src/contentElements/imageGallery/editor/index.js
+++ entry_types/scrolled/package/src/contentElements/imageGallery/editor/index.js
@@ -9,7 +9,6 @@ import pictogram from './pictogram.svg';
 editor.contentElementTypes.register('imageGallery', {
   pictogram,
   category: 'media',
-  featureName: 'image_gallery_content_element',
   supportedPositions: ['inline', 'sticky', 'left', 'right', 'wide', 'full'],
 
   configurationEditor({entry}) {
