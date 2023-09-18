c22ad25eab0c021615ef68dfcaca07dd5696fb1b
Tim Fischbach <tfischbach@codevise.de>
2023-09-18 11:14:39 +0200

    Make file fixtures work in Rails 6
    REDMINE-19438

--- entry_types/scrolled/spec/spec_helper.rb
+++ entry_types/scrolled/spec/spec_helper.rb
@@ -22,7 +22,11 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
-  config.fixture_path = './spec/fixtures'
+  if Pageflow::RailsVersion.experimental?
+    config.file_fixture_path = './spec/fixtures'
+  else
+    config.fixture_path = './spec/fixtures'
+  end
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
