936a5d7647a3c4dc304c1cb1bdcd99a93dd4d50a
Tim Fischbach <tfischbach@codevise.de>
2023-09-20 16:52:44 +0200

    Add error level for InfoBoxView
    REDMINE-20438

--- package/src/editor/views/InfoBoxView.js
+++ package/src/editor/views/InfoBoxView.js
@@ -4,6 +4,7 @@ export const InfoBoxView = Marionette.View.extend({
   className: 'info_box',
 
   render: function() {
+    this.$el.addClass(this.options.level)
     this.$el.html(this.options.text);
     return this;
   }
