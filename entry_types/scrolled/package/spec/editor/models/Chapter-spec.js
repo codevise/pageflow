5f33eb6ac3315e6d9be5c3a34211f791a9b157e8
Tim Fischbach <tfischbach@codevise.de>
2023-09-26 15:01:21 +0200

    Allow duplicating and inserting sections via Backbone chapter
    Use new controller end-points. Optimistically set positions.
    
    REDMINE-20217

--- entry_types/scrolled/package/spec/editor/models/Chapter-spec.js
+++ entry_types/scrolled/package/spec/editor/models/Chapter-spec.js
@@ -1,7 +1,7 @@
 import 'pageflow-scrolled/editor';
 import {ScrolledEntry} from 'editor/models/ScrolledEntry';
 import {factories, setupGlobals} from 'pageflow/testHelpers';
-import {normalizeSeed} from 'support';
+import {useFakeXhr, normalizeSeed} from 'support';
 
 describe('Chapter', () => {
   let testContext;
@@ -30,6 +30,188 @@ describe('Chapter', () => {
       const section = entry.chapters.first().addSection();
 
       expect(section.configuration.get('transition')).toEqual('beforeAfter');
+    });
+  });
+
+  describe('#insertSection', () => {
+    beforeEach(() => {
+      testContext.entry = factories.entry(ScrolledEntry, {}, {
+        entryTypeSeed: normalizeSeed({
+          chapters: [{id: 10}],
+          sections: [
+            {id: 100, chapterId: 10, position: 0},
+            {id: 101, chapterId: 10, position: 1}
+          ]
+        })
+      });
+    });
+
+    setupGlobals({
+      entry: () => testContext.entry
+    });
+
+    it('re-indexes pages when inserting before other section', () => {
+      const {entry} = testContext;
+      const chapter = entry.chapters.first();
+
+      chapter.insertSection({before: chapter.sections.last()});
+
+      expect(chapter.sections.pluck('position')).toEqual([0, 1, 2]);
+      expect(chapter.sections.pluck('id')).toEqual([100, undefined, 101]);
+    });
+
+    it('re-indexes pages when inserting after other section', () => {
+      const {entry} = testContext;
+      const chapter = entry.chapters.first();
+
+      chapter.insertSection({after: chapter.sections.first()});
+
+      expect(chapter.sections.pluck('position')).toEqual([0, 1, 2]);
+      expect(chapter.sections.pluck('id')).toEqual([100, undefined, 101]);
+    });
+
+    describe('with sparse positions', () => {
+      beforeEach(() => {
+        testContext.entry = factories.entry(ScrolledEntry, {}, {
+          entryTypeSeed: normalizeSeed({
+            chapters: [{id: 10}],
+            sections: [
+              {id: 100, chapterId: 10, position: 7},
+              {id: 101, chapterId: 10, position: 8}
+            ]
+          })
+        });
+      });
+
+      it('re-indexes pages when inserting after other section', () => {
+        const {entry} = testContext;
+        const chapter = entry.chapters.first();
+
+        chapter.insertSection({after: chapter.sections.first()});
+
+        expect(chapter.sections.pluck('position')).toEqual([7, 8, 9]);
+        expect(chapter.sections.pluck('id')).toEqual([100, undefined, 101]);
+      });
+    });
+  });
+
+  describe('#duplicateSection', () => {
+    beforeEach(() => {
+      testContext.entry = factories.entry(ScrolledEntry, {id: 1}, {
+        entryTypeSeed: normalizeSeed({
+          chapters: [{id: 10}],
+          sections: [
+            {id: 100, chapterId: 10, position: 0, configuration: {transition: 'scroll'}},
+            {id: 101, chapterId: 10, position: 1}
+          ],
+          contentElements: [
+            {id: 1000, permaId: 1, sectionId: 101, position: 0, typeName: 'inlineImage'},
+            {id: 1001, permaId: 2, sectionId: 101, position: 1, typeName: 'textBlock'}
+          ]
+        })
+      });
+    });
+
+    setupGlobals({
+      entry: () => testContext.entry
+    });
+
+    useFakeXhr(() => testContext);
+
+    it('re-indexes pages when inserting before other section', () => {
+      const {entry} = testContext;
+      const chapter = entry.chapters.first();
+
+      chapter.duplicateSection(chapter.sections.first());
+
+      expect(chapter.sections.pluck('position')).toEqual([0, 1, 2]);
+      expect(chapter.sections.pluck('id')).toEqual([100, undefined, 101]);
+    });
+
+    it('posts requests to duplicate action and adds content elements', () => {
+      const {entry, server, requests} = testContext;
+      const chapter = entry.chapters.first();
+
+      chapter.duplicateSection(chapter.sections.first());
+
+      expect(requests[0].method).toBe('POST');
+      expect(requests[0].url).toBe('/editor/entries/1/scrolled/sections/100/duplicate');
+
+      expect(chapter.sections.pluck('position')).toEqual([0, 1, 2])
+      expect(chapter.sections.pluck('id')).toEqual([100, undefined, 101])
+
+      server.respond(
+        'POST', '/editor/entries/1/scrolled/sections/100/duplicate',
+        [200, {'Content-Type': 'application/json'}, JSON.stringify({
+          id: 102,
+          configuration: {transition: 'scroll'},
+          contentElements: [
+            {
+              id: 1002,
+              permaId: 3,
+              sectionId: 102,
+              position: 0,
+              typeName: 'inlineImage',
+              configuration: {image: 5}
+            },
+            {
+              id: 1003,
+              permaId: 4,
+              sectionId: 102,
+              position: 0,
+              typeName: 'textBlock',
+              configuration: {value: 'Some text'}
+            },
+          ]
+        })]
+      );
+
+      expect(requests.length).toEqual(1);
+      expect(chapter.sections.pluck('id')).toEqual([100, 102, 101]);
+      const section = chapter.sections.get(102);
+
+      expect(section.configuration.get('transition')).toEqual('scroll');
+      expect(section.contentElements.pluck('id')).toEqual([1002, 1003]);
+      expect(section.contentElements.pluck('typeName')).toEqual(['inlineImage', 'textBlock']);
+      expect(section.contentElements.first().configuration.get('image')).toEqual(5);
+      expect(section.contentElements.last().configuration.get('value')).toEqual('Some text');
+    });
+
+    it('does not use duplicate url on subsequent save', () => {
+      const {entry, server, requests} = testContext;
+      const chapter = entry.chapters.first();
+
+      const section = chapter.duplicateSection(chapter.sections.first());
+
+      server.respond(
+        'POST', '/editor/entries/1/scrolled/sections/100/duplicate',
+        [200, {'Content-Type': 'application/json'}, JSON.stringify({
+          id: 102,
+          configuration: {transition: 'scroll'},
+          contentElements: [
+            {
+              id: 1002,
+              permaId: 3,
+              sectionId: 102,
+              position: 0,
+              typeName: 'inlineImage',
+              configuration: {image: 5}
+            },
+            {
+              id: 1003,
+              permaId: 4,
+              sectionId: 102,
+              position: 0,
+              typeName: 'textBlock',
+              configuration: {value: 'Some text'}
+            },
+          ]
+        })]
+      );
+      section.save();
+
+      expect(requests.length).toEqual(2);
+      expect(requests[1].url).toBe('/editor/entries/1/scrolled/sections/102');
     });
   });
 });
