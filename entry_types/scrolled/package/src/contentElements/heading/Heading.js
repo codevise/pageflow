9725158f6556c739ac86db2a47cfcf9eaa952f12
Tim Fischbach <tfischbach@codevise.de>
2023-10-30 06:43:14 +0100

    Add tagline and subtitle to heading content element
    REDMINE-20488

--- entry_types/scrolled/package/src/contentElements/heading/Heading.js
+++ entry_types/scrolled/package/src/contentElements/heading/Heading.js
@@ -6,9 +6,11 @@ import {
   Text,
   EditableInlineText,
   useContentElementConfigurationUpdate,
+  useContentElementEditorState,
   useDarkBackground,
   useI18n,
-  contentElementWidths
+  contentElementWidths,
+  utils
 } from 'pageflow-scrolled/frontend';
 
 import styles from './Heading.module.css';
@@ -19,47 +21,78 @@ export function Heading({configuration, sectionProps, contentElementWidth}) {
   const updateConfiguration = useContentElementConfigurationUpdate();
   const {t} = useI18n({locale: 'ui'});
   const darkBackground = useDarkBackground();
+  const {isSelected} = useContentElementEditorState();
 
   const legacyValue = configuration.children;
   const Tag = firstSectionInEntry ? 'h1' : 'h2';
 
   const forcePaddingTop = firstSectionInEntry && !('marginTop' in configuration);
 
-  return (
-    <Tag className={classNames(styles.root,
-                               'scope-headings',
-                               configuration.typographyVariant &&
-                               `typography-heading-${configuration.typographyVariant}`,
-                               darkBackground ? styles.light : styles.dark,
-                               {[styles.forcePaddingTop]: forcePaddingTop},
-                               {[styles[sectionProps.layout]]:
-                                 contentElementWidth > contentElementWidths.md ||
-                                 sectionProps.layout === 'centerRagged'},
-                               {[withShadowClassName]: !sectionProps.invert})}
-         style={{color: paletteColor(configuration.color)}}>
-      <Text scaleCategory={getScaleCategory(configuration, firstSectionInEntry)}
-            inline={true}>
-        <EditableInlineText value={configuration.value}
-                            defaultValue={legacyValue}
-                            hyphens={configuration.hyphens}
-                            placeholder={firstSectionInEntry ?
-                                         t('pageflow_scrolled.inline_editing.type_title') :
-                                         t('pageflow_scrolled.inline_editing.type_heading')}
-                            onChange={value => updateConfiguration({value})} />
+  function renderSubtitle(name) {
+    const value = configuration[name];
+
+    if (!isSelected && utils.isBlankEditableTextValue(value)) {
+      return null;
+    }
+
+    return (
+      <Text scaleCategory={getScaleCategory(configuration, firstSectionInEntry, name)}>
+        <div className={styles[name]}
+             role="doc-subtitle">
+          <EditableInlineText value={value}
+                              hyphens={configuration.hyphens}
+                              placeholder={t(`pageflow_scrolled.inline_editing.type_${name}`)}
+                              onChange={value => updateConfiguration({[name]: value})} />
+        </div>
       </Text>
-    </Tag>
+    );
+  }
+
+  return (
+    <header className={classNames(styles.root,
+                                  {[styles.forcePaddingTop]: forcePaddingTop},
+                                  {[styles[sectionProps.layout]]:
+                                    contentElementWidth > contentElementWidths.md ||
+                                   sectionProps.layout === 'centerRagged'},
+                                  {[withShadowClassName]: !sectionProps.invert})}>
+      {renderSubtitle('tagline')}
+      <Tag className={classNames(styles.main,
+                                 'scope-headings',
+                                 configuration.typographyVariant &&
+                                 `typography-heading-${configuration.typographyVariant}`,
+                                 darkBackground ? styles.light : styles.dark)}
+           style={{color: paletteColor(configuration.color)}}>
+        <Text scaleCategory={getScaleCategory(configuration, firstSectionInEntry)}
+              inline={true}>
+          <EditableInlineText value={configuration.value}
+                              defaultValue={legacyValue}
+                              hyphens={configuration.hyphens}
+                              placeholder={firstSectionInEntry ?
+                                           t('pageflow_scrolled.inline_editing.type_title') :
+                                           t('pageflow_scrolled.inline_editing.type_heading')}
+                              onChange={value => updateConfiguration({value})} />
+        </Text>
+      </Tag>
+      {renderSubtitle('subtitle')}
+    </header>
   );
 }
 
-function getScaleCategory(configuration, firstSectionInEntry) {
+function getScaleCategory(configuration, firstSectionInEntry, suffix = '') {
+  const base = `heading${capitalize(suffix)}`;
+
   switch (configuration.textSize) {
     case 'large':
-      return 'heading-lg';
+      return `${base}-lg`;
     case 'medium':
-      return 'heading-md';
+      return `${base}-md`;
     case 'small':
-      return 'heading-sm';
+      return `${base}-sm`;
     default:
-      return firstSectionInEntry ? 'heading-lg' : 'heading-sm';
+      return firstSectionInEntry ? `${base}-lg` : `${base}-sm`;
   }
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
