0b91045785a45fa2cf0cf83e8b183b6f4224d38b
Tim Fischbach <tfischbach@codevise.de>
2023-11-14 06:50:26 +0100

    View paths are now immutable
    REDMINE-20487

--- spec/support/helpers/stub_template_in_helper_specs.rb
+++ spec/support/helpers/stub_template_in_helper_specs.rb
@@ -1,7 +1,11 @@
 RSpec.configure do |config|
   module StubTemplateTestHelper
     def stub_template(hash)
-      view.view_paths.unshift(ActionView::FixtureResolver.new(hash))
+      if view.view_paths.respond_to?(:unshift)
+        view.view_paths.unshift(ActionView::FixtureResolver.new(hash))
+      else
+        view.lookup_context.prepend_view_paths([ActionView::FixtureResolver.new(hash)])
+      end
     end
   end
 
