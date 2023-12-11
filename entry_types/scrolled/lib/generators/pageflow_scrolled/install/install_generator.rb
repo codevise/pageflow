631a636f4c5212ced500d3c342e7c1953271bf29
Tim Fischbach <tfischbach@codevise.de>
2023-12-11 11:52:55 +0100

    Decouple host application from scrolled widget packs
    Implicitly define packs via pageflow-scrolled/config. Allow importing
    all widget for SSR.
    
    REDMINE-20093

--- entry_types/scrolled/lib/generators/pageflow_scrolled/install/install_generator.rb
+++ entry_types/scrolled/lib/generators/pageflow_scrolled/install/install_generator.rb
@@ -129,26 +129,7 @@ module PageflowScrolled
         create_file 'app/javascript/packs/pageflow-scrolled-server.js', <<-JS
           import 'pageflow-scrolled/frontend-server';
           import 'pageflow-scrolled/contentElements-frontend';
-          import 'pageflow-scrolled/widgets/defaultNavigation';
-          import 'pageflow-scrolled/widgets/consentBar';
-        JS
-      end
-
-      def default_navigation_widget_packs
-        widget_dir = 'app/javascript/packs/pageflow-scrolled/widgets'
-
-        create_file File.join(widget_dir, 'defaultNavigation.js'), <<-JS
-          import 'pageflow-scrolled/widgets/defaultNavigation';
-          import 'pageflow-scrolled/widgets/defaultNavigation.css';
-        JS
-      end
-
-      def consent_bar_widget_packs
-        widget_dir = 'app/javascript/packs/pageflow-scrolled/widgets'
-
-        create_file File.join(widget_dir, 'consentBar.js'), <<-JS
-          import 'pageflow-scrolled/widgets/consentBar';
-          import 'pageflow-scrolled/widgets/consentBar.css';
+          import 'pageflow-scrolled/widgets-server';
         JS
       end
 
---
+++ entry_types/scrolled/package/widgets-server.js
@@ -0,0 +1,2 @@
+import 'pageflow-scrolled/widgets/defaultNavigation';
+import 'pageflow-scrolled/widgets/consentBar';
