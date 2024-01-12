3690aaf5390fd5e2098d6eac0b4d3439ec71fabe
Tim Fischbach <tfischbach@codevise.de>
2024-01-12 10:48:04 +0100

    Remove Rails < 7 case distinction
    REDMINE-20487

--- spec/support/helpers/stub_template_in_helper_specs.rb
+++ spec/support/helpers/stub_template_in_helper_specs.rb
@@ -1,11 +1,7 @@
 RSpec.configure do |config|
   module StubTemplateTestHelper
     def stub_template(hash)
-      if view.view_paths.respond_to?(:unshift)
-        view.view_paths.unshift(ActionView::FixtureResolver.new(hash))
-      else
-        view.lookup_context.prepend_view_paths([ActionView::FixtureResolver.new(hash)])
-      end
+      view.lookup_context.prepend_view_paths([ActionView::FixtureResolver.new(hash)])
     end
   end
 
