a7ca3a7557d48fed4e5c19ca677c2a34e2f1eb66
Tim Fischbach <tfischbach@codevise.de>
2024-06-28 16:37:16 +0200

    Allow opening external privacy settings via privacy link
    This is an ugly workaround to allow triggering a global function that
    displays a consent layer. This wants to be an official JS API instead
    someday. But since Pageflow itself currently does not have a concept
    of external consent, today is not that day.
    
    REDMINE-20765

--- entry_types/scrolled/package/src/widgets/defaultNavigation/LegalInfoLink.js
+++ entry_types/scrolled/package/src/widgets/defaultNavigation/LegalInfoLink.js
@@ -2,6 +2,22 @@ import React from 'react';
 import styles from "./LegalInfoMenu.module.css";
 
 export function LegalInfoLink(props) {
+  // eslint-disable-next-line no-script-url
+  if (props.url === 'javascript:pageflowDisplayPrivacySettings()' && props.label) {
+    return (
+      <div>
+        <a href="#privacySettings"
+           onClick={event => {
+             window.pageflowDisplayPrivacySettings();
+             event.preventDefault();
+           }}
+           className={styles.legalInfoLink}
+           dangerouslySetInnerHTML={{__html: props.label}}>
+        </a>
+      </div>
+    )
+  }
+
   return (
     <div>
       {props.label && props.url &&
