ffe3d2b314054787f34d55df2d437fc680dc8e26
Daniel Kuhn <dkuhn@codevise.de>
2024-10-09 13:35:14 +0200

    Create helper for available_public_locales

--- spec/helpers/pageflow/admin/locales_helper_spec.rb
+++ spec/helpers/pageflow/admin/locales_helper_spec.rb
@@ -12,6 +12,16 @@ module Pageflow
           expect(pairs).to eq([['Deutsch', 'de']])
         end
       end
+
+      describe '#available_public_locales_collection' do
+        it 'returns collection of configured available public locales for select' do
+          Pageflow.config.available_public_locales = [:de, :en, :fr]
+
+          pairs = available_public_locales_collection
+
+          expect(pairs).to eq([['Deutsch', 'de'], ['English', 'en'], ["Français", "fr"]])
+        end
+      end
     end
   end
 end
