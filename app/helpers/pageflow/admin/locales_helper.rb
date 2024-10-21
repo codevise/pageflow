0fe130f431f02ed385fb502ac380fe387aea6ac2
Daniel Kuhn <dkuhn@codevise.de>
2024-10-21 16:42:47 +0200

    Introduce helper method for translating public locale names

--- app/helpers/pageflow/admin/locales_helper.rb
+++ app/helpers/pageflow/admin/locales_helper.rb
@@ -1,19 +1,23 @@
 module Pageflow
   module Admin
     module LocalesHelper
+      def locale_name_for(locale, i18n_key = 'pageflow.public._language')
+        I18n.t(i18n_key, locale: locale)
+      end
+
       def available_locales_collection
         locales_collection(Pageflow.config.available_locales, 'language')
       end
 
       def available_public_locales_collection
-        locales_collection(Pageflow.config.available_public_locales, 'pageflow.public._language')
+        locales_collection(Pageflow.config.available_public_locales)
       end
 
       private
 
-      def locales_collection(locales, i18n_key)
+      def locales_collection(locales, i18n_key = 'pageflow.public._language')
         locales.map do |locale|
-          [I18n.t(i18n_key, locale: locale), locale.to_s]
+          [locale_name_for(locale, i18n_key), locale.to_s]
         end
       end
     end
