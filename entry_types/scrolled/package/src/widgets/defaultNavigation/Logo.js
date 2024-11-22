26ec85f7a894455fa28c01090456c4924c1e4513
Tim Fischbach <tfischbach@codevise.de>
2024-11-22 08:57:50 +0100

    Allow overriding navigation bar logo settings via prop
    Can be used to construct custom navigation bar widgets based on
    default navigation that are not limited to only showing theme logos.
    
    REDMINE-20856

--- entry_types/scrolled/package/src/widgets/defaultNavigation/Logo.js
+++ entry_types/scrolled/package/src/widgets/defaultNavigation/Logo.js
@@ -5,22 +5,22 @@ import {useTheme} from 'pageflow-scrolled/frontend';
 
 import styles from './DefaultNavigation.module.css';
 
-export function Logo() {
+export function Logo({srcMobile, srcDesktop, url, altText}) {
   const theme = useTheme();
   return (
     <a target="_blank"
        rel="noopener noreferrer"
-       href={theme.options.logoUrl}
+       href={url || theme.options.logoUrl}
        className={classNames(
          styles.logo,
          {[styles.centerMobileLogo]:
            theme.options.defaultNavigationMobileLogoPosition === 'center'}
        )}>
       <picture>
-        <source media="(max-width: 780px)" srcSet={theme.assets.logoMobile} />
-        <source media="(min-width: 781px)" srcSet={theme.assets.logoDesktop} />
-        <img src={theme.assets.logoDesktop}
-             alt={theme.options.logoAltText} />
+        <source media="(max-width: 780px)" srcSet={srcMobile || theme.assets.logoMobile} />
+        <source media="(min-width: 781px)" srcSet={srcDesktop || theme.assets.logoDesktop} />
+        <img src={srcDesktop || theme.assets.logoDesktop}
+             alt={altText || theme.options.logoAltText} />
       </picture>
     </a>
   );
