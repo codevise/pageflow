f7d93cffad9ca771fbe2a39118bed3de65c9dfa4
Tim Fischbach <tfischbach@codevise.de>
2024-12-19 11:07:56 +0100

    Remove external links "new tab" tooltip
    Will be handled by EditableLink.
    
    REDMINE-20852, REDMINE-20896

--- entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLink.js
+++ entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLink.js
@@ -1,18 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import styles from './ExternalLink.module.css';
 import {
   InlineFileRights,
   useFileWithInlineRights,
-  useI18n,
   useContentElementEditorState
 } from 'pageflow-scrolled/frontend';
 
 import {Thumbnail} from './Thumbnail';
 
 export function ExternalLink(props) {
-  const [hideTooltip, setHideTooltip] = useState(true);
-  const {t} = useI18n({locale: 'ui'});
   const {isEditable, isSelected} = useContentElementEditorState();
   const thumbnailImageFile = useFileWithInlineRights({
     configuration: props,
@@ -26,33 +23,8 @@ export function ExternalLink(props) {
       if (!props.open_in_new_tab || !isSelected) {
         event.preventDefault();
       }
-
-      if (!props.open_in_new_tab && isSelected) {
-        setHideTooltip(false);
-      }
     }
   };
-
-  const onMouseLeave = function () {
-    setHideTooltip(true);
-  };
-
-  function renderNewTabTooltip() {
-    if (isEditable) {
-      const onTooltipClick = function () {
-        window.open(url, '_blank');
-        setHideTooltip(true);
-      };
-
-      return (
-        <div className={classNames({[styles.hidden]: hideTooltip}, styles.tooltip)}
-             onClick={onTooltipClick}>
-          {t('pageflow_scrolled.inline_editing.external_links.open_in_new_tab_message')}
-          {<span>{t('pageflow_scrolled.inline_editing.external_links.open_in_new_tab')}</span>}
-        </div>
-      )
-    }
-  }
 
   return (
     <LinkOrDiv
@@ -66,7 +38,6 @@ export function ExternalLink(props) {
              [props.title, props.description].filter(Boolean).join("\n") :
              null}
       onClick={onClick}
-      onMouseLeave={onMouseLeave}
       target={props.open_in_new_tab ? '_blank' : '_self'}
       rel={props.open_in_new_tab ? 'noopen noreferrer' : undefined}>
       <div className={styles.thumbnail}>
@@ -84,7 +55,6 @@ export function ExternalLink(props) {
           <p className={styles.link_desc}>{props.description}</p>
         </div>
       </div>
-      {renderNewTabTooltip()}
     </LinkOrDiv>
   );
 }
@@ -106,6 +76,7 @@ function LinkOrDiv({children, ...props}) {
     );
   }
 }
+
 function ensureAbsolute(url) {
   if (!url || url.match(/^(https?:)?\/\//)) {
     return url;
