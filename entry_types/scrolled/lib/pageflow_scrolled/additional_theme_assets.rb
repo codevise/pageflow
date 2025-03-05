bda241420bee808b75da9be58d1e8e556f4378e2
Tim Fischbach <tfischbach@codevise.de>
2025-03-05 10:18:35 +0100

    Allow including additional theme customization files in seed data
    Can be used to provide configurable assets for widgets defined in
    plugins.
    
    REDMINE-20913

---
+++ entry_types/scrolled/lib/pageflow_scrolled/additional_theme_assets.rb
@@ -0,0 +1,27 @@
+module PageflowScrolled
+  # Register additional theme customization files that shall be
+  # included in theme assets seed data.
+  class AdditionalThemeAssets
+    # @api private
+    def initialize
+      @assets = []
+    end
+
+    # Register additional theme customization files and specify style
+    # that shall be used.
+    def register(theme_file_role:, theme_file_style: :resized)
+      @assets << AdditionalThemeAsset.new(
+        theme_file_role,
+        theme_file_style
+      )
+    end
+
+    # @api private
+    def each(&block)
+      @assets.each(&block)
+    end
+
+    # @api private
+    AdditionalThemeAsset = Struct.new(:theme_file_role, :theme_file_style)
+  end
+end
