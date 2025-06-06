025c293764950e751a727cdcaf9793fd505bf6bb
Tim Fischbach <tfischbach@codevise.de>
2025-06-06 08:30:56 +0200

    Support full width and scroller case in teaser lists
    - Activate custom margin to allow placing scroll buttons in content
      margin.
    
    - Base item width on reference width instead of container width when
      element has full width. This allows aligning items width different
      content max widths even if element spans full width. Calculate
      padding to align items with other section content based on content
      margin and section max width.
    
    - Optionally render scroller. When full width is used, apply padding
      as scoll padding + margin for the first and last item to make
      padding at the end of the scroller work.
    
    - For centered layout, initially align first and last item with
      section content when scroller is moved to beginning/end
      respectively.
    
    REDMINE-20981

--- entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLinkList.js
+++ entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/ExternalLinkList.js
@@ -5,9 +5,11 @@ import {
   useContentElementEditorState,
   useContentElementLifecycle,
   useDarkBackground,
-  contentElementWidthName
+  contentElementWidthName,
+  contentElementWidths
 } from 'pageflow-scrolled/frontend';
 import {ExternalLink} from './ExternalLink';
+import {Scroller} from './Scroller';
 import styles from './ExternalLinkList.module.css';
 
 import textPositionBelowStyles from './textPositons/below.module.css';
@@ -72,42 +74,58 @@ export function ExternalLinkList(props) {
                              textPositionRightStyles :
                              textPositionBelowStyles;
 
+  const scrollerEnabled = props.configuration.enableScroller === 'always';
+  const fullWidth = props.contentElementWidth === contentElementWidths.full;
+  const linkAlignment = scrollerEnabled ? 'left' : props.configuration.linkAlignment;
+
   return (
-    <div className={styles.container}>
-      <ul className={classNames(
-            styles.list,
-            styles[`textPosition-${textPosition}`],
+    <div className={classNames({[styles.contentMargin]: props.customMargin || fullWidth})}>
+      <Scroller enabled={scrollerEnabled}>
+        {({scrollerRef, handleScroll}) =>
+          <div className={classNames(styles.container, {[styles.fullContainer]: fullWidth})}>
+            <ul ref={scrollerRef}
+                className={classNames(
+                  styles.list,
+                  styles[`textPosition-${textPosition}`],
+                  styles[`layout-${layout}`],
+                  {[styles.full]: fullWidth},
+                  {[styles.scroller]: scrollerEnabled},
 
-            props.configuration.variant &&
-            `scope-externalLinkList-${props.configuration.variant}`,
+                  props.configuration.variant &&
+                  `scope-externalLinkList-${props.configuration.variant}`,
 
-            textPositionStyles.list,
-            textPositionStyles[`layout-${layout}`],
-            textPositionStyles[`width-${contentElementWidthName(props.configuration.width)}`],
-            textPositionStyles[`linkWidth-${linkWidth}`],
-            textPositionStyles[`linkAlignment-${props.configuration.linkAlignment}`],
-            textPositionStyles[`textPosition-${textPosition}`]
-          )}
-          style={{'--overlay-opacity': (props.configuration.overlayOpacity || 70) / 100,
-                  '--thumbnail-aspect-ratio': `var(--theme-aspect-ratio-${props.configuration.thumbnailAspectRatio || 'wide'})`}}
-          onClick={handleListClick}>
-        {linkList.map((link, index) =>
-          <ExternalLink {...link}
-                        key={link.id}
-                        configuration={props.configuration}
-                        thumbnailAspectRatio={props.configuration.thumbnailAspectRatio}
-                        thumbnailSize={props.configuration.thumbnailSize || 'small'}
-                        thumbnailFit={props.configuration.thumbnailFit || 'cover'}
-                        textPosition={props.configuration.textPosition || 'below'}
-                        textSize={props.configuration.textSize || 'small'}
-                        darkBackground={darkBackground}
-                        loadImages={shouldLoad}
-                        outlined={isSelected}
-                        highlighted={highlightedIndex === index}
-                        selected={link.id === selectedItemId && isSelected}
-                        onClick={event => handleItemClick(event, link.id)} />
-        )}
-      </ul>
+                  textPositionStyles.list,
+                  textPositionStyles[`layout-${layout}`],
+                  textPositionStyles[`width-${contentElementWidthName(props.contentElementWidth)}`],
+                  textPositionStyles[`linkWidth-${fullWidth ? 'full-' : ''}${linkWidth}`],
+                  textPositionStyles[`linkAlignment-${linkAlignment}`],
+                  textPositionStyles[`textPosition-${textPosition}`],
+                  {[textPositionStyles.scroller]: scrollerEnabled}
+                )}
+                style={{'--overlay-opacity': (props.configuration.overlayOpacity || 70) / 100,
+                        '--thumbnail-aspect-ratio': `var(--theme-aspect-ratio-${props.configuration.thumbnailAspectRatio || 'wide'})`}}
+                onScroll={handleScroll}
+                onClick={handleListClick}>
+              {linkList.map((link, index) =>
+                <ExternalLink {...link}
+                              key={link.id}
+                              configuration={props.configuration}
+                              thumbnailAspectRatio={props.configuration.thumbnailAspectRatio}
+                              thumbnailSize={props.configuration.thumbnailSize || 'small'}
+                              thumbnailFit={props.configuration.thumbnailFit || 'cover'}
+                              textPosition={props.configuration.textPosition || 'below'}
+                              textSize={props.configuration.textSize || 'small'}
+                              darkBackground={darkBackground}
+                              loadImages={shouldLoad}
+                              outlined={isSelected}
+                              highlighted={highlightedIndex === index}
+                              selected={link.id === selectedItemId && isSelected}
+                              onClick={event => handleItemClick(event, link.id)} />
+              )}
+            </ul>
+          </div>
+        }
+      </Scroller>
     </div>
   );
 }
---
+++ entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/Scroller.js
@@ -0,0 +1,84 @@
+import React, {useRef} from 'react';
+import Measure from 'react-measure';
+
+import {ScrollButton} from 'pageflow-scrolled/frontend';
+
+export function Scroller({enabled, children}) {
+  const ref = useRef();
+
+  if (!enabled) {
+    return children({});
+  }
+
+  return (
+    <Measure scroll client innerRef={ref}>
+      {({contentRect, measureRef, measure}) => {
+        const canScrollLeft =
+          contentRect.scroll.left > 0;
+
+        const canScrollRight =
+          contentRect.scroll.width > contentRect.client.width &&
+          contentRect.scroll.left < contentRect.scroll.width - contentRect.client.width;
+
+        function scrollBy(x) {
+          const list = ref.current;
+          const listRect = list.getBoundingClientRect();
+
+          if (x > 0) {
+            for (let i = 0; i < list.children.length; i++) {
+              const child = list.children[i];
+              const rect = child.getBoundingClientRect();
+
+              if (Math.floor(rect.right - listRect.left) > contentRect.client.width) {
+                ref.current.scrollTo(child.offsetLeft - list.firstChild.offsetLeft, 0)
+                break;
+              }
+            }
+          }
+          else {
+            let lastPartiallyVisibleChildRect;
+
+            for (let i = list.children.length - 1; i >= 0; i--) {
+              const child = list.children[i];
+              const rect = child.getBoundingClientRect();
+
+              if (rect.left < listRect.left) {
+                lastPartiallyVisibleChildRect = rect;
+                break;
+              }
+            }
+
+            if (!lastPartiallyVisibleChildRect) {
+              return;
+            }
+
+            for (let i = 0; i < list.children.length - 1; i++) {
+              const child = list.children[i];
+              const rect = child.getBoundingClientRect();
+
+              if (Math.floor(lastPartiallyVisibleChildRect.right - rect.left) <= contentRect.client.width) {
+                ref.current.scrollTo(child.offsetLeft - list.firstChild.offsetLeft, 0)
+                break;
+              }
+            }
+          }
+        }
+
+        return (
+          <>
+            <ScrollButton direction="left"
+                          disabled={!canScrollLeft}
+                          onClick={() => scrollBy(-1)} />
+            <ScrollButton direction="right"
+                          disabled={!canScrollRight}
+                          onClick={() => scrollBy(1)}/>
+            {children({
+              scrollerRef: measureRef,
+              handleScroll: () => measure()
+            })}
+          </>
+        )
+      }}
+    </Measure>
+  );
+}
