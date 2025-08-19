f38b8e545a68da332c71374dea5c96a871d55f74
Tim Fischbach <tfischbach@codevise.de>
2025-08-19 12:38:33 +0200

    Set widget-margin-top custom property on document element
    Make current height of navigation bar accessible in CSS by toggling a
    data attribute when navigation expands/collapses.
    
    REDMINE-20481

--- entry_types/scrolled/package/spec/widgets/defaultNavigation/DefaultNavigation/styling-spec.js
+++ entry_types/scrolled/package/spec/widgets/defaultNavigation/DefaultNavigation/styling-spec.js
@@ -4,10 +4,13 @@ import {DefaultNavigation} from 'widgets/defaultNavigation/DefaultNavigation';
 
 import {useFakeTranslations} from 'pageflow/testHelpers';
 import {renderInEntry} from 'pageflow-scrolled/testHelpers';
+import {act} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('DefaultNavigation - Styling', () => {
   useFakeTranslations({});
+
+  afterEach(() => jest.restoreAllMocks());
 
   it('does not have style attribute on header by default', () => {
     const {container} = renderInEntry(
@@ -25,5 +28,42 @@ describe('DefaultNavigation - Styling', () => {
     expect(container.querySelector('header')).toHaveStyle(
       {'--theme-accent-color': 'var(--theme-palette-color-brand-blue)'}
     );
+  });
+
+  it('toggles data-default-navigation-expanded attribute based on scroll direction', () => {
+    // Mock window.scrollY and getBoundingClientRect to simulate scroll positions
+    Object.defineProperty(window, 'scrollY', {
+      writable: true,
+      value: 0
+    });
+
+    jest.spyOn(document.body, 'getBoundingClientRect').mockImplementation(() => ({
+      top: -window.scrollY,
+      left: 0,
+      right: 1024,
+      bottom: 768 - window.scrollY,
+      width: 1024,
+      height: 768
+    }));
+
+    renderInEntry(
+      <DefaultNavigation configuration={{}} />
+    );
+
+    expect(document.documentElement).toHaveAttribute('data-default-navigation-expanded');
+
+    act(() => {
+      window.scrollY = 100;
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(document.documentElement).not.toHaveAttribute('data-default-navigation-expanded');
+
+    act(() => {
+      window.scrollY = 50;
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(document.documentElement).toHaveAttribute('data-default-navigation-expanded');
   });
 });
