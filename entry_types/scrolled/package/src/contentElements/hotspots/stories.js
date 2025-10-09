39c1d20fa12d18cbfd3f7b13b9d1dbbaec906dae
Tim Fischbach <tfischbach@codevise.de>
2025-10-09 09:00:54 +0200

    Allow hiding hotspots pager buttons via theme option
    Themes can now disable the pager navigation buttons in hotspots content
    elements to provide a cleaner appearance when desired.
    
    REDMINE-21130

--- entry_types/scrolled/package/src/contentElements/hotspots/stories.js
+++ entry_types/scrolled/package/src/contentElements/hotspots/stories.js
@@ -26,6 +26,65 @@ storiesOfContentElement(module, {
     {
       name: 'With Caption',
       configuration: {caption: 'Some text here'}
+    },
+    {
+      name: 'With pan zoom enabled',
+      configuration: {
+        enablePanZoom: 'always',
+        areas: [
+          {
+            id: 1,
+            outline: [[20, 30], [50, 30], [50, 50], [20, 50]],
+            indicatorPosition: [25, 45]
+          },
+          {
+            id: 2,
+            outline: [[60, 40], [80, 40], [80, 60], [60, 60]],
+            indicatorPosition: [65, 55]
+          }
+        ],
+        tooltipTexts: {
+          1: {
+            title: [{children: [{text: 'First Area'}]}],
+            description: [{type: 'paragraph', children: [{text: 'First area description'}]}]
+          },
+          2: {
+            title: [{children: [{text: 'Second Area'}]}],
+            description: [{type: 'paragraph', children: [{text: 'Second area description'}]}]
+          }
+        }
+      }
+    },
+    {
+      name: 'With hidden pager buttons',
+      configuration: {
+        enablePanZoom: 'always',
+        areas: [
+          {
+            id: 1,
+            outline: [[20, 30], [50, 30], [50, 50], [20, 50]],
+            indicatorPosition: [25, 45]
+          },
+          {
+            id: 2,
+            outline: [[60, 40], [80, 40], [80, 60], [60, 60]],
+            indicatorPosition: [65, 55]
+          }
+        ],
+        tooltipTexts: {
+          1: {
+            title: [{children: [{text: 'First Area'}]}],
+            description: [{type: 'paragraph', children: [{text: 'First area description'}]}]
+          },
+          2: {
+            title: [{children: [{text: 'Second Area'}]}],
+            description: [{type: 'paragraph', children: [{text: 'Second area description'}]}]
+          }
+        }
+      },
+      themeOptions: {
+        hotspotsPagerButtonsHidden: true
+      }
     }
   ],
   inlineFileRights: true
