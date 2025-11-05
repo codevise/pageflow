a3ad07dd58aa131873e82ef9a1dcfeec42fb7a25
Tim Fischbach <tfischbach@codevise.de>
2025-11-05 10:51:13 +0100

    Support flippable teasers
    When backfaces are enabled:
    
    * Render link as button on the backface
    
    * Disable link preview on the card to prevent collision with flip
      action button. Enable link preview on the link button instead.
    
    * Allow flipping card via click or via invisible/pointer inaccessible
      button for keyboard/screen reader control.
    
    Make sure card and card wrapper take full height in overlay text
    position to give front and back equal height.
    
    REDMINE-21163

---
+++ entry_types/scrolled/package/src/contentElements/externalLinkList/frontend/FlippedItemProvider.js
@@ -0,0 +1,44 @@
+import React, {useContext, useCallback, useEffect, useMemo, useRef, useState} from 'react';
+
+const FlippedItemContext = React.createContext();
+
+export function FlippedItemProvider({children}) {
+  const [flippedId, setFlippedId] = useState(null);
+  const value = useMemo(() => [flippedId, setFlippedId], [flippedId]);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    function reset(event) {
+      if (!ref.current.contains(event.target)) {
+        setFlippedId(null);
+      }
+    }
+
+    if (flippedId) {
+      document.addEventListener('click', reset);
+      return () => document.removeEventListener('click', reset);
+    }
+  }, [flippedId]);
+
+  return (
+    <div ref={ref}>
+      <FlippedItemContext.Provider value={value}>
+        {children}
+      </FlippedItemContext.Provider>
+    </div>
+  );
+};
+
+export function useFlippedItem(id) {
+  const [flippedId, setFlippedId] = useContext(FlippedItemContext);
+
+  const toggle = useCallback(() => {
+    setFlippedId(flippedId => flippedId === id ? null : id);
+  }, [id, setFlippedId]);
+
+  return [
+    flippedId === id,
+    toggle
+  ];
+}
