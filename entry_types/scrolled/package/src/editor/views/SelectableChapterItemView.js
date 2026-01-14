d1d4e9d1d2421ab3d120e5ed7c31102df92f0331
Tim Fischbach <tfischbach@codevise.de>
2026-01-14 08:48:30 +0100

    Do not scroll entry outline tabs in link selection dialog
    REDMINE-21205

--- entry_types/scrolled/package/src/editor/views/SelectableChapterItemView.js
+++ entry_types/scrolled/package/src/editor/views/SelectableChapterItemView.js
@@ -1,41 +1,44 @@
 import I18n from 'i18n-js';
 import Marionette from 'backbone.marionette';
+import classNames from 'classnames';
 
 import {cssModulesUtils, CollectionView} from 'pageflow/ui';
 
 import {SelectableSectionItemView} from './SelectableSectionItemView';
 
-import styles from './ChapterItemView.module.css';
+import baseStyles from './ChapterItemView.module.css';
+import styles from './SelectableChapterItemView.module.css';
 
 export const SelectableChapterItemView = Marionette.ItemView.extend({
   tagName: 'li',
-  className: styles.root,
+
+  className: classNames(baseStyles.root, styles.root),
 
   template: () => `
-     <a class="${styles.link}"
+     <a class="${baseStyles.link}"
         href=""
         title="${I18n.t(`pageflow_scrolled.editor.selectable_chapter_item.title`)}">
-       <span class="${styles.number}"></span>
-       <span class="${styles.title}"></span>
+       <span class="${baseStyles.number}"></span>
+       <span class="${baseStyles.title}"></span>
      </a>
 
-     <ul class="${styles.sections}"></ul>
+     <ul class="${baseStyles.sections}"></ul>
      `,
 
-  ui: cssModulesUtils.ui(styles, 'title', 'number', 'sections'),
+  ui: cssModulesUtils.ui(baseStyles, 'title', 'number', 'sections'),
 
-  events: cssModulesUtils.events(styles, {
+  events: cssModulesUtils.events(baseStyles, {
     'click link': function(event) {
       event.preventDefault();
       return this.options.onSelectChapter(this.model);
     },
 
     'mouseenter link': function() {
-      this.$el.addClass(styles.selectableHover);
+      this.$el.addClass(baseStyles.selectableHover);
     },
 
     'mouseleave link': function() {
-      this.$el.removeClass(styles.selectableHover);
+      this.$el.removeClass(baseStyles.selectableHover);
     }
   }),
 
