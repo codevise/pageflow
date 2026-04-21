02ecdde18116fbd4326cbd802e0880c88a2fd7c7
Tim Fischbach <tfischbach@codevise.de>
2026-04-21 11:18:28 +0200

    Select comment thread on badge click in inline editing
    Clicking a thread badge next to a commented text range now selects
    the thread in the editor. This sets up navigation to a per-thread
    comments view in the editor sidebar.

--- entry_types/scrolled/package/src/frontend/inlineEditing/EditableText/BadgeColumn.js
+++ entry_types/scrolled/package/src/frontend/inlineEditing/EditableText/BadgeColumn.js
@@ -4,17 +4,20 @@ import {FloatingPortal} from '@floating-ui/react';
 
 import {Badge, useAnchoredFloating} from 'pageflow-scrolled/review';
 import {useFloatingPortalRoot} from '../../FloatingPortalRootProvider';
+import {useEditorSelection} from '../EditorState';
 
 export function BadgeColumn({highlights, anchors}) {
   return highlights.map(highlight => (
     <PositionedBadge key={highlight.key}
                      rangeKey={highlight.key}
+                     thread={highlight.thread}
                      anchors={anchors} />
   ));
 }
 
-function PositionedBadge({rangeKey, anchors}) {
+function PositionedBadge({rangeKey, thread, anchors}) {
   const portalRoot = useFloatingPortalRoot();
+  const {select} = useEditorSelection({type: 'commentThread', id: thread?.id});
 
   const {refs, floatingStyles, hasAnchor} =
     useAnchoredFloating(rangeKey, anchors);
@@ -24,7 +27,7 @@ function PositionedBadge({rangeKey, anchors}) {
   return (
     <FloatingPortal root={portalRoot}>
       <div ref={refs.setFloating} style={floatingStyles}>
-        <Badge counter={1} />
+        <Badge counter={1} onClick={() => select()} />
       </div>
     </FloatingPortal>
   );
