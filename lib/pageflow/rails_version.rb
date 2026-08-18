33366283084c5cb4b55af5e770a6ce74ad45ca12
Tim Fischbach <tfischbach@codevise.de>
2026-08-18 09:56:59 +0200

    Support Rails 8.0 and 8.1
    Move the supported range to >= 7.2, < 8.2 and derive the gemspec
    requirement from it. Outside that range PAGEFLOW_RAILS_VERSION now drops
    the rails constraint entirely, so both older and newer versions can
    still be tried out. CI covers both ends of the range.

--- lib/pageflow/rails_version.rb
+++ lib/pageflow/rails_version.rb
@@ -3,15 +3,26 @@ module Pageflow
   module RailsVersion
     extend self
 
+    SUPPORTED = ['>= 7.2', '< 8.2'].freeze
+    DEFAULT = '~> 7.2.0'.freeze
+
     def detect
-      from_env || '~> 7.2.0'
+      from_env || DEFAULT
+    end
+
+    def requirement
+      experimental? ? [] : SUPPORTED
     end
 
     def experimental?
-      detect != '~> 7.2.0'
+      !Gem::Requirement.new(SUPPORTED).satisfied_by?(requested_version)
     end
 
     private
+
+    def requested_version
+      Gem::Requirement.new(detect).requirements.first.last
+    end
 
     def from_env
       ENV['PAGEFLOW_RAILS_VERSION'] if ENV['PAGEFLOW_RAILS_VERSION'] != ''
